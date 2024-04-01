64664e1bacf4de083f99805f8c7c4ded8ea98a61
kyhlsd <kmyghn@gmail.com>
2024-04-01 17:12:54 +0900

    Tuple

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,23 +1,10 @@
-protocol Saveable {
-      var saveNeeded: Bool { get set }
-      func saveToRemoteDatabase(handler: @escaping (Bool) -> Void)
-}
-class Person: Saveable {
-    //....
-    var saveHandler: ((Bool) -> Void)?
-    var saveNeeded: Bool = true
-     
-    func saveToRemoteDatabase(handler: @escaping (Bool) -> Void) {
-         saveHandler = handler
-         // Send person information to remove database
-         // Once remote save is complete, it calls
-           // saveComplete(Bool)
-         // We'll fake it for the moment, and assume the save is
-           // complete.
-         saveComplete(success: true)
-    }
-     
-    func saveComplete(success: Bool) {
-        saveHandler?(success)
-    }
+import Foundation
+
+func normalizedStarRating(forRating rating: Float,ofPossibleTotal total: Float ) -> (Int, String){
+    let fraction = rating / total
+    let ratingOutof5 = fraction * 5
+    let roundedRating = round(ratingOutof5)
+    let numberofStars = Int(roundedRating)
+    let ratingString = "\(numberofStars) Star Movie"
+    return (numberofStars,ratingString)
 }
