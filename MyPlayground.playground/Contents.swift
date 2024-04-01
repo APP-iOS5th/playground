02391403d430e2e089d0e9c853a3cd3adf89f42b
SeongKookKIM <ksk8646@naver.com>
2024-04-01 17:00:10 +0900

    프로토콜

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -248,3 +248,31 @@ let createPerson: (String,String,String) -> PPerson = {
 }
 let felix = createPerson("Fleix", "Robert", "Moon")
 print(felix.displayString)
+
+
+//프로토콜
+
+protocol Saveable {
+    var saveNeeded: Bool {get set}
+    func saveToRemoteDatabase(handler: @escaping (Bool) -> Void)
+}
+
+class PTPerson: Saveable {
+    //....
+    var saveHandler: ((Bool) -> Void)?
+    var saveNeeded: Bool = true
+     
+    func saveToRemoteDatabase(handler: @escaping (Bool) -> Void) {
+         saveHandler = handler
+         // Send person information to remove database
+         // Once remote save is complete, it calls
+           // saveComplete(Bool)
+         // We'll fake it for the moment, and assume the save is
+           // complete.
+         saveComplete(success: true)
+    }
+     
+    func saveComplete(success: Bool) {
+        saveHandler?(success)
+    }
+}
