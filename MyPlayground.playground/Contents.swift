ae062a0c90433a55d721236fa37b43a70e7bf210
jmbae <jmbae@codegrove.co.kr>
2024-04-01 11:27:41 +0900

    Basic 1

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,3 +1,36 @@
 import UIKit
 
 var greeting = "Hello, playground"
+
+let phrase: String = "The quick brown fox jumps over the lazy dog"
+
+greeting = "Modification"
+
+let numberOfFoxes: Int = 1
+let numberOfAnimals: Int = 2
+
+let averageCharactersPerWord: Float = (3+5+5+3+5+4+3+4+3) / 9
+
+var anotherPhrase = phrase
+
+anotherPhrase = "Modification"
+
+var phraseInfo = "The phrase" + " has: "
+print(phraseInfo) // "The phrase has: "
+
+phraseInfo = phraseInfo + "\(numberOfFoxes) fox and \(numberOfAnimals) animals"
+print(phraseInfo) // "The phrase has: 1 fox and 2 animals"
+
+print("Number of characters in phrase: \(phrase.count)")
+
+let multilineExplanation: String = """
+Why is the following phrase often used?
+"The quick brown fox jumps over the lazy dog"
+This phrase contains every letter in the alphabet.
+"""
+
+let phrasesAreEqual = phrase == anotherPhrase
+print(phrasesAreEqual)
+
+let phraseHas43Characters = phrase.count == 40 + 3
+print(phraseHas43Characters)
