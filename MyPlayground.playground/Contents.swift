88f0fe9e346292e5c96cf8937d7247bda183bb03
Lee Inho <inhoking@kookmin.ac.kr>
2024-04-01 17:12:18 +0900

    tuple

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,80 +1,12 @@
-//
-//struct PersonName {
-//    let givenName: String
-//    let middleName: String
-//    var familyName: String
-//     
-//    func fullName() -> String {
-//        return "\(givenName) \(middleName) \(familyName)"
-//    }
-//     
-//    mutating func change(familyName: String) {
-//        self.familyName = familyName
-//    }
-//}
-//
-//class Person {
-//     
-//    let birthName: PersonName
-//    var currentName: PersonName
-//    var countryOfResidence: String
-//     
-//    init(name: PersonName, countryOfResidence: String = "UK") {
-//        birthName = name
-//        currentName = name
-//        self.countryOfResidence = countryOfResidence
-//    }
-//     
-//    var displayString: String {
-//        return "\(currentName.fullName()) - Location: \(countryOfResidence)"
-//    }
-//}
-//
-//let printAuthorDetails: (String, String, String) -> Void = {
-//    given, middle, family in
-//    let name = PersonName(givenName: given, middleName: middle, familyName: family)
-//    let author = Person(name: name, countryOfResidence: "KR")
-//    
-//    print(author.displayString)
-//}
-//
-//printAuthorDetails("Inho", "Harry", "Lee")
-//
-//let createPerson: (String, String, String) -> Person = {
-//    given, middle, family in
-//    let name = PersonName(givenName: given, 
-//                          middleName: middle,
-//                          familyName: family)
-//    let person = Person(name: name, countryOfResidence: "KR")
-//    
-//    return person
-//}
-//
-//let me = createPerson("Inho", "Harry", "Lee")
-//print(me.displayString)
-//
+import Foundation
 
-protocol Saveable {
-      var saveNeeded: Bool { get set }
-      func saveToRemoteDatabase(handler: @escaping (Bool) -> Void)
+func normalizedStarRating(forRating rating: Float, ofPossibleTotal total: Float) -> (Int, String) {
+    let fraction = rating / total
+    let ratingOutOf5 = fraction * 5
+    let roundedRating = round(ratingOutOf5)
+    let numberOfStars = Int(roundedRating)
+    let ratingString = "\(numberOfStars) Star Movie"
+    
+    return (numberOfStars, ratingString)
 }
 
-class Person: Saveable {
-    //....
-    var saveHandler: ((Bool) -> Void)?
-    var saveNeeded: Bool = true
-     
-    func saveToRemoteDatabase(handler: @escaping (Bool) -> Void) {
-         saveHandler = handler
-         // Send person information to remove database
-         // Once remote save is complete, it calls
-           // saveComplete(Bool)
-         // We'll fake it for the moment, and assume the save is
-           // complete.
-         saveComplete(success: true)
-    }
-     
-    func saveComplete(success: Bool) {
-        saveHandler?(success)
-    }
-} 
