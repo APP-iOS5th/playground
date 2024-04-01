ec274e640964f49e51f8783af16f3cd4f744e1a0
wnylxx <hyy96hyy@naver.com>
2024-04-01 17:59:42 +0900

    tuple array

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,25 +1,55 @@
 
-protocol Saveable {
-    var saveNeeded: Bool { get set }
-    func saveToRemoteDatabase(handler: @escaping (Bool)-> Void)
+// 튜플
+
+import Foundation
+
+func normalizedStarRating(forRating rating: Float, ofPossibleTotal total: Float) -> (Int, String) {
+    let fraction = rating / total
+    let ratingOutOf5 = fraction * 5
+    let roundedRating = round(ratingOutOf5)
+    let numberOfStars = Int(roundedRating)
+    let ratingString = "\(numberOfStars) Star Movie"
+    return (numberOfStars,ratingString)
 }
 
-class Person: Saveable {
-    //.... 이 프로토콜을 쓰면 너는 무조건 이거 넣어야해! 라고 강제할 수 있음
-    var saveHandler: ((Bool) -> Void)?
-    var saveNeeded: Bool = true
-     
-    func saveToRemoteDatabase(handler: @escaping (Bool) -> Void) {
-         saveHandler = handler
-         // Send person information to remove database
-         // Once remote save is complete, it calls
-           // saveComplete(Bool)
-         // We'll fake it for the moment, and assume the save is
-           // complete.
-         saveComplete(success: true)
-    }
-     
-    func saveComplete(success: Bool) {
-        saveHandler?(success)
-    }
-}
+
+// 배열
+
+
+var moviesToWatch: Array<String> = Array()
+
+moviesToWatch.append("The Shawshank")
+moviesToWatch.append("ghost busters")
+moviesToWatch.append("goooood3")
+
+moviesToWatch[1]
+
+moviesToWatch.insert("why insert", at: 1)
+
+moviesToWatch[1]
+
+moviesToWatch.first
+
+let secondMovieToWatch = moviesToWatch[1]
+moviesToWatch[1] = "shinsaegye (1984)"
+print(moviesToWatch.count)
+print(moviesToWatch)
+
+
+let spymovie: [String] = ["1","2","3"]
+moviesToWatch = moviesToWatch + spymovie
+
+print(moviesToWatch)
+
+var starwarsTrilogy = Array<String>(repeating: "Star Wars: ", count: 3)
+starwarsTrilogy[0] += "A New Hope"
+starwarsTrilogy[1] += "Empire Strikes Back"
+starwarsTrilogy[2] += "Return of the Jedi"
+
+print(starwarsTrilogy)
+
+moviesToWatch.replaceSubrange(2...4, with: starwarsTrilogy)
+print(moviesToWatch)
+
+moviesToWatch.remove(at: 6)
+print(moviesToWatch)
