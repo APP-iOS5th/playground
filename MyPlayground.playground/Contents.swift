ff429cc141752701fa5c47102672c6294d1ae2ca
Jin Park <jinpark1504@gmail.com>
2024-04-01 17:00:52 +0900

    protocol

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -12,7 +12,12 @@ struct PersonName {
     }
 }
 
-class Person {
+protocol Saveable {
+    var saveNeeded: Bool { get set }
+    func saveToRemoteDatabase(handler: @escaping (Bool) -> Void)
+}
+
+class Person: Saveable {
     let birthName: PersonName
     var currentName: PersonName
     var countryOfResidence: String
@@ -26,39 +31,20 @@ class Person {
     var displayString: String {
         return "\(currentName.fullName()) - Location: \(countryOfResidence)"
     }
+    var saveHandler: ((Bool) -> Void)?
+    var saveNeeded: Bool = true
+     
+    func saveToRemoteDatabase(handler: @escaping (Bool) -> Void) {
+         saveHandler = handler
+         // Send person information to remove database
+         // Once remote save is complete, it calls
+           // saveComplete(Bool)
+         // We'll fake it for the moment, and assume the save is
+           // complete.
+         saveComplete(success: true)
+    }
+     
+    func saveComplete(success: Bool) {
+        saveHandler?(success)
+    }
 }
-// closer
-//let printAuthorsDetails: () -> Void = {
-let printAuthorsDetails: () -> () = {
-    let name = PersonName(givenName: "Jin", middleName: "J.", familyName: "Park")
-    let author = Person(name: name)
-    print(author.displayString)
-}
-
-printAuthorsDetails()
-
-let createAuthor: () -> Person = {
-    let name = PersonName(givenName: "Keith", middleName: "David", familyName: "Moon")
-    let author = Person(name: name)
-    return author
-}
-
-let author = createAuthor()
-print(author.displayString)
-
-let printPersonDetails: (String, String, String) -> Void = { given, middle, family in
-    let name = PersonName(givenName: given, middleName: middle, familyName: family)
-    let person = Person(name: name)
-    print(person.displayString)
-}
-
-printPersonDetails("Kathleen", "Mary", "Moon")
-
-let createPerson: (String, String, String) -> Person = { given, middle, family in
-    let name = PersonName(givenName: given, middleName: middle, familyName: family)
-    let person = Person(name: name)
-    return person
-}
-
-let felix = createPerson("Felix", "Robert", "Moon")
-print(felix.displayString)
