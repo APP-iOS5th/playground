c9b593fbf9529c8d0b611ae2832e2556b00729f8
rainbow_an <eventscripts@nate.com>
2024-04-01 14:34:12 +0900

    Func initial

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,16 +1,26 @@
 
 
-
-func combine(_ givenName: String, _ familyName: String) -> String {
-    return "\(givenName) \(familyName)"
+class Person {
+    let givenName: String
+    let middleName: String
+    let familyName: String
+    var countryOfResidence: String = "KR"
+    
+    init(givenName: String, middleName: String, familyName: String, countryOfResidence: String) {
+        self.givenName = givenName
+        self.middleName = middleName
+        self.familyName = familyName
+    }
+    
+    func fullName() -> String {
+        return "\(givenName) \(middleName) \(familyName)"
+    }
+  
+    var displayString : String {
+        return "\(fullName()) - Location: \(countryOfResidence)"
+    }
+    
+    
 }
-func combine(_ integer1: Int, _ integer2: Int) -> Int {
-    return integer1 + integer2
-}
-let combinedString = combine("Finnley", "Moon")
-let combinedInt = combine(5, 10)
-print(combinedString) // Finnley Moon
-print(combinedInt) // 15
 
-
-
+var person = Person(givenName: "Jungman", middleName: "Dan", familyName: "Bae", countryOfResidence: "KR")
