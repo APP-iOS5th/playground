6a3eea9fa27b4d4e3ca4da930c8ccf563005199e
Lee Inho <inhoking@kookmin.ac.kr>
2024-04-01 14:24:55 +0900

    swift class

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,14 +1,22 @@
-
-func combine(_ givenName: String, _ familyName: String) -> String {
-    return "\(givenName) \(familyName)"
+class Person {
+    let givenName: String
+    let middleName: String
+    let familyName: String
+    var countryOfResidence: String = "KR"
+    
+    init(givenName: String, middleName: String, familyName: String) {
+        self.givenName = givenName
+        self.middleName = middleName
+        self.familyName = familyName
+    }
+    
+    func fullName() -> String{
+        return "\(givenName) \(middleName) \(familyName)"
+    }
+    
+    var displayString: String {
+        return "\(fullName()) - Location: \(countryOfResidence)"
+    }
 }
 
-func combine(_ integer1: Int, _ integer2: Int) -> Int {
-    return integer1 + integer2
-}
 
-let combinedString = combine("Finnley", "Moon")
-let combinedInt = combine(5, 10)
-
-print(combinedString)
-print(combinedInt)
