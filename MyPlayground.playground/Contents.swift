804ad69b9d2ff8f01a91ec13b8b12deeff18622e
Hyunseo Yook <hhh3332@naver.com>
2024-04-01 13:48:14 +0900

    overloading

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,7 +1,18 @@
-func fullName(givenName: String, middleName: String, familyName: String) -> String {
-    return "\(givenName) \(middleName) \(familyName)"
-}
+//func fullName(givenName: String, middleName: String = "Unknown", familyName: String) -> String {
+//    return "\(givenName) \(middleName) \(familyName)"
+//}
+//
+//let myFullName = fullName(givenName: "Keith", familyName: "Moon");
+//
+//print(myFullName)
 
-let myFullName = fullName(givenName: "Keith", middleName: "David", familyName: "Moon");
 
-print(myFullName)
+func combine(_ givenName: String, _ familyName: String) -> String { return "\(givenName) \(familyName)"}
+
+func combine(_ integer1: Int, _ interger2: Int) -> Int { return integer1 + interger2}
+
+let combinedString = combine("Finnley", "Moon")
+let combinedInt = combine(5, 10)
+
+print(combinedString)
+print(combinedInt)
