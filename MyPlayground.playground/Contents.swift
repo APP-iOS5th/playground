b5b4c97af94ee92767ee580c7d6a297e7a1d4f07
YACHAE <104hsh76@gmail.com>
2024-04-01 14:51:50 +0900

    class inheritance

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -4,9 +4,9 @@ class Person {
     let givenName: String
     let middleName: String
     let familyName: String
-    var contryOfResidence: String = "UK"
+    var contryOfResidence: String = "KR"
     
-    init(givenName: String, middleName: String, familyName: String, contryOfResidence: String) {
+    init(givenName: String, middleName: String, familyName: String) {
         self.givenName = givenName
         self.middleName = middleName
         self.familyName = familyName
@@ -20,3 +20,18 @@ class Person {
     }
 }
 
+var person = (givenName: "Sanghyeon", middleName: "Yan", familyName: "Han") // 클래스 인스턴스
+
+final class Family: Person { // 상속이 여끼까지라는 의미
+    let relationship: String
+    
+    init(givenName: String,
+         middleName: String,
+         familyName: String,
+         relationship: String) { // 새롭게 추가된 상수값을 초기화하는 생성자가 추가되었다.
+        self.relationship = relationship
+        super.init(givenName: givenName, middleName: middleName, familyName: familyName) // 부모의 생성자를 상속
+    }
+}
+
+var family = Family(givenName: "Sanghyeon", middleName: "Yan", familyName: "Han", relationship: "Brother")
