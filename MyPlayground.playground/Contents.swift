48856192f313d4dbc9905c025c9095817a4ef1e3
wnylxx <hyy96hyy@naver.com>
2024-04-01 15:22:35 +0900

    class

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -60,6 +60,78 @@ func combine(_ integer1: Int, _ integer2: Int) -> Int {
 let combinedString = combine("Hoe", "Won")
 let combinedInt = combine(5, 10)
 
-print(combinedString)
-print(combinedInt)
+//print(combinedString)
+//print(combinedInt)
+
+class Person {
+    let givenName: String
+    let middleName: String
+    let familyName: String
+    var countryOfResidence: String = "KR"
+    
+    
+    //초기화 매서드
+    init(givenName: String, middleName: String, familyName: String) {
+        self.givenName = givenName
+        self.middleName = middleName
+        self.familyName = familyName
+    }
+    
+    func fullName() -> String {
+        return "\(givenName) \(middleName) \(familyName)"
+    }
+    
+    // class 내의 함수이므로 파라미터를 지정안해도 값을 사용할 수 있다.
+    
+    var displayString: String {
+        return "\(self.fullName()) - Location: \(self.countryOfResidence)"
+    }
+}
+
+var person = Person(givenName: "HEO", middleName: "WON", familyName: "yoel")
+
+//print(person.fullName())
+
+// 클래스 상속하기
+final class Family: Person{
+    let relationship: String
+    
+    init(givenName: String,
+         middleName: String,
+         familyName: String,
+         relationship: String) {
+        self.relationship = "Son"
+        super.init(givenName: givenName, middleName: middleName, familyName: familyName)
+        // super : 나를 상속한 부모 클래스를 호출, 이미 정의되어있는 변수를 불러올 수 있다.
+        
+        func displayrelation() -> String {
+            return "relation: \(self.relationship)"
+        //person의 fullname 함수를 쓸수 있나?
+        }
+    }
+}
+    
+var family = Family(givenName: "HEO", middleName: "won", familyName: "yeol", relationship: "Son")
+//print(family.displayrelation()) 왜 안될까..
+
+class MovieReview {
+    let movieTitle: String
+    var statRating: Int
+    init(movieTitle: String, statRating: Int) {
+        self.movieTitle = movieTitle
+        self.statRating = statRating
+    }
+}
+
+let shawshankReview = MovieReview(movieTitle: "Shawshank", statRating: 3)
+
+print(shawshankReview.statRating)
+
+shawshankReview.statRating = 5
+
+print(shawshankReview.statRating)
+    
+
+//enums (열겨형)
+
 
