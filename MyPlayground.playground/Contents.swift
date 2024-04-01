a4587eb3cc06e9c140d6142befb3b662d19d0f9a
shin-jae-uk <shinju1412@gmail.com>
2024-04-01 13:45:42 +0900

    Parameter overloading

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -51,12 +51,25 @@
 ////print(lastNumberOfFIngersHeldUpByFinn)
 //
 
-func fullName(givenName: String, middleName: String = "", familyName: String) -> String {
-    return "\(givenName) \(middleName) \(familyName)"
+//func fullName(givenName: String, middleName: String = "", familyName: String) -> String {
+//    return "\(givenName) \(middleName) \(familyName)"
+//}
+//
+//let myFullName = fullName(givenName: "Keith", middleName: "David", familyName: "Moon")
+//let hisFullName = fullName(givenName: "Jay", familyName: "Shin")
+//print(myFullName)
+//print(hisFullName)
+
+func combine(_ givenName: String, _ familyName: String) -> String {
+    return "\(givenName) \(familyName)"
 }
 
-let myFullName = fullName(givenName: "Keith", middleName: "David", familyName: "Moon")
-let hisFullName = fullName(givenName: "Jay", familyName: "Shin")
-print(myFullName)
-print(hisFullName)
+func combine(_ integer1: Int, _ integer2: Int) -> Int {
+    return integer1 + integer2
+}
 
+let combinedString = combine("Finnley", "Moon")
+let combinedInt = combine(5, 10)
+
+print(combinedString) // Finnley Moon
+print(combinedInt) // 15
