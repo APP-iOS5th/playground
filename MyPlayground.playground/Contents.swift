2f4a2893f1ab0895c0664a06d548c86e7e1089f2
sohee <danwater86@gmail.com>
2024-04-01 15:22:09 +0900

    Reference Type

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,40 +1,22 @@
-class Person {
-    let givenName: String
-    let middleName: String
-    let familyName: String
-    var countryOfResidence: String = "KR"
-    
-    init(givenName: String, middleName: String, familyName: String) {
-        self.givenName = givenName
-        self.middleName = middleName
-        self.familyName = familyName
-    }
-    
-    func fullName() -> String {
-        return "\(givenName) \(middleName) \(familyName)"
-        
-    }
-    
-    var displayString: String {
-//      var contryOfResidence = "?"  // 변수스코프 설명하면서 추가
-        return "\(fullName()) - Location: \(countryOfResidence)"
-    }
-    
-}
-
-// 실행부
-var person = Person(givenName: "Jungman", middleName: "Dan", familyName: "Bae")
-
-final class Family: Person {
-    let relationship: String
-    
-    init(givenName: String,
-         middleName: String,
-         familyName: String,
-         relationship: String) {
-        self.relationship = relationship
-        super.init(givenName: givenName, middleName: middleName, familyName: familyName)
+class MovieReview {
+    let movieTitle: String
+    var starRating: Int
+    init(movieTitle: String, starRating: Int) {
+        self.movieTitle = movieTitle
+        self.starRating = starRating
     }
 }
 
-var family = Family(givenName: "Jungman", middleName: "Dan", familyName: "bae", relationship: "Dad")
+let shawshandkReviewOnYourWebsite = MovieReview(movieTitle: "Shawshank Redemption", starRating: 3)
+// 소셜 미디어에 게시
+let referenceToReviewOnTwitter = shawshandkReviewOnYourWebsite
+let referenceToReviewOnFacebook = shawshandkReviewOnYourWebsite
+
+print(referenceToReviewOnTwitter.starRating)
+print(referenceToReviewOnFacebook.starRating)
+
+shawshandkReviewOnYourWebsite.starRating = 5
+
+// 변경 사항이 모든 참조에 반영됨
+print(referenceToReviewOnTwitter.starRating)
+print(referenceToReviewOnFacebook.starRating)
