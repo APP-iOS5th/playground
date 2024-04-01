3c47c5949e79254569ac3c9b115a6729e1feb562
Lee Inho <inhoking@kookmin.ac.kr>
2024-04-01 17:56:36 +0900

    list

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,12 +1,26 @@
-import Foundation
+var moviesToWatch: Array<String> = Array()
+moviesToWatch.append("1")
+moviesToWatch.append("2")
+moviesToWatch.append("3")
+moviesToWatch.append("4")
+moviesToWatch.append("5")
 
-func normalizedStarRating(forRating rating: Float, ofPossibleTotal total: Float) -> (Int, String) {
-    let fraction = rating / total
-    let ratingOutOf5 = fraction * 5
-    let roundedRating = round(ratingOutOf5)
-    let numberOfStars = Int(roundedRating)
-    let ratingString = "\(numberOfStars) Star Movie"
-    
-    return (numberOfStars, ratingString)
-}
+//var arr: Array<[String]> = Array(repeating: [], count: 2)
+//arr[0].append("fd")
+//arr[0].append("ff")
+//print(arr.first ?? "Empty")
+
+//var arr: [String] = []
+//var arr: Array<String> = Array()
+//var arr: Array<String> = Array<String>()
+
+var starWars = Array<String>(repeating: "Star Wars: ", count: 3)
+starWars[0] += "1"
+starWars[1] += "2"
+starWars[2] += "3"
+
+moviesToWatch.replaceSubrange(2...4, with: starWars)
+
+print(moviesToWatch)
+
 
