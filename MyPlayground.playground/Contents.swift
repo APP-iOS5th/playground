5347d194cbdb4d1fc5310c0e42475c950416fb8d
Jongchan Yun <jcyun1996@gmail.com>
2024-04-01 16:39:37 +0900

    Closer

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -14,7 +14,6 @@ struct PersonName {
 }
 
 class Person {
-     
     let birthName: PersonName
     var currentName: PersonName
     var countryOfResidence: String
@@ -29,3 +28,34 @@ class Person {
         return "\(currentName.fullName()) - Location: \(countryOfResidence)"
     }
 }
+
+
+let printAuthorDetails: () -> Void = {
+    let name = PersonName(givenName: "Jungman", middleName: "Dan", familyName: "Bae")
+    let author = Person(name: name)
+    print(author.displayString)
+}
+
+printAuthorDetails()
+
+
+let createAuthor: () -> Person = {
+    let name = PersonName(givenName: "Keith",
+                          middleName: "David",
+                          familyName: "Moon")
+    let author = Person(name: name)
+    return author
+}
+let author = createAuthor()
+print(author.displayString)
+
+let createPerson: (String, String, String) -> Person = { given,
+  middle, family in
+    let name = PersonName(givenName: given,
+                          middleName: middle,
+                          familyName: family)
+    let person = Person(name: name)
+    return person
+}
+let felix = createPerson("Felix", "Robert", "Moon")
+print(felix.displayString)
