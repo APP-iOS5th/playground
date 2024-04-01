3c4413cb89621169431e3fd3fe513b95f209e812
shin-jae-uk <shinju1412@gmail.com>
2024-04-01 17:56:11 +0900

    Array

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,13 +1,40 @@
-// Tuples
-import Foundation
+// Array
 
-func normalizedStarRating(forRating rating: Float, ofPossibleTotal total: Float) ->(Int, String) {
-    let fraction = rating / total
-    let ratingOutOf5 = fraction * 5
-    let roundedRating = round(ratingOutOf5) // 반올림
-    let numberOfStars = Int(roundedRating)
-    let ratingString = "\(numberOfStars) Star Movie"
-    
-    return (numberOfStars, ratingString)
-}
+var moviesToWatch: Array<String> = Array()
 
+moviesToWatch.append("The Shawshank Redemption")
+moviesToWatch.append("Ghostbusters")
+moviesToWatch.append("Terminator 2")
+
+moviesToWatch[0]
+moviesToWatch[1]
+moviesToWatch[2]
+print(moviesToWatch.count)
+
+moviesToWatch.insert("The Matrix", at: 2) // 기존 2번 내용을 뒤로 밀어내고 새롭게 추가됨
+
+print(moviesToWatch.first ?? "Empty")
+print(moviesToWatch.last ?? "Empty")
+
+let secondMovieToWatch = moviesToWatch[1]
+moviesToWatch[1] = "Ghostbusters (1984)"
+print(moviesToWatch.count)
+print(moviesToWatch)
+
+let spyMovieSuggestion: [String] = ["The Bourne Indenty", "Casino Royale", "Mission Impossible"]
+moviesToWatch = moviesToWatch + spyMovieSuggestion
+print(moviesToWatch.count)
+print(moviesToWatch)
+
+var starWarsTrilogy = Array<String>(repeating: "Star Wars: ", count: 3)
+starWarsTrilogy[0] += "A New Hope"
+starWarsTrilogy[1] += "Empire Strikes Back"
+starWarsTrilogy[2] += "Return of the Jedi"
+print(starWarsTrilogy)
+
+moviesToWatch.replaceSubrange(2...4, with: starWarsTrilogy) // [2]~[4] 범위에서 배열 교체
+print(moviesToWatch.count)
+
+moviesToWatch.remove(at: 6) // [6] 배열 값 삭제
+print(moviesToWatch.count)
+print(moviesToWatch)
