009c8435ea6a32b626a7d7f76f333c7a33e8e3d3
Cha, Jiyong <wldyd2113@gmail.com>
2024-04-01 13:46:42 +0900

    Parameter Overload

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,11 +1,13 @@
 import UIKit
 
-//Chapter3
-//함수 선언
-func fullName(giveName: String, middleName: String = "Unkown", familyName: String) -> String{
-    return "\(giveName) \(middleName) \(familyName) !"
+func combine(_ givenName: String, _ familyNme: String) -> String{ //_을 사용하면 함수 레이블을 사용하자 않고 사용할수 있음
+    return "\(givenName) \(familyNme)"
 }
+func combine(_ integer1: Int, _ integer2: Int) -> Int {
+    return integer1 + integer2
+}
+let combinedString = combine("Finnley", "Moon")
+let combinedInt = combine(5,10)
 
-let myFullName = fullName(giveName: "Keith", familyName: "Moon")
-print(myFullName)
-
+print(combinedString)
+print(combinedInt)
