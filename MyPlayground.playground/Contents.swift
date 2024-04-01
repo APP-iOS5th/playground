2a548e59df4d1f42e689941ac5a9717600b6533a
Jin Park <jinpark1504@gmail.com>
2024-04-01 15:22:16 +0900

    reference type

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,39 +1,20 @@
-class Person {
-    let givenName: String
-    let middleName: String
-    let familyName: String
-    var countryOfResidence: String = "KR"
-    
-    init (givenName: String, middleName: String, familyName: String) {
-        self.givenName = givenName
-        self.middleName = middleName
-        self.familyName = familyName
-    }
-    
-    var displayString: String {
-        return "\(fullName()) - Location: \(countryOfResidence)"
-    }
-    func fullName() -> String {
-        return "\(givenName) \(middleName) \(familyName)"
+class MovieReview {
+    let movieTitle: String
+    var starRating: Int
+    init(movieTitle: String, starRating: Int) {
+        self.movieTitle = movieTitle
+        self.starRating = starRating
     }
 }
 
-let jin = Person(givenName: "Jin", middleName: "J.", familyName: "Park")
-jin.countryOfResidence = "CA"
-print(jin.displayString)
+let shawshankReviewOnYourWebsite = MovieReview(movieTitle: "Shawshank Redemption", starRating: 3)
+let referenceToReviewOnTwitter =  shawshankReviewOnYourWebsite
+let referenceToReviewOnFacebook = shawshankReviewOnYourWebsite
 
-final class Family: Person {
-    let relationship: String
-    
-    init(givenName: String,
-         middleName: String,
-         familyName: String,
-         relationship: String) {
-        self.relationship = relationship
-        super.init(givenName: givenName, middleName: middleName, familyName: familyName)
-    }
-}
+print(referenceToReviewOnTwitter.starRating)
+print(referenceToReviewOnFacebook.starRating)
 
-var family = Family(givenName: "Jin", middleName: "J.", familyName: "Park", relationship: "Dad")
-print(family.givenName)
+shawshankReviewOnYourWebsite.starRating = 5
 
+print(referenceToReviewOnTwitter.starRating)
+print(referenceToReviewOnFacebook.starRating)
