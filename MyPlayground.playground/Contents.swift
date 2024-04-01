3a02bddaa6aa5c1e2127504327d7089747f9fcc1
rainbow_an <eventscripts@nate.com>
2024-04-01 16:37:18 +0900

    Closure

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -29,3 +29,46 @@ class Person {
         return "\(currentName.fullName()) - Location: \(countryOfResidence)"
     }
 }
+
+/* Void 대신()*/
+let printAuthorDetails: () -> Void = {
+    let name = PersonName(givenName: "Seonggeun", middleName: "Mosi", familyName: "An")
+    let author = Person(name: name)
+    print(author.displayString)
+}
+
+printAuthorDetails()
+
+
+let createAuthor: () -> Person = {
+    let name = PersonName(givenName: "Keith", middleName: "David", familyName: "Moon")
+    let author = Person(name: name)
+    return author
+}
+
+let author = createAuthor()
+print(author.displayString)
+
+
+//String input !!No Output
+let printPersonDetails: (String, String, String) -> Void = {
+    given, middle, family in
+    let name = PersonName(givenName: given, middleName: middle, familyName: family)
+    let author = Person(name:name)
+    print(author.displayString)
+}
+
+printPersonDetails("Kathleen", "Mary", "Moon")
+
+
+//String Input  Output is OK
+let createPerson: (String, String, String) -> Person = { given,
+  middle, family in
+    let name = PersonName(givenName: given,
+                          middleName: middle,
+                          familyName: family)
+    let person = Person(name: name)
+    return person
+}
+let felix = createPerson("Felix", "Robert", "Moon")
+print(felix.displayString)
