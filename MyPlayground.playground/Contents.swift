59b3e75221fcc83a72e7fd3b927030df73f59224
hwang <jangku88@naver.com>
2024-04-01 15:36:22 +0900

    Enumeration

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,26 +1,29 @@
 import UIKit
 
-class MovieReview {
-    let movieTitle: String
-    var starRating: Int
+// 열거형
+enum ComparisionResult: Int {
+    case orderedAscending // 0
+    case orderedSome // 1
+    case orderDescendin // 2
+    // ... 3, 4, 5
+}
+
+enum Title: String {
+    case mr = "Mr"
+    case mrs = "Mrs"
+    case mister = "Mister"
+    case miss = "Miss"
+    case dr = "Dr"
+    case prof = "Prof"
+    case other
     
-    // 상수라고 하고 변수라고 초기화 안해주는 건 아니다.
-    init(movieTitle: String, starRating: Int) {
-        self.movieTitle = movieTitle
-        self.starRating = starRating
+    // () -> 이게 빠지면 29line의 isProfessional()의 ()도 빠진다. (빠진게 더 낫다고 함.)
+    func isProfessional() -> Bool {
+        return self == Title.dr || self == Title.prof
     }
 }
 
-let shawshankReviewOnYourWebsite = MovieReview(movieTitle: "Shawshank Redemtion", starRating: 3)
+// 로직을 이해하기 쉽고 간결하다는 장점이 있다.
+let title1 = Title.mr
 
-// 소셜 미디어에 게시
-let referenceToReviewOnTwitter = shawshankReviewOnYourWebsite
-let referenceToReviewOnFaceBook = shawshankReviewOnYourWebsite
-
-print(referenceToReviewOnTwitter.starRating)
-print(referenceToReviewOnFaceBook.starRating)
-
-shawshankReviewOnYourWebsite.starRating = 5
-
-print(referenceToReviewOnTwitter.starRating)
-print(referenceToReviewOnFaceBook.starRating)
+print(title1.isProfessional())
