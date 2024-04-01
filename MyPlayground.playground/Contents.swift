739a5278210f626302f1dc330b8909477cf7a90a
youngwoo_ahn <codeway119@gmail.com>
2024-04-01 17:03:35 +0900

    프로토콜

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -245,6 +245,33 @@ print(felix.displayString)
 
 
 
+// --프로토콜-- //
+protocol Saveable {
+    var saveNeeded : Bool {get set}
+    func saveToRemoteDatabase(handler: @escaping (Bool) -> Void)
+}
+
+class Person3: Saveable {
+    //....
+    var saveHandler: ((Bool) -> Void)?
+    var saveNeeded: Bool = true
+     
+    func saveToRemoteDatabase(handler: @escaping (Bool) -> Void) {
+         saveHandler = handler
+         // Send person information to remove database
+         // Once remote save is complete, it calls
+           // saveComplete(Bool)
+         // We'll fake it for the moment, and assume the save is
+           // complete.
+         saveComplete(success: true)
+    }
+     
+    func saveComplete(success: Bool) {
+        saveHandler?(success)
+    }
+}
+
+
 
 
 
