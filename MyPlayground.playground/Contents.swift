f1f9ce4b4cba665ffcf294a63f1c4acc8555c8e8
YACHAE <104hsh76@gmail.com>
2024-04-01 14:27:06 +0900

    classDeclaration

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,15 +1,22 @@
 import UIKit
 
-func combine(_ givenName: String, _ familyName: String) -> String {
-    return "\(givenName) \(familyName)"
+class Person {
+    let givenName: String
+    let middleName: String
+    let familyName: String
+    var contryOfResidence: String = "UK"
+    
+    init(givenName: String, middleName: String, familyName: String, contryOfResidence: String) {
+        self.givenName = givenName
+        self.middleName = middleName
+        self.familyName = familyName
+    }
+    func funllName() -> String {
+        return "\(givenName) \(middleName) \(familyName)"
+    }
+    
+    var displayString: String {
+        return "\(self.funllName()) - Location: \(self.contryOfResidence)"
+    }
 }
-
-func combine(_ integer1: Int, _ integer2: Int) -> Int {
-    return integer1 + integer2
-}
-
-let combinedString = combine("Finnley", "Moon")
-let combinedInt = combine(5, 10)
-print(combinedString) // Finnley Moon
-print(combinedInt) // 15
 
