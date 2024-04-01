e240dd93e0840130752e7b85996481991171b93c
youngwoo_ahn <codeway119@gmail.com>
2024-04-01 15:34:29 +0900

    열거형

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -147,9 +147,33 @@ print(referceToReviewOnTwiter.startRating)
 print(referceToReviewOnFacebook.startRating)
 
 
+// -- enum -- //
+enum ComparisionResult: Int {
+    case orderedAsceding // 0
+    case orderedSame // 1
+    case orderedDescending // 2
+    // ... 3, 4, 5
+}
+
+enum Title: String {
+    case mr = "Mr"
+    case mrs = "Mrs"
+    case mister = "Mister"
+    case miss = "Miss"
+    case dr = "Dr"
+    case prof = "Prof"
+    case other
+    
+    var isProfessional: Bool {
+        return self == Title.dr || self == Title.prof
+    }
+}
+
+let title1 = Title.mr
+print(title1.isProfessional)
 
 
-
+// -- //
 
 
 
