bf65f87704646ce78d2b2d0e62b84e14ef639817
hwang <jangku88@naver.com>
2024-04-01 15:00:09 +0900

    Class

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,20 +1,61 @@
 import UIKit
 
-// _ : 와일드카드 패턴 (변수명, 레이블을 제외하고 사용할 수 있도록 해 줌.)
-// 매개변수 오버로드
-func combine (_ giveName: String, _ familyName: String) -> String {
-    return "\(giveName) \(familyName)"
+// 1. Person 클래스 객체 생성
+// 클래스의 선언부(설계도)
+class Person {
+    let givenName: String
+    let middleName: String
+    let familyName: String
+    let countryOfResidence: String = "KR"
+    // 위에 까지만 작성하면 Class 'Person' has no initializers 이런 에러가 난다. (init이 필요함.)
+    
+    // 변수는 나중에 값을 넣어줘도 되지만 상수는 미리? 넣어줘야 함.(꼭 안 넣어도 된다 안 넣어야 한다는 아님)
+    // self는 class parameter가 가지고 있는 이름이라는 걸 명시
+    init(givenName: String, middleName: String, familyName: String) {
+        self.givenName = givenName
+        self.middleName = middleName
+        self.familyName = familyName
+    }
+    
+    // class method와 일반 함수의 차이점 : 파라미터를 만들지 않아도 된다. (맞나?)
+    func fullName() -> String {
+        return "\(givenName) \(middleName) \(familyName)"
+    }
+    
+    // 함수와 변수 중간에 있는 녀석, 뭘까..
+    var displayString: String {
+        // var countryOfResidence = "?" // 변수 스코프 설명을 위해 작성했던 코드
+        return "\(fullName()) - Location: \(self.countryOfResidence))"
+    }
 }
 
-func combine (_ integer1: Int, _ integer2: Int) -> Int {
-    return integer1 + integer2
+// 아래의 peson이 클래스 인스턴스
+var person = Person(givenName: "Kyusang", middleName: "Shark", familyName: "Hwang")
+
+// 가족이라는 클래스는 펄슨이라는 클래스 보다 더 자세하기 설명한다.
+final class Family: Person {
+    let relationship: String
+    
+    init(givenName: String,
+        middleName: String,
+        familyName: String = "Moon",
+        relationship: String) {
+        self.relationship = relationship
+        super.init(givenName: givenName, middleName: middleName, familyName: familyName)
+    }
 }
 
-// 파라미터 오버로딩
-// 와일드카드 패턴 혹은 오버로딩을 안 한다면: 
-// let comebinedString = combine(givenName: "Finnley", familyName: "Moon")
-let comebinedString = combine("Finnley", "Moon")
-let comebinedInt = combine(5, 10)
+var family = Family(givenName: "Kyusang", middleName: "Shark", familyName: "Hwang", relationship: "Pet")
 
-print(comebinedString)
-print(comebinedInt)
+
+
+
+
+
+
+
+
+
+
+
+
