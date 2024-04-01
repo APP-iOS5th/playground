921ca37a7d1abdbc1dca5ac5f650e3a1437d7409
rainbow_an <eventscripts@nate.com>
2024-04-01 13:26:50 +0900

    How to func use

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,3 +1,10 @@
-import UIKit
 
-var greeting = "Hello, playground"
+
+
+func fullName(givenName: String, middleName: String, familyName: String) -> String {
+    return "\(givenName) \(middleName) \(familyName)!"
+}
+
+let myFullName = fullName(givenName: "Keith", middleName: "David", familyName: "Moon")
+
+print(myFullName)
