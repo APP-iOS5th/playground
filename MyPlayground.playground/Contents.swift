6e304b055cc7b756f94b0ca1b5cb5ed81039e94b
kyhlsd <kmyghn@gmail.com>
2024-04-01 17:56:34 +0900

    Array

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,10 +1,36 @@
-import Foundation
+var moviesToWatch: Array<String> = Array()
 
-func normalizedStarRating(forRating rating: Float,ofPossibleTotal total: Float ) -> (Int, String){
-    let fraction = rating / total
-    let ratingOutof5 = fraction * 5
-    let roundedRating = round(ratingOutof5)
-    let numberofStars = Int(roundedRating)
-    let ratingString = "\(numberofStars) Star Movie"
-    return (numberofStars,ratingString)
-}
+moviesToWatch.append("The Shawshank Redemption")
+moviesToWatch.append("Ghostbusters")
+moviesToWatch.append("Terminator 2")
+
+print(moviesToWatch[1])
+print(moviesToWatch.count)
+
+moviesToWatch.insert("The Matrix", at: 2)
+
+print(moviesToWatch.first ?? "Empty")
+print(moviesToWatch.last ?? "Empty")
+
+let secondMovieToWatch = moviesToWatch[1]
+moviesToWatch[1] = "Ghostbusters (1984)"
+print(moviesToWatch.count)
+print(moviesToWatch)
+
+let spyMovieSuggestions: [String] = ["The Bourne Identity", "Casino Royale", "Mission Impossible"]
+moviesToWatch = moviesToWatch + spyMovieSuggestions
+print(moviesToWatch.count)
+print(moviesToWatch)
+
+var starWarsTrilogy = Array<String>(repeating: "Star Wars: ",count: 3)
+starWarsTrilogy[0] += "A New Hope"
+starWarsTrilogy[1] += "Empire Strikes Back"
+starWarsTrilogy[2] += "Return of the Jedi"
+print(starWarsTrilogy)
+
+moviesToWatch.replaceSubrange(2...4, with: starWarsTrilogy)
+print(moviesToWatch.count)
+print(moviesToWatch)
+
+moviesToWatch.remove(at: 6)
+print(moviesToWatch.count)
