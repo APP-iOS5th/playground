1c1edf66f8a5f5116926f45d846b31b45c0ee910
shin-jae-uk <shinju1412@gmail.com>
2024-04-01 16:35:06 +0900

    Closure

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,3 +1,4 @@
+// closure
 
 struct PersonName {
     let givenName: String
@@ -17,8 +18,8 @@ class Person {
     let birthName: PersonName
     var currentName: PersonName
     var countryOfResidence: String
-     
-    init(name: PersonName, countryOfResidence: String = "UK") {
+    
+    init(name: PersonName, countryOfResidence: String = "KR") {
         birthName = name
         currentName = name
         self.countryOfResidence = countryOfResidence
@@ -28,3 +29,38 @@ class Person {
         return "\(currentName.fullName()) - Location: \(countryOfResidence)"
     }
 }
+
+let printAuthorDetails: () -> Void = {
+    let name = PersonName(givenName: "Jaeuk", middleName: "Jay", familyName: "Shin")
+    let author = Person(name: name)
+    print(author.displayString)
+}
+
+printAuthorDetails()
+
+let createAuthor: () -> Person = {
+    let name = PersonName(givenName: "Keith", middleName: "David", familyName: "Moon")
+    let author = Person(name: name)
+    return author
+}
+let author = createAuthor()
+print(author.displayString)
+
+
+// String inputs, no output
+let printPersonsDetails: (String, String, String) -> Void = { given, middle, family in
+    let name = PersonName(givenName: given, middleName: middle, familyName: family)
+    let author = Person(name: name)
+    print(author.displayString)
+}
+printPersonsDetails("Kathleen", "Mary", "Moon")
+
+let createPerson: (String, String, String) -> Person = { given, middle, family in
+    let name = PersonName(givenName: given,
+                          middleName: middle,
+                          familyName: family)
+    let person = Person(name: name)
+    return person
+}
+let felix = createPerson("Felix", "Robert", "Moon")
+print(felix.displayString)
