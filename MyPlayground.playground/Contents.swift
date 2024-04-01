db317fd50585cb1c423a4ff3f8df3c3393a4e66a
YACHAE <104hsh76@gmail.com>
2024-04-01 16:00:29 +0900

    struct

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,28 +1,32 @@
 import UIKit
 
-
-enum ComparisonResult: Int {
-    case orderedAscending // 0
-    case orderedSame // 1
-    case orderedDescending // 2
-}
-
-
-enum Title: String {
-    case mr = "Mr"
-    case mrs = "Mrs"
-    case mister = "Mister"
-    case miss = "Miss"
-    case dr = "Dr"
-    case prof = "Prof"
-    case other
-    
-    var isProfesstional: Bool {
-        return self == Title.dr || self == Title.prof
+struct PersonName {
+    let givenName: String
+    let middleName: String
+    var familyName: String
+     
+    func fullName() -> String {
+        return "\(givenName) \(middleName) \(familyName)"
+    }
+     
+    mutating func change(familyName: String) {
+        self.familyName = familyName
     }
 }
 
-let title1 = Title.mr
-
-print(title1.isProfesstional) // false
-
+class Person {
+     
+    let birthName: PersonName
+    var currentName: PersonName
+    var countryOfResidence: String
+     
+    init(name: PersonName, countryOfResidence: String = "UK") {
+        birthName = name
+        currentName = name
+        self.countryOfResidence = countryOfResidence
+    }
+     
+    var displayString: String {
+        return "\(currentName.fullName()) - Location: \(countryOfResidence)"
+    }
+}
