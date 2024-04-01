3170e4f31e9d2a593760c065922007e8a2f4520d
Ruel <c0407h@gmail.com>
2024-04-01 16:34:48 +0900

    clouser

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -20,7 +20,7 @@ class Person {
     var currentName: PersonName
     var countryOfResidence: String
     
-    init(name: PersonName, countryOfResidence: String = "UK") {
+    init(name: PersonName, countryOfResidence: String = "KR") {
         birthName = name
         currentName = name
         self.countryOfResidence = countryOfResidence
@@ -34,8 +34,33 @@ class Person {
 
 
 let printAuthorsDetails: () -> Void = {
-    let name = PersonName(givenName: "Keith", middleName: "David", familyName: "Moon")
+    let name = PersonName(givenName: "Chunghyun", middleName: "Ruel", familyName: "Lee")
     let author = Person(name: name)
     print(author.displayString)
 }
+
 printAuthorsDetails()
+
+let createAuthor: () -> Person = {
+    let name = PersonName(givenName: "Keith", middleName: "David", familyName: "Moon")
+    let author = Person(name: name)
+    return author
+}
+let author = createAuthor()
+print(author.displayString)
+
+
+let printPersonsDetails: (String, String, String) -> Void = { given, middle, family in
+    let name = PersonName(givenName: given, middleName: middle, familyName: family)
+    let person = Person(name: name)
+    print(person.displayString)
+}
+printPersonsDetails("Kathleen", "Mary", "Moon")
+
+let createPerson: (String, String, String) -> Person = { given, middle, family in
+    let name = PersonName(givenName: given, middleName: middle, familyName: family)
+    let person = Person(name: name)
+    return person
+}
+let felix = createPerson("Felix", "Robert", "Moon")
+print(felix.displayString)
