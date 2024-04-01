ea84bb080679bdd732e3a13fc0fdb2f97f512082
YACHAE <104hsh76@gmail.com>
2024-04-01 13:26:27 +0900

    optional

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,37 +1,18 @@
 import UIKit
 
-var greeting = "Hello, playground"
+// 기본 변수
+var dayOfTheWeek = "Tuesday"
+// 기본타입이 String?이 아닌 String으로 선언되어서 nil할당 불가
+dayOfTheWeek = "Tuesday"
+dayOfTheWeek = "Wedenesday"
+//dayOfTheWeek = nil // 컴파일 오류 발생 (옵셔널로 선언되지 않아서)
 
-let phrase: String = "The quick brown fox jumps over the lazy dog"
+var numberOfFingerHeldByFinn: Int? // 값이 있을수도 없을수도 있다.
+numberOfFingerHeldByFinn = nil
+numberOfFingerHeldByFinn = 3
 
-greeting = "Modification"
+print(numberOfFingerHeldByFinn ?? "Unknown")
 
-let numberOfFoxes: Int = 1
-let numberOfAnimals: Int = 2
-
-let averageCharactersPerWord: Float = (3+5+5+3+5+4+3+4+3) / 9
-
-var anotherPhrase = phrase
-
-anotherPhrase = "Modification"
-
-var phraseInfo = "The phrase" + " has: "
-print(phraseInfo)
-
-phraseInfo = phraseInfo + "\(numberOfFoxes) fox and \(numberOfAnimals) animals"
-
-print(phraseInfo)
-
-print("Number of characters in phrase: \(phrase.count)")
-
-let multilineExplanation = """
-Why is the following phrase often used?
-"the quick brown fox jumps over the lazy dog"
-This phrase contains every letter in the alphabet.
-"""
-
-let phrasesAreEqual = phrase == anotherPhrase // 참과 거짓
-print(phrasesAreEqual)
-
-let phraseHas43Characters = phrase.count == 40 + 3 // 참과 거짓
-print(phraseHas43Characters)
+let lastNumberOfFingersHeldUpByFinn: Int = numberOfFingerHeldByFinn!
+// 위험할 수 있음❗️
+print(lastNumberOfFingersHeldUpByFinn)
