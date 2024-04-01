f1a66dbc056cd95eca346fce70a47a2a8b6b0017
Jin Park <jinpark1504@gmail.com>
2024-04-01 16:37:05 +0900

    closer

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,24 +1,64 @@
-enum  ComparisonResult : Int {
-    case orderedAscending // 0
-    case orderedSame // 1
-    case orderedDescending // 2
-}
-
-enum Title: String {
-    case mr = "Mr"
-    case mrs = "Mrs"
-    case mister = "Mister"
-    case miss = "Miss"
-    case dr = "Dr"
-    case prof = "Prof"
-    case other
-    
-    
-    var isProfessional : Bool {
-        return self == Title.dr || self == Title.prof
+struct PersonName {
+    let givenName: String
+    let middleName: String
+    var familyName: String
+     
+    func fullName() -> String {
+        return "\(givenName) \(middleName) \(familyName)"
+    }
+     
+    mutating func change(familyName: String) {
+        self.familyName = familyName
     }
 }
 
-let title1 = Title.mr
+class Person {
+    let birthName: PersonName
+    var currentName: PersonName
+    var countryOfResidence: String
+     
+    init(name: PersonName, countryOfResidence: String = "UK") {
+        birthName = name
+        currentName = name
+        self.countryOfResidence = countryOfResidence
+    }
+     
+    var displayString: String {
+        return "\(currentName.fullName()) - Location: \(countryOfResidence)"
+    }
+}
+// closer
+//let printAuthorsDetails: () -> Void = {
+let printAuthorsDetails: () -> () = {
+    let name = PersonName(givenName: "Jin", middleName: "J.", familyName: "Park")
+    let author = Person(name: name)
+    print(author.displayString)
+}
 
-print(title1.isProfessional)
+printAuthorsDetails()
+
+let createAuthor: () -> Person = {
+    let name = PersonName(givenName: "Keith", middleName: "David", familyName: "Moon")
+    let author = Person(name: name)
+    return author
+}
+
+let author = createAuthor()
+print(author.displayString)
+
+let printPersonDetails: (String, String, String) -> Void = { given, middle, family in
+    let name = PersonName(givenName: given, middleName: middle, familyName: family)
+    let person = Person(name: name)
+    print(person.displayString)
+}
+
+printPersonDetails("Kathleen", "Mary", "Moon")
+
+let createPerson: (String, String, String) -> Person = { given, middle, family in
+    let name = PersonName(givenName: given, middleName: middle, familyName: family)
+    let person = Person(name: name)
+    return person
+}
+
+let felix = createPerson("Felix", "Robert", "Moon")
+print(felix.displayString)
