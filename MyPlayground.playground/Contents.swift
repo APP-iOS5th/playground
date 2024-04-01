003cf7f40ca8159c5078cb7e3096857cfdbfbf43
jinwoong <baseball0020@gmail.com>
2024-04-01 13:25:34 +0900

    functions

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,3 +1,9 @@
 import UIKit
 
-var greeting = "Hello, playground"
+func fullName(givenName: String, middleName: String, familyName: String) -> String {
+    return "\(givenName) \(middleName) \(familyName)"
+}
+
+let myFullName = fullName(givenName: "Keith", middleName: "David", familyName: "Moon");
+
+print(myFullName)
