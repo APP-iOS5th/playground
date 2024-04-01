0d58c4b18e199a0fb55bfddc722f601fb492b7ad
jmbae <jmbae@codegrove.co.kr>
2024-04-01 16:37:16 +0900

    Closures

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -28,3 +28,46 @@ class Person {
         return "\(currentName.fullName()) - Location: \(countryOfResidence)"
     }
 }
+
+
+let printAuthorDetails: () -> Void = {
+    let name = PersonName(givenName: "Jungman", middleName: "Dan", familyName: "Bae")
+    let author = Person(name: name)
+    print(author.displayString)
+}
+
+printAuthorDetails()
+
+
+let createAuthor: () -> Person = {
+    let name = PersonName(givenName: "Keith",
+                          middleName: "David",
+                          familyName: "Moon")
+    let author = Person(name: name)
+    return author
+}
+let author = createAuthor()
+print(author.displayString)
+
+
+// String inputs, no output
+let printPersonsDetails: (String, String, String) -> Void = { given,
+  middle, family in
+    let name = PersonName(givenName: given,
+                          middleName: middle,
+                          familyName: family)
+    let author = Person(name: name)
+    print(author.displayString)
+}
+printPersonsDetails("Kathleen", "Mary", "Moon")
+
+
+let createPerson: (String, String, String) -> Person = { given, middle, family in
+    let name = PersonName(givenName: given,
+                          middleName: middle,
+                          familyName: family)
+    let person = Person(name: name)
+    return person
+}
+let felix = createPerson("Felix", "Robert", "Moon")
+print(felix.displayString)
