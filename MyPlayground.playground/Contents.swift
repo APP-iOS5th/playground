2f46d5e06523a8f57b8e286814933cab658cbdf5
jeongwookim <112370814+JeongWo@users.noreply.github.com>
2024-04-01 17:13:05 +0900

    Tuples

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -248,3 +248,12 @@ class Person: Saveable {
         saveHandler?(success)
     }
 }
+
+func normalizedStarRating(forRating rating: Float, ofPossibleTotal total: Float) -> (Int, String) {
+    let fraction = rating / total
+    let ratingOutOf5 = fraction * 5
+    let roundedRating = round(ratingOutOf5)
+    let numberOfStars = Int(roundedRating)
+    let ratingString = "\(numberOfStars) Star Movie"
+    return (numberOfStars,ratingString)
+}
