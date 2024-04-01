e87a045abfaa502e972d40cfdd065773f5c1ccf2
Seonghoon Jung <seonghoonj@icloud.com>
2024-04-01 15:22:48 +0900

    Class

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -2,94 +2,65 @@ import UIKit
 
 var greeting = "Hello, playground"
 
-let phrase: String = "The quick brown fox jumps over the lazy dog"
 
-greeting = "Modification"
+class Person {
+    let givenName: String
+    let middleName: String
+    let familyName: String
+    var countryOfResidence: String = "KR"
+    
+    init(givenName: String, middleName: String, familyName: String) {
+        self.givenName = givenName
+        self.middleName = middleName
+        self.familyName = familyName
+    }
+    
+    func fullName() -> String {
+        return "\(givenName) \(middleName) \(familyName)"
+    }
+    
+    var displayString: String {
+        return "\(self.fullName()) -  Location: \(self.countryOfResidence)"
+    }
 
-let numberOfFoxes: Int = 1
-let numberOfAnimals: Int = 2
-
-let averageCharactersPerWord: Float = (3+5+5+3+5+4+3+4+3) / 9
-
-var anotherPhrase = phrase
-
-anotherPhrase = "Modification"
-
-var phraseInfo = "The phrase" + " has: "
-print(phraseInfo)
-
-phraseInfo = phraseInfo + "\(numberOfFoxes) fox and \(numberOfAnimals) animals"
-print(phraseInfo)
-
-print("Number of characters in phrase: \(phrase.count)")
-
-let multilineExplanation = """
-Why is the following phrase often used?
-"The quick brown fox jumps over the lazy dog"
-This phrase contains every letter in the alphabet.
-"""
-
-let pharseAreEqual = phrase == anotherPhrase
-print(pharseAreEqual)
-
-let phraseHas43Characters = phrase.count == 40 + 3
-print(phraseHas43Characters)
-
-
-
-var dayOfTheWeek: String = "Monday"
-dayOfTheWeek = "Tuesday"
-dayOfTheWeek = "Wednesday"
-//dayOfTheWeek = nil
-
-
-var numberOfFingersHeldUpByFinn: Int?
-numberOfFingersHeldUpByFinn = nil
-numberOfFingersHeldUpByFinn = 3
-
-print(numberOfFingersHeldUpByFinn ?? "Unknown")
-
-
-let lastNumberOfFingersHeldUpByFinn: Int = numberOfFingersHeldUpByFinn!
-print(lastNumberOfFingersHeldUpByFinn)
-
-var legalName: String!
-legalName = nil
-legalName = "Alissa Jones"
-
-
-
-func fullName(givenName: String,
-middleName: String,
-familyName: String) -> String {
-return "\(givenName) \(middleName) \(familyName)"
 }
 
-let myFullName = fullName(givenName: "Keith", middleName: "David", familyName: "Moon")
-print(myFullName)
+var person = Person(givenName: "Seonghoon", middleName: "Ben", familyName: "Jung")
 
-
-func fullName2(givenName: String,
-middleName: String = "Unknown",
-familyName: String) -> String {
-return "\(givenName) \(middleName) \(familyName)"
+final class Family: Person {
+    let relationship: String
+    
+    init(givenName: String,
+         middleName: String,
+         familyName: String,
+         relationship: String) {
+        self.relationship = relationship
+        super.init(givenName: givenName, middleName: middleName, familyName: familyName)
+    }
 }
 
-let myFullName2 = fullName2(givenName: "Keith", familyName: "Moon")
-print(myFullName2)
+var family = Family(givenName: "Seonghoon", middleName: "Ben", familyName: "Jung", relationship: "Son")
 
 
 
-func combine(_ givenName: String, _ familyName: String) -> String {
-    return "\(givenName) \(familyName)"
+class MovieReview {
+    let movieTitle: String
+    var starRating: Int
+    init(movieTitle: String, starRating: Int) {
+        self.movieTitle = movieTitle
+        self.starRating = starRating
+    }
 }
 
-func combine(_ integer1: Int, _ integer2: Int) -> Int {
-    return integer1 + integer2
-}
+let shawshankReviewOnYourWebsite = MovieReview(movieTitle: "Shawshank Redemption", starRating: 3)
 
-let combinedString = combine("Finnley", "Moon")
-let combinedInt = combine(5, 10)
-print(combinedString)
-print(combinedInt)
+let referenceToReviewOnTwitter = shawshankReviewOnYourWebsite
+let referenceToReviewOnFacebook = shawshankReviewOnYourWebsite
+print(referenceToReviewOnTwitter.starRating)
+print(referenceToReviewOnFacebook.starRating)
+
+shawshankReviewOnYourWebsite.starRating = 5
+
+print(referenceToReviewOnTwitter.starRating)
+print(referenceToReviewOnFacebook.starRating)
 
