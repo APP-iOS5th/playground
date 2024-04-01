7b5177d88052495acb33feb684a3a51b2af1ab68
kyhlsd <kmyghn@gmail.com>
2024-04-01 13:46:53 +0900

    Parameter Overload

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,7 +1,13 @@
-func fullName(givenName: String, middleName: String = "Unknown", familyName: String) -> String {
-    return "\(givenName) \(middleName) \(familyName)"
+func combine(_ givenName: String, _ familyName: String) -> String{
+    return "\(givenName) \(familyName)"
 }
 
-let myFullName = fullName(givenName: "Keith", middleName: "David", familyName: "Moon")
-print(myFullName)
+func combine(_ integer1: Int, _ integer2: Int) -> Int {
+    return integer1 + integer2
+}
 
+let combinedString = combine("Finnley","Moon")
+let combinedInt = combine(5,10)
+
+print(combinedString)
+print(combinedInt)
