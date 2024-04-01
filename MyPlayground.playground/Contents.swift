279f6bfc2a91c2da40da5219ce300bff3ec26995
Lee Inho <inhoking@kookmin.ac.kr>
2024-04-01 13:46:46 +0900

    function parameter overloading

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,8 +1,14 @@
 
-func fullName(givenName: String, middleName: String = "Default", familyName: String) -> String {
-    return "\(givenName) \(middleName) \(familyName)"
+func combine(_ givenName: String, _ familyName: String) -> String {
+    return "\(givenName) \(familyName)"
 }
 
-let myFullName = fullName(givenName: "Inho", familyName: "Lee");
+func combine(_ integer1: Int, _ integer2: Int) -> Int {
+    return integer1 + integer2
+}
 
-print(myFullName)
+let combinedString = combine("Finnley", "Moon")
+let combinedInt = combine(5, 10)
+
+print(combinedString)
+print(combinedInt)
