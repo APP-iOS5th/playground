f6ad5965fd261fcbd3890da704f9de0e1fba4189
Jongchan Yun <jcyun1996@gmail.com>
2024-04-01 14:25:12 +0900

    Class

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,9 +1,22 @@
-import UIKit
 
-func fullName(givenName: String, middleName: String, familyName: String) -> String {
-    return "\(givenName) \(middleName) \(familyName)"
+class Person {
+    let givenName: String
+    let middleName: String
+    let familyName: String
+    var countryOfResidence: String = "KR"
+    
+    init(givenName: String, middleName: String, familyName: String, countryOfResidence: String) {
+        self.givenName = givenName
+        self.middleName = middleName
+        self.familyName = familyName
+    }
+    
+    func fullName() -> String {
+        return "\(givenName) \(middleName) \(familyName)"
+    }
+    
+    var displayString: String {
+        return "\(self.fullName()) - Location: \(self.countryOfResidence)"
+    }
+    
 }
-
-let myFullName = fullName(givenName: "Keith", middleName: "David", familyName: "Moon");
-
-print(myFullName)
