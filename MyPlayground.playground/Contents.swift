dacffe7e7a0652d2ce63a31f8088c15b9a5d5627
Jongchan Yun <jcyun1996@gmail.com>
2024-04-01 13:27:23 +0900

    Optional

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,36 +1,12 @@
 import UIKit
 
-var greeting = "Hello, playground"
+var dayOfTheWeek: String = "Monday"
+dayOfTheWeek = "Tuesday"
+dayOfTheWeek = "Wednesday"
+//dayOfTheWeek = nil // 컴파일 오류 발생
 
-let phrase: String = "The quick brown fox jumps over the lazy dog"
+var numberOfFingersHeldUpByFinn: Int?
+numberOfFingersHeldUpByFinn = nil
+//numberOfFingersHeldUpByFinn = 3
 
-greeting = "Modification"
-
-let numberOfFoxes: Int  = 1
-let numberOfAnimals: Int  = 2
-
-let averageChractersPerWord: Float = (3+5+5+3+5+4+3+4+3) / 9
-
-var anotherPhrase = phrase
-
-anotherPhrase = "Modification"
-
-var phraseInfo = "The phrase" + " has: "
-print(phraseInfo)
-
-phraseInfo = phraseInfo + "\(numberOfFoxes) fox and \(numberOfAnimals) animals"
-print(phraseInfo)
-
-print("Number of chracters in phrase: \(phrase.count)")
-
-let multilineExplanation = """
-why is the following phrase often used?
-"The quick brown fox jumps over the lazy dog"
-This phrase contains every letter in the alphabet.
-"""
-
-let phrasesAreEqual = phrase == anotherPhrase
-print(phrasesAreEqual)
-
-let phraseHas43Characters = phrase.count == 40 + 3
-print(phraseHas43Characters)
+print(numberOfFingersHeldUpByFinn ?? "Unknown")
