651d31de97f58724f0d89fceb6919b194784cfc6
Jin Park <jinpark1504@gmail.com>
2024-04-01 13:27:15 +0900

    optional

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,36 +1,14 @@
-import UIKit
+var dayOfTheWeek: String? = "Monday"
+dayOfTheWeek = "Tuesday"
+dayOfTheWeek = "Wednesday"
+dayOfTheWeek = nil
 
-var greeting = "Hello, playground"
+var numberOfFingersHeldUpByFinn: Int?
+numberOfFingersHeldUpByFinn = nil
+numberOfFingersHeldUpByFinn = 3
 
-let phrase: String = "The quick brown fox jumps over the lazy dog"
+print(numberOfFingersHeldUpByFinn)
 
-greeting = "Modificiation"
+let lastNumber: Int = numberOfFingersHeldUpByFinn!
 
-let numberOfFoxes: Int = 1
-let numberOfAnimals: Int = 2
-
-let averageCharactersPerWord: Float = (3+5+5+3+5+4+3+4+3) / 9
-
-var anotherPhrase = phrase
-
-anotherPhrase = "Modification"
-
-var phraseInfo = "The phrase" + " has: "
-print(phraseInfo)
-
-phraseInfo = phraseInfo + "\(numberOfFoxes) fox and \(numberOfAnimals) animals"
-print(phraseInfo)
-
-print("Number of characters in phrase: \(phrase.count)")
-
-let multilineExplanation: String = """
-This is mulitiline explanation
-This is the second line
-"""
-print(multilineExplanation)
-
-let phrasesAreEqual = phrase == anotherPhrase
-print(phrasesAreEqual)
-
-let phraseHas43Characters = phrase.count == 40 + 3
-print(phraseHas43Characters)
+print(lastNumber)
