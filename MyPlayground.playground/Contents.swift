79aa45d34c43563b4e6d74224d04935a386f53e4
MadCow <lobasketve@naver.com>
2024-04-01 13:36:52 +0900

    Default Parameter Value

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -3,9 +3,9 @@
 
 import UIKit
 
-func fullName(givenName: String, middleName: String, familyName: String) -> String {
+func fullName(givenName: String, middleName: String = "initial middleName", familyName: String) -> String {
     return "\(givenName) \(middleName) \(familyName)"
 }
 
-let myFullName: String = fullName(givenName: "KwangWoo", middleName: "MadCow", familyName: "Choi")
+let myFullName: String = fullName(givenName: "KwangWoo", familyName: "Choi")
 print(myFullName)
