dba1907782e8468851eb7b1091e1151cc2685166
rainbow_an <eventscripts@nate.com>
2024-04-01 17:12:39 +0900

    Tuples

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,27 +1,12 @@
+import Foundation
 
-protocol Saveable {
-    var saveNeeded: Bool { get set}
-    func saveToRemoteDatabase(handler: @escaping (Bool) -> Void)
+func normalizedStarRating(forRating rating: Float, ofPossibleTotal total: Float) -> (Int, String) {
+    
+    let fraction = rating / total
+    let ratingOutOf5 = fraction * 5
+    let roundedRating = round(ratingOutOf5)
+    let numberOfStars = Int(roundedRating)
+    let ratingString = "\(numberOfStars) Star Movie"
+    return (numberOfStars,ratingString)
+    
 }
-
-class Person: Saveable {
-    //....
-    var saveHandler: ((Bool) -> Void)?
-    var saveNeeded: Bool = true
-     
-    func saveToRemoteDatabase(handler: @escaping (Bool) -> Void) {
-         saveHandler = handler
-         // Send person information to remove database
-         // Once remote save is complete, it calls
-           // saveComplete(Bool)
-         // We'll fake it for the moment, and assume the save is
-           // complete.
-         saveComplete(success: true)
-    }
-     
-    func saveComplete(success: Bool) {
-        saveHandler?(success)
-    }
-}
-
-
