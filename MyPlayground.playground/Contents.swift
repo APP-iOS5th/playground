3ffdebf0b80871c02973095bae7d1182ba1ebd49
sohee <danwater86@gmail.com>
2024-04-01 13:37:27 +0900

    Default Parameter Value

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,3 +1,8 @@
-import UIKit
+func fullName(givenName: String, middleName:String, familyName:String) -> String {
+    return "\(givenName) \(middleName) \(familyName)"
+}
 
-var greeting = "Hello, playground"
+let myFullName = fullName(givenName: "Keith", middleName: "David", familyName: "Moon");
+
+print(myFullName)
+
