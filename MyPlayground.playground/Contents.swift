19300eec78d60c39dfbed528e330edc5ca4635cf
MadCow <lobasketve@naver.com>
2024-04-01 16:00:30 +0900

    struct

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -3,28 +3,33 @@
 
 import UIKit
 
-enum ComparisionResult: Int {
-    case orderedAscending   // 0
-    case orderedSame        // 1
-    case orderedDescending  // 2
-}
-
-enum Title: String {
-    case mr = "Mr"
-    case mrs = "Mrs"
-    case mister = "Mister"
-    case miss = "Miss"
-    case dr = "Dr"
-    case prof = "Prof"
-    case other
-    
-    var isProfessor: Bool {
-        return self == Title.dr || self == Title.prof
+struct PersonName {
+    let givenName: String
+    let middleName: String
+    var familyName: String
+     
+    func fullName() -> String {
+        return "\(givenName) \(middleName) \(familyName)"
+    }
+     
+    mutating func change(familyName: String) {
+        self.familyName = familyName
     }
 }
 
-// Title을 생성하면서 title은 Title의 prof를 가지게된다
-let title1 = Title.prof
-
-// 그래서 isProfessor의 self는 prof가 된다.
-print(title1.isProfessor)
+class Person {
+     
+    let birthName: PersonName
+    var currentName: PersonName
+    var countryOfResidence: String
+     
+    init(name: PersonName, countryOfResidence: String = "UK") {
+        birthName = name
+        currentName = name
+        self.countryOfResidence = countryOfResidence
+    }
+     
+    var displayString: String {
+        return "\(currentName.fullName()) - Location: \(countryOfResidence)"
+    }
+}
