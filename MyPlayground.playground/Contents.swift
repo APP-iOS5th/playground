944bbdc72c4686931bc49a35e5c9f5c7079d6e10
avan31017 <avan31017@gmail.com>
2024-04-01 13:26:19 +0900

    Function Example

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,3 +1,9 @@
 import UIKit
 
-var greeting = "Hello, playground"
+func fullName(_ givenName: String, _ middleName: String, _ familyName: String) -> String {
+    let fullName = "\(givenName) \(middleName) \(familyName)"
+    
+    return fullName
+}
+
+print(fullName("조", "성", "빈"))
