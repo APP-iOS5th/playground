af95ad0fa99d8c9abf1dcab21474e1da6d064a90
youngwoo_ahn <codeway119@gmail.com>
2024-04-01 13:27:07 +0900

    함수1

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,3 +1,98 @@
 import UIKit
 
+// var : 변수
 var greeting = "Hello, playground"
+greeting = "Modification"
+
+// let : 상수 -> 값 변경시 에러
+let phrase: String = "The quick brown for jumps voer the lazy dog"
+// phrase = "Modification"
+
+let numberOfFoxes: Int = 1
+let numberOfAnimal: Int = 2
+
+let averageCharactersPerWord: Float = (3+5+9+2) / 9
+
+var anotherPhrase = phrase
+anotherPhrase = "Modification"
+
+var phraseInfo = "The phrase" + "has : "
+print(phraseInfo)
+
+phraseInfo = phraseInfo + "\(numberOfFoxes) fox and \(numberOfAnimal) animals"
+print(phraseInfo)
+
+print("Number of character in phrase: \(phrase.count)")
+
+let multilineExplantion = 
+"""
+--MultiLine--
+TEST1
+TEST2 "큰따움표"
+TEST3
+--MultiLine--
+"""
+print(multilineExplantion)
+
+let phraseAreEqual = phrase == anotherPhrase
+print(phraseAreEqual) // false
+
+let phraseHas43Charaters = phrase.count == 40+3
+print(phraseHas43Charaters) // true
+
+// -------------------- //
+
+var dayOfWeek: String = "Monday"
+// dayOfWeek = nil
+
+var numverOfFinn: Int?
+numverOfFinn = nil
+numverOfFinn = 3
+
+print(numverOfFinn) // Optional 객체 리턴
+print(numverOfFinn ?? "Unknown") // 값 출력 or nil일 경우 Unkown 출력
+
+let lastNumOfFinUpFinn: Int = numverOfFinn! // 위험! -> 강제 unrwap
+print(lastNumOfFinUpFinn)
+
+
+// ---------- //
+
+func fullName(givenName: String, middleName : String, familyName: String) -> String {
+    return "\(givenName) \(middleName) \(familyName) !"
+}
+let myFullName = fullName(givenName: "A", middleName: "B", familyName: "C")
+print(myFullName)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
