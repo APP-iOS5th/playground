7e681e03c4435719f8cef718feac0709efd4ede2
Seonghoon Jung <seonghoonj@icloud.com>
2024-04-01 16:38:16 +0900

    Closure

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -3,64 +3,65 @@ import UIKit
 var greeting = "Hello, playground"
 
 
-class Person {
+struct PersonName {
     let givenName: String
     let middleName: String
-    let familyName: String
-    var countryOfResidence: String = "KR"
-    
-    init(givenName: String, middleName: String, familyName: String) {
-        self.givenName = givenName
-        self.middleName = middleName
-        self.familyName = familyName
-    }
-    
+    var familyName: String
+     
     func fullName() -> String {
         return "\(givenName) \(middleName) \(familyName)"
     }
-    
+     
+    mutating func change(familyName: String) {
+        self.familyName = familyName
+    }
+}
+
+class Person {
+    let birthName: PersonName
+    var currentName: PersonName
+    var countryOfResidence: String
+     
+    init(name: PersonName, countryOfResidence: String = "UK") {
+        birthName = name
+        currentName = name
+        self.countryOfResidence = countryOfResidence
+    }
+     
     var displayString: String {
-        return "\(self.fullName()) -  Location: \(self.countryOfResidence)"
-    }
-
-}
-
-var person = Person(givenName: "Seonghoon", middleName: "Ben", familyName: "Jung")
-
-final class Family: Person {
-    let relationship: String
-    
-    init(givenName: String,
-         middleName: String,
-         familyName: String,
-         relationship: String) {
-        self.relationship = relationship
-        super.init(givenName: givenName, middleName: middleName, familyName: familyName)
+        return "\(currentName.fullName()) - Location: \(countryOfResidence)"
     }
 }
 
-var family = Family(givenName: "Seonghoon", middleName: "Ben", familyName: "Jung", relationship: "Son")
 
-
-
-class MovieReview {
-    let movieTitle: String
-    var starRating: Int
-    init(movieTitle: String, starRating: Int) {
-        self.movieTitle = movieTitle
-        self.starRating = starRating
-    }
+let printAuthorDetails: () -> Void = {
+    let name = PersonName(givenName: "Jungman", middleName: "Dan", familyName: "Bae")
+    let author = Person(name: name)
+    print(author.displayString)
 }
 
-let shawshankReviewOnYourWebsite = MovieReview(movieTitle: "Shawshank Redemption", starRating: 3)
+printAuthorDetails()
 
-let referenceToReviewOnTwitter = shawshankReviewOnYourWebsite
-let referenceToReviewOnFacebook = shawshankReviewOnYourWebsite
-print(referenceToReviewOnTwitter.starRating)
-print(referenceToReviewOnFacebook.starRating)
 
-shawshankReviewOnYourWebsite.starRating = 5
+let createAuthor: () -> Person = {
+    let name = PersonName(givenName: "Keith",
+                          middleName: "David",
+                          familyName: "Moon")
+    let author = Person(name: name)
+    return author
+}
+let author = createAuthor()
+print(author.displayString)
 
-print(referenceToReviewOnTwitter.starRating)
-print(referenceToReviewOnFacebook.starRating)
+
+let createPerson: (String, String, String) -> Person = { given,
+  middle, family in
+    let name = PersonName(givenName: given,
+                          middleName: middle,
+                          familyName: family)
+    let person = Person(name: name)
+    return person
+}
+let felix = createPerson("Felix", "Robert", "Moon")
+print(felix.displayString)
 
