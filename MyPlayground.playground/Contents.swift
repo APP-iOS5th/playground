edf458cfa5e0dbdea04818c2d4918d82bc186353
jeongwookim <112370814+JeongWo@users.noreply.github.com>
2024-04-01 15:06:01 +0900

    class

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -74,3 +74,40 @@ let combinedInt = combine(5, 10)
 
 print(combinedString)
 print(combinedInt)
+
+
+class Person {
+    let givenName: String
+    let middleName: String
+    let familyName: String
+    var countryOfResidence: String = "KR"
+    
+    init(givenName: String, middleName: String, familyName: String) {
+        self.givenName = givenName
+        self.middleName = middleName
+        self.familyName = familyName
+    }
+    
+    func fullName() -> String {
+    return "\(givenName) \(middleName) \(familyName)"
+    }
+    
+    var displayString: String {
+        return "\(self.fullName()) -Location: \(self.countryOfResidence)"
+    }
+}
+var person = Person(givenName: "Jeong", middleName: "Woo", familyName: "Kim")
+
+final class Family: Person {
+    let  relationship: String
+    
+    init(givenName: String,
+         middleName: String,
+         familyName: String,
+         relationship: String) {
+        self.relationship = relationship
+        super.init(givenName: givenName, middleName: middleName, familyName: familyName)
+    }
+}
+
+var family = Family(givenName: "Jeong", middleName: "Woo", familyName: "Kim", relationship: "Man")
