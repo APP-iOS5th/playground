43df0a174ea16627af2c3c779d79c9ebfd288a95
YACHAE <104hsh76@gmail.com>
2024-04-01 13:47:54 +0900

    overloading

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,9 +1,15 @@
 import UIKit
 
-func fullName(givenName: String, middleName: String = "Unknown", familyName: String) -> String {
-    return "\(givenName) \(middleName) \(familyName) !"
+func combine(_ givenName: String, _ familyName: String) -> String {
+    return "\(givenName) \(familyName)"
 }
 
-let myFullName = fullName(givenName: "keith",  familyName: "Moon")
+func combine(_ integer1: Int, _ integer2: Int) -> Int {
+    return integer1 + integer2
+}
 
-print(myFullName)
+let combinedString = combine("Finnley", "Moon")
+let combinedInt = combine(5, 10)
+print(combinedString) // Finnley Moon
+print(combinedInt) // 15
+
