66a23c6aee83d2034d9440ca5a903cbdef798140
SeongKookKIM <ksk8646@naver.com>
2024-04-01 15:36:07 +0900

    열거형

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -150,3 +150,31 @@ shawShankReviewOnYourWebsite.startRating = 5
 
 print(referenceToReviewOnTwitter.startRating)
 print(referenceToReviewOnFacebook.startRating)
+
+//열거형 (enum)
+
+enum ComparisonResult: Int {
+    case orderedAscending //0
+    case orderedSame //1
+    case orderedDescending //2
+    // .. 3, 4, 5
+}
+
+enum Title: String {
+    case mr = "Mr"
+    case mrs = "Mrs"
+    case mister = "Mister"
+    case miss = "Miss"
+    case dr = "Dr"
+    case prof = "Prof"
+    case other
+    
+    func isProfessioal() -> Bool {
+        return self == Title.dr || self == Title.prof
+    }
+
+}
+
+let title1 = Title.mr
+
+print(title1.isProfessioal())
