f4267d01db8926b328b9801a3e1ab5560f08ec9a
rainbow_an <eventscripts@nate.com>
2024-04-01 15:06:32 +0900

    Family,init,super,self

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -6,7 +6,7 @@ class Person {
     let familyName: String
     var countryOfResidence: String = "KR"
     
-    init(givenName: String, middleName: String, familyName: String, countryOfResidence: String) {
+    init(givenName: String, middleName: String, familyName: String) {
         self.givenName = givenName
         self.middleName = middleName
         self.familyName = familyName
@@ -23,4 +23,21 @@ class Person {
     
 }
 
-var person = Person(givenName: "Jungman", middleName: "Dan", familyName: "Bae", countryOfResidence: "KR")
+var person = Person(givenName: "Jungman", middleName: "Dan", familyName: "Bae")
+
+
+
+final class Family: Person {
+    let relationship: String
+    
+    init(givenName: String,
+         middleName: String,
+         familyName: String,
+         relationship: String) {
+        self.relationship = relationship
+        super.init(givenName: givenName, middleName: middleName, familyName: familyName)
+    }
+}
+
+
+var family = Family(givenName: "Jungman", middleName: "Dan", familyName: "Bae", relationship: "Dad")
