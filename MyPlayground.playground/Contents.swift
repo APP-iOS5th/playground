25230802b7a392084272bd408e44323c060a8943
Ruel <c0407h@gmail.com>
2024-04-01 14:25:58 +0900

    add Person class

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,32 +1,24 @@
-import UIKit
-
-var dayOfTheWeek: String = "Monday"
-dayOfTheWeek = "Tuesday"
-dayOfTheWeek = "Wednesday"
-//dayOfTheWeek = nil
-
-var numberOfFingersHeldUpByFinn: Int?
-numberOfFingersHeldUpByFinn = nil
-numberOfFingersHeldUpByFinn = 3
-print(numberOfFingersHeldUpByFinn ?? 0)
-
-func fullName(givenName: String,
-              middleName: String,
-              familyName: String) -> String {
-    return "\(givenName) \(middleName) \(familyName)"
+class Person {
+    let givenName: String
+    let middleName: String
+    let familyName: String
+    var countryOfResidence: String = "UK"
+    
+    init(givenName: String, middleName: String, familyName: String) {
+        self.givenName = givenName
+        self.middleName = middleName
+        self.familyName = familyName
+    }
+    
+    var displayString: String {
+        return "\(self.fullName()) - Location: \(countryOfResidence)"
+    }
+    
+    
+    func fullName() -> String {
+        return "\(givenName) \(middleName) \(familyName)"
+    }
+    
 }
-fullName(givenName: "충", middleName: "현", familyName: "이")
 
-let myFullName = fullName(givenName: "이", middleName: "충", familyName: "현")
-print(myFullName)
 
-func combine(_ givenName: String, _ familyName: String) -> String {
-    return "\(givenName) \(familyName)"
-}
-func combine(_ integer1: Int, _ integer2: Int) -> Int {
-    return integer1 + integer2
-}
-let combinedString = combine("Finnley", "Moon")
-let combinedInt = combine(5, 10)
-print(combinedString) // Finnley Moon
-print(combinedInt) // 15
