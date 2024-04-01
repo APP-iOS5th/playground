e96e2d0b879a8dbc2b5d0c77d201cd7053673efe
khyerim <ax1021@naver.com>
2024-04-01 16:17:30 +0900

    클로저

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -79,58 +79,151 @@ import UIKit
 
 //함수 이름이 같아도, 안에 들어있는 타입에 따라 알아서 구분
 //매개변수앞의 _는 매개변수 이름을 생략하겠다는 뜻
-func combine(givenName: String, familyName: String) -> String{
-    return "\(givenName) \(familyName)"
-}
-func combine(_ integer1: Int, _ integer2: Int) -> Int{
-    return integer1 + integer2
-}
+//func combine(givenName: String, familyName: String) -> String{
+//    return "\(givenName) \(familyName)"
+//}
+//func combine(_ integer1: Int, _ integer2: Int) -> Int{
+//    return integer1 + integer2
+//}
+//
+////앞에 매개변수이름 생략 불가능
+//let combinedString = combine(givenName: "Finnley", familyName: "Moon")
+////앞에 매개변수이름 생략 가능
+//let combinedInt = combine(/*integer1:*/ 5, /*integer2:*/ 10)
+//
+//print(combinedString)
+//print(combinedInt)
+//
+//class Person {
+//    let givenName: String
+//    let middleName: String
+//    let familyName: String
+//    var countryOfResidence: String = "KR"
+//    
+//    //self가 붙어있다는 것은 클래스가 가지고 있는 변수라는 의미
+//    //범위 한정자
+//    //self = 나 = class
+//    //init : 상수를 초기화 함수로 추가하는 것
+//    init(givenName: String, middleName: String, familyName: String) {
+//        self.givenName = givenName
+//        self.middleName = middleName
+//        self.familyName = familyName
+//    }
+//    
+//    func fullName() -> String {
+//        return "\(givenName) \(middleName) \(familyName)"
+//    }
+//    
+//    var displayString: String {
+//        return "\(fullName()) - Location: \(countryOfResidence)"
+//    }
+//}
+//
+//var person = Person(givenName: "Jungman", middleName: "Dan", familyName: "Bae")
+//
+////final class는 이제 더 상속받지 않겠다는 뜻
+////아래예시는 Family 클래스는 더 이상 상속 받지 않을테니, 이 안에 더 넣고 싶으면 내 자식인 Person을 통해서 추가해라, 라는 뜻
+//final class Family: Person {
+//    let relationship: String
+//    
+//    init(givenName: String,
+//         middleName: String,
+//         familyName: String,
+//         relationship: String) {
+//        //self는 나, super는 부모 상속자
+//        self.relationship = relationship
+//        super.init(givenName: givenName, middleName: middleName, familyName: familyName)
+//    }
+//}
 
-//앞에 매개변수이름 생략 불가능
-let combinedString = combine(givenName: "Finnley", familyName: "Moon")
-//앞에 매개변수이름 생략 가능
-let combinedInt = combine(/*integer1:*/ 5, /*integer2:*/ 10)
+//struct PersonName {
+//    let givenName: String
+//    let middleName: String
+//    var familyName: String
+//     
+//    func fullName() -> String {
+//        return "\(givenName) \(middleName) \(familyName)"
+//    }
+//     
+//    mutating func change(familyName: String) {
+//        self.familyName = familyName
+//    }
+//}
+//
+//class Person {
+//     
+//    let birthName: PersonName
+//    var currentName: PersonName
+//    var countryOfResidence: String
+//     
+//    init(name: PersonName, countryOfResidence: String = "UK") {
+//        birthName = name
+//        currentName = name
+//        self.countryOfResidence = countryOfResidence
+//    }
+//     
+//    var displayString: String {
+//        return "\(currentName.fullName()) - Location: \(countryOfResidence)"
+//    }
+//}
 
-print(combinedString)
-print(combinedInt)
+//
+//var family = Family(givenName: "Jungman", middleName: "Dan", familyName: "Bae", relationship: "Dad")
 
-class Person {
-    let givenName: String
-    let middleName: String
-    let familyName: String
-    var countryOfResidence: String = "KR"
-    
-    //self가 붙어있다는 것은 클래스가 가지고 있는 변수라는 의미
-    //범위 한정자
-    //self = 나 = class
-    init(givenName: String, middleName: String, familyName: String) {
-        self.givenName = givenName
-        self.middleName = middleName
-        self.familyName = familyName
-    }
-    
-    func fullName() -> String {
-        return "\(givenName) \(middleName) \(familyName)"
-    }
-    
-    var displayString: String {
-        return "\(fullName()) - Location: \(countryOfResidence)"
-    }
-}
+//무비리뷰는 영화이름과 별점으로 이루어진 클래스
+//class MovieReview {
+//    let movieTitle: String
+//    var starRating: Int
+//    
+//    //변수 초기화
+//    init(movieTitle: String, starRating: Int) {
+//        self.movieTitle = movieTitle
+//        self.starRating = starRating
+//    }
+//}
+//
+////리뷰 원본
+//let shawshankReviewonYourWebsite = MovieReview(movieTitle: "Shawshank Redmption", starRating: 3)
+//
+////리뷰를 소셜 미디어에 게시
+////복사해서 다른 미디어에 게시하는 방식
+//let referenceToReviewOnTwitter = shawshankReviewonYourWebsite //3
+//let referenceToReviewOnFacebook = shawshankReviewonYourWebsite //3
+//
+//print(referenceToReviewOnTwitter.starRating) //3
+//print(referenceToReviewOnFacebook.starRating) //3
+//
+////리뷰 수정
+//shawshankReviewonYourWebsite.starRating = 5
+//
+////수정 사항 자동 반영
+////이게 참조(reference 기능을 활용하는 이유)
+//print(referenceToReviewOnTwitter.starRating) //5
+//print(referenceToReviewOnFacebook.starRating) //5
 
-var person = Person(givenName: "Jungman", middleName: "Dan", familyName: "Bae")
-
-final class Family: Person {
-    let relationship: String
-    
-    init(givenName: String,
-         middleName: String,
-         familyName: String,
-         relationship: String) {
-        //self는 나, super는 부모 상속자
-        self.relationship = relationship
-        super.init(givenName: givenName, middleName: middleName, familyName: familyName)
-    }
-}
-
-var family = Family(givenName: "Jungman", middleName: "Dan", familyName: "Bae", relationship: "Dad")
+//enum ComparionResult: Int {
+//    case orderedAscending //0
+//    case orderedSame //1
+//    case orderedDescending //2
+//    //... 3, 4, 5
+//}
+//
+//
+////사람의 호칭을 나타내는 열거형 생성
+//enum Title: String {
+//    case mr = "Mr"
+//    case mrs = "Mrs"
+//    case master = "Master"
+//    case miss = "Miss"
+//    case dr = "Dr"
+//    case prof = "Prof"
+//    case other
+//    
+//    var isProfessional: Bool {
+//        return self == Title.dr || self == Title.prof
+//    }
+//}
+//
+//let title1 = Title.mr
+//
+//print(title1.isProfessional) // false
