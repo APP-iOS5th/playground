e5f5201507615f1ba2c54b76f814a73a198fd12f
hwang <jangku88@naver.com>
2024-04-01 16:00:59 +0900

    Struct, Class

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,29 +1,32 @@
 import UIKit
 
-// 열거형
-enum ComparisionResult: Int {
-    case orderedAscending // 0
-    case orderedSome // 1
-    case orderDescendin // 2
-    // ... 3, 4, 5
-}
-
-enum Title: String {
-    case mr = "Mr"
-    case mrs = "Mrs"
-    case mister = "Mister"
-    case miss = "Miss"
-    case dr = "Dr"
-    case prof = "Prof"
-    case other
-    
-    // () -> 이게 빠지면 29line의 isProfessional()의 ()도 빠진다. (빠진게 더 낫다고 함.)
-    func isProfessional() -> Bool {
-        return self == Title.dr || self == Title.prof
+struct PersonName {
+    let givenName: String
+    let middleName: String
+    var familyName: String
+     
+    func fullName() -> String {
+        return "\(givenName) \(middleName) \(familyName)"
+    }
+     
+    mutating func change(familyName: String) {
+        self.familyName = familyName
     }
 }
 
-// 로직을 이해하기 쉽고 간결하다는 장점이 있다.
-let title1 = Title.mr
-
-print(title1.isProfessional())
+class Person {
+     
+    let birthName: PersonName
+    var currentName: PersonName
+    var countryOfResidence: String
+     
+    init(name: PersonName, countryOfResidence: String = "UK") {
+        birthName = name
+        currentName = name
+        self.countryOfResidence = countryOfResidence
+    }
+     
+    var displayString: String {
+        return "\(currentName.fullName()) - Location: \(countryOfResidence)"
+    }
+}
