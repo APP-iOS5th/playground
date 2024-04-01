39eb1269c98cbf5ac67ee55c03fe799ab84ece40
kyhlsd <kmyghn@gmail.com>
2024-04-01 15:06:03 +0900

    Class

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,13 +1,35 @@
-func combine(_ givenName: String, _ familyName: String) -> String{
-    return "\(givenName) \(familyName)"
+class Person {
+    let givenName: String
+    let middleName: String
+    let familyName: String
+    var countryOfResidence: String = "KR"
+    
+    init(givenName: String, middleName: String, familyName: String){
+        self.givenName = givenName
+        self.middleName = middleName
+        self.familyName = familyName
+    }
+    
+    func fullName() -> String{
+        return "\(givenName) \(middleName) \(familyName)"
+    }
+    
+    var displayString: String {
+        return "\(self.fullName()) - Location: \(self.countryOfResidence)"
+    }
 }
 
-func combine(_ integer1: Int, _ integer2: Int) -> Int {
-    return integer1 + integer2
+var person = Person(givenName: "Jungman", middleName: "Dan", familyName: "Bae")
+
+final class Family: Person {
+    let relationship: String
+    
+    init(givenName: String,
+         middleName: String,
+         familyName: String,
+         relationship: String){
+        self.relationship = relationship
+        super.init(givenName: givenName, middleName: middleName, familyName: familyName)
+    }
 }
-
-let combinedString = combine("Finnley","Moon")
-let combinedInt = combine(5,10)
-
-print(combinedString)
-print(combinedInt)
+var family = Family(givenName: "Jungman", middleName: "Dan", familyName: "Bae",relationship: "Dad")
