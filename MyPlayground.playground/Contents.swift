37d2dbf1061d8b56b63b6e5be55c94351fca4724
Jin Park <jinpark1504@gmail.com>
2024-04-01 13:26:39 +0900

    variables

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,3 +1,36 @@
 import UIKit
 
 var greeting = "Hello, playground"
+
+let phrase: String = "The quick brown fox jumps over the lazy dog"
+
+greeting = "Modificiation"
+
+let numberOfFoxes: Int = 1
+let numberOfAnimals: Int = 2
+
+let averageCharactersPerWord: Float = (3+5+5+3+5+4+3+4+3) / 9
+
+var anotherPhrase = phrase
+
+anotherPhrase = "Modification"
+
+var phraseInfo = "The phrase" + " has: "
+print(phraseInfo)
+
+phraseInfo = phraseInfo + "\(numberOfFoxes) fox and \(numberOfAnimals) animals"
+print(phraseInfo)
+
+print("Number of characters in phrase: \(phrase.count)")
+
+let multilineExplanation: String = """
+This is mulitiline explanation
+This is the second line
+"""
+print(multilineExplanation)
+
+let phrasesAreEqual = phrase == anotherPhrase
+print(phrasesAreEqual)
+
+let phraseHas43Characters = phrase.count == 40 + 3
+print(phraseHas43Characters)
