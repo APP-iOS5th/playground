4c18e5df3b3d04f12d79b2ee3d8d4bc570927680
MadCow <lobasketve@naver.com>
2024-04-01 13:24:38 +0900

    Function

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,3 +1,11 @@
+// 최광우꺼
+// 2024-04-01
+
 import UIKit
 
-var greeting = "Hello, playground"
+func fullName(givenName: String, middleName: String, familyName: String) -> String {
+    return "\(givenName) \(middleName) \(familyName)"
+}
+
+let myFullName: String = fullName(givenName: "KwangWoo", middleName: "MadCow", familyName: "Choi")
+print(myFullName)
