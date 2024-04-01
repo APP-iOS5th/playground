458434b172ab82661e5a73397ebfa4b180829c94
Seunghye Hwang <ferrydraw@naver.com>
2024-04-01 15:08:09 +0900

    상속

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,3 +1,47 @@
-import UIKit
+//func combine(_ givenName: String, _ familyName: String) -> String {
+//    return "\(givenName) \(familyName)"
+//}
+//
+//func combine(_ integer1: Int, _ integer2: Int) -> Int {
+//    return integer1 + integer2
+//}
+//
+//let combinedString = combine("Finnley", "Moon")
+//let combinedInt = combine(5, 10)
+//
+//print(combinedString)
+//print(combinedInt)
 
-var greeting = "Hello, playground"
+class Person {
+    let givenName: String
+    let middleName: String
+    let familyName: String
+    var countryOfResidence: String = "KR"
+    
+    init(givenName: String, middleName: String, familyName: String) {
+        self.givenName = givenName
+        self.middleName = middleName
+        self.familyName = familyName
+    }
+    
+    func fullName() -> String {
+        return "\(givenName) \(middleName) \(familyName)"
+    }
+    
+    var displayString: String {
+        return "\(self.fullName()) - Location: \(countryOfResidence)"
+    }
+}
+
+var person = Person(givenName: "Jungnam", middleName: "Dan", familyName: "Bae")
+
+final class Family: Person {
+    let relationships: String
+    
+    init(givenName: String, middleName: String, familyName: String, relationships: String) {
+        self.relationships = relationships
+        super.init(givenName: givenName, middleName: middleName, familyName: familyName)
+    }
+}
+
+var family = Family(givenName: "Jungnam", middleName: "Dan", familyName: "Bae", relationships: "Dad")
