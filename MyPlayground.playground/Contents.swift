62a30a279fc3593f253325b7b4b128dd260e2825
Cha, Jiyong <wldyd2113@gmail.com>
2024-04-01 15:36:38 +0900

    Enumeration

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,26 +1,34 @@
 import UIKit
 
+//Chapter6
+//enum(열거형)
+enum ComprisonResult: Int{ //아무것도 주지않았을때 0부터 시작
+    case ordersAscending //0
+    case orderdSame //1
+    case orderDescending //2
+} //...3, 4, 5,
 
-//Chapter5
-//reference: 참조 타입, 참조의 장점 한부분에서 변경하면 여러부분에 적용
-class MovieReview{
-    let movieTitle: String
-    var startRating: Int
+enum Title: String {
+    case mr = "Mr"
+    case mrs = "Mrs"
+    case mister = "Mister"
+    case miss = "Miss"
+    case dr = "Dr"
+    case prof = "Prof"
+    case other
     
-    init(movieTitle: String, startRating: Int){
-        self.movieTitle = movieTitle
-        self.startRating = startRating
+    func isProfessional() -> Bool{
+        return self == Title.dr || self == Title.prof
     }
+    /*
+    var isProfessional: Bool{
+        return self == Title.dr || self == Title.prof
+    }
+     */
 }
 
-let shawshankReviewOnYourWebsite = MovieReview(movieTitle: "Shawshank Redemption", startRating: 3)
+let title1 = Title.mr
 
-let referenceToReviewOnTwitter = shawshankReviewOnYourWebsite
-let referenceToReviewOnFacebook = shawshankReviewOnYourWebsite
-
-shawshankReviewOnYourWebsite.startRating = 5
-
-print(referenceToReviewOnTwitter.startRating)
-print(referenceToReviewOnFacebook.startRating)
-
+print(title1.isProfessional())
+//print(title1.isProfessional)
 
