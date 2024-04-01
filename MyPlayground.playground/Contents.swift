ca27f2c44d824adce8cac3fa49394732e9093cb5
hwang <jangku88@naver.com>
2024-04-01 14:04:41 +0900

    Parameter Overloading

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,14 +1,20 @@
 import UIKit
 
-func combine(_ givenName: String, _ familyName: String) -> String {
-    return "\(givenName) \(familyName)"
+// _ : 와일드카드 패턴 (변수명, 레이블을 제외하고 사용할 수 있도록 해 줌.)
+// 매개변수 오버로드
+func combine (_ giveName: String, _ familyName: String) -> String {
+    return "\(giveName) \(familyName)"
 }
 
-func combine(_ integer1: Int, _ integer2: Int) -> Int {
+func combine (_ integer1: Int, _ integer2: Int) -> Int {
     return integer1 + integer2
 }
 
-let combinedString = combine("Finnley", "Moon")
-let combinedInt = combine(5, 10)
-print(combinedString) // Finnley Moon
-print(combinedInt) // 15
+// 파라미터 오버로딩
+// 와일드카드 패턴 혹은 오버로딩을 안 한다면: 
+// let comebinedString = combine(givenName: "Finnley", familyName: "Moon")
+let comebinedString = combine("Finnley", "Moon")
+let comebinedInt = combine(5, 10)
+
+print(comebinedString)
+print(comebinedInt)
