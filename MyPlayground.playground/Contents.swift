6038f580844827c73528b51ffa57d9b2162e5034
jeongwookim <112370814+JeongWo@users.noreply.github.com>
2024-04-01 15:36:23 +0900

    enumeration

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -133,3 +133,29 @@ shawshankReviewOnYourWebsite.starRating = 5
 
 print(referenceToReviewOnTwitter.starRating)
 print(referenceToReviewOnFacebook.starRating)
+
+
+enum ComparisonResult: Int {
+    case orderedAscending
+    case orderedSame
+    case orderedDescending
+}
+
+
+enum Title: String {
+    case mr = "Mr"
+    case mrs = "Mrs"
+    case mister = "Mister"
+    case miss = "Miss"
+    case dr = "Dr"
+    case prof = "Prof"
+    case other
+    
+    var isProfessional: Bool {
+        return self == Title.dr || self == Title.prof
+    }
+    
+}
+
+let title1 = Title.mr
+print(title1.isProfessional)
