cb128013bfb9925e84cb274a5402c8e6b7988d16
sohee <danwater86@gmail.com>
2024-04-02 11:34:05 +0900

    Basic Game Play

--- TicTacToe.playground/Contents.swift
+++ TicTacToe.playground/Contents.swift
@@ -16,23 +16,28 @@ struct TicTacToe {
     func gameStateString() -> String {
         var stateString = "-------------\n"
         stateString += printableString(forRow: gridStorage[0])
-        stateString = "-------------\n"
+        stateString += "-------------\n"
         stateString += printableString(forRow: gridStorage[1])
-        stateString = "-------------\n"
+        stateString += "-------------\n"
         stateString += printableString(forRow: gridStorage[2])
-        stateString = "-------------\n"
+        stateString += "-------------\n"
         
         return stateString
-
     }
     
     func printableString(forRow row: [GridPosition]) -> String {
         var rowString = "| \(row[0].rawValue) "
-        rowString = "| \(row[1].rawValue) "
-        rowString = "| \(row[2].rawValue) |\n"
+        rowString += "| \(row[1].rawValue) "
+        rowString += "| \(row[2].rawValue) |\n"
         return rowString
     }
 }
 
 var game = TicTacToe()
-print(gmae.gameStateString())
+print(game.gameStateString())
+
+game.gridStorage[1][1] = .player1
+print(game.gameStateString())
+
+game.gridStorage[0][2] = .player2
+print(game.gameStateString())
