c8fa18742fa880e759d515094fd7197afefa6db0
hwang <jangku88@naver.com>
2024-04-02 10:13:29 +0900

    Print HashValue and Compare

--- Hashable.playground/Contents.swift
+++ Hashable.playground/Contents.swift
@@ -11,17 +11,31 @@ struct Person: Hashable {
     }
     
     // 해시프로토콜의 비교연산자 ->  중복 체크 (이름과 나이로)
+//    static func == (lhs: Person, rhs: Person) -> Bool {
+//        return lhs.name == rhs.name && lhs.age == rhs.age
+//    }
+    
     static func == (lhs: Person, rhs: Person) -> Bool {
-        return lhs.name == rhs.name && lhs.age == rhs.age
+        return lhs.hashValue == rhs.hashValue
     }
+    
+    var hashValue: Int {
+        var hasher = Hasher()
+        self.hash(into: &hasher)
+        return hasher.finalize()
+    }
+
 }
+
 let person1 = Person(name: "Alice", age: 30)
 let person2 = Person(name: "Bob", age: 25)
-
-var peopleSet: Set<Person> = [person1, person2]
-
 let person3 = Person(name: "Alice", age: 20)
 
+print("person1 hash: ", person1.hashValue)
+print("person2 hash: ", person2.hashValue)
+print("person3 hash: ", person3.hashValue)
+
+var peopleSet: Set<Person> = [person1, person2]
 peopleSet.insert(person3)
 
 print(peopleSet.count)
