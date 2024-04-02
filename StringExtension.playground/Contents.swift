549a4139e72170b4e38d4fde2aa465d9dae1fa94
Jihye Park <deviloper.park@gmail.com>
2024-04-02 15:18:17 +0900

    Computed property

--- StringExtension.playground/Contents.swift
+++ StringExtension.playground/Contents.swift
@@ -1,12 +1,19 @@
 // 문자열 클래스 확장
 extension String {
-    func firstWord() -> String {
+    var firstWord: String {
         let spaceIndex = firstIndex(of: " ") ?? endIndex
         let word = prefix(upTo: spaceIndex)
         return String(word)
     }
+    
+//    func firstWord() -> String {
+//        let spaceIndex = firstIndex(of: " ") ?? endIndex
+//        let word = prefix(upTo: spaceIndex)
+//        return String(word)
+//    }
 }
 
 let llap = "Live long, and prosper"
-let firstWord = llap.firstWord()
+let firstWord = llap.firstWord
+//let firstWord = llap.firstWord()
 print(firstWord)
