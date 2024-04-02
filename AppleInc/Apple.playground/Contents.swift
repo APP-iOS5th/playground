b3127a23b43a2d5d474c868ad4c4fe805b833481
Jongchan Yun <jcyun1996@gmail.com>
2024-04-02 17:41:46 +0900

    Swift Language

---
+++ AppleInc/Apple.playground/Contents.swift
@@ -0,0 +1,32 @@
+public class Person {
+    public let name: String
+    
+    public init(name: String) {
+        self.name = name
+    }
+}
+
+public class Apple {
+    public private(set) var ceo: Person
+    private var employees = [Person]()
+    public let store = AppleStore()
+    private let secretDepartment = SecretProductDepartment()
+    
+    public init() {
+        ceo = Person(name: "Tim Cook")
+        employees.append(ceo)
+    }
+    
+    public func newEmployee(person: Person) {
+        employees.append(person)
+    }
+    
+    func weeklyProductMeeting() {
+        var superSecretProduct = secretDepartment.nextProduct(codeWord: "Not sure!")
+        
+        var superSecretProduct = secretDepartment.nextProduct(codeWord: "Titan")
+        
+        print(superSecretProduct as Any)
+        
+    }
+}
---
+++ AppleInc/SwiftLanguage.playground/Contents.swift
@@ -0,0 +1,9 @@
+open class SwiftLanguage {
+    open func versionNumber() -> Float {
+        return 5.0
+    }
+    
+    open func supportedPlatforms() -> [String] {
+        return ["iOS", "macOSX", "tvOS", "watchOS", "Linux"]
+    }
+}
