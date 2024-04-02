be0158ccb6a35b0d70ec3f87292633d4edbbe0d4
uunwon <chea2089@naver.com>
2024-04-02 15:41:35 +0900

    Protocol Extensions

--- IntRepresentable.playground/Contents.swift
+++ IntRepresentable.playground/Contents.swift
@@ -19,3 +19,28 @@ extension String: IntRepresentable {
 var ten = "10"
 var tenInt: Int = ten.intValue // 정수형 변환
 print(tenInt)
+
+enum CrewComplement: Int {
+    case enterpriseD = 1014
+    case voyager = 150
+    case deepSpaceNine = 2000
+}
+
+extension CrewComplement: IntRepresentable {
+    var intValue: Int {
+        return rawValue // case 에 오는 값을 리턴
+    }
+}
+
+var intableThings = [IntRepresentable]()
+intableThings.append(55)
+intableThings.append(1200)
+intableThings.append("5")
+intableThings.append("1009")
+intableThings.append(CrewComplement.enterpriseD)
+intableThings.append(CrewComplement.voyager)
+intableThings.append(CrewComplement.deepSpaceNine)
+
+// compactMap 은 forLoop 과 유사
+let over1000 = intableThings.compactMap{$0.intValue > 1000 ? $0.intValue: nil}
+print(over1000)
