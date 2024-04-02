4286c49b31bffc0c71999a15835051c3007e118e
Cha, Jiyong <wldyd2113@gmail.com>
2024-04-02 15:17:19 +0900

    computed property

--- StringExtension.playground/Contents.swift
+++ StringExtension.playground/Contents.swift
@@ -1,7 +1,6 @@
 
 extension String{
-    
-    func firstWord() -> String{
+    var firstWord:  String{
         let spaceIndex = firstIndex(of: " ") ?? endIndex //
         let word = prefix(upTo: spaceIndex)
         return String(word)
@@ -9,5 +8,5 @@ extension String{
 }
 
 let llap = "Live Long, and prosper"
-let firstWord = llap.firstWord()
+let firstWord = llap.firstWord
 print(firstWord)
