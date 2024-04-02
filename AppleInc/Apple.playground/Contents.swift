17905107276728f8b0742bd73464fb290a34febd
Ruel <c0407h@gmail.com>
2024-04-02 17:41:08 +0900

    open

---
+++ AppleInc/Apple.playground/Contents.swift
@@ -0,0 +1,37 @@
+import AppleInc
+
+public class Person {
+    public let name: String
+    
+    public init(name: String) {
+        self.name = name
+    }
+    
+}
+
+
+public class Apple {
+    //set 은 private한 것 -> ceo를 바꿀수 없음, 읽기는 가능
+    public private(set) var ceo: Person
+    private var employees = [Person]()
+    
+    public let store = AppleStore()
+    private let secretDepartment = SecretProductDepartment()
+    
+    public init() {
+        self.ceo = Person(name: "Tim Cook")
+        self.employees.append(self.ceo)
+    }
+    
+    public func newEmployee(person: Person) {
+        employees.append(person)
+    }
+    
+    func weeklyProductMeeting() {
+        var superSecretProduct = secretDepartment.nextProduct(codeWord: "Not sure!")
+        var sueprSecretPorduct = secretDepartment.nextProduct(codeWord: "Titan")
+        print(superSecretProduct as Any)
+    }
+
+    
+}
---
+++ AppleInc/Apple.playground/Sources/SwiftLanguage.playground/Contents.swift
@@ -0,0 +1,9 @@
+
+open class SwiftLanguage {
+    open func versionNumber() -> Float {
+        return 5.0
+    }
+    open func suppertedPlatforms() -> [String] {
+        return ["iOS", "macOS", "tvOS", "watchOS", "Linux"]
+    }
+}
