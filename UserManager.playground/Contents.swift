2618c8e0b13160f5c13f47ceacd632b212d40f66
rainbow_an <eventscripts@nate.com>
2024-04-02 14:30:47 +0900

    Property Observer

---
+++ UserManager.playground/Contents.swift
@@ -0,0 +1,20 @@
+class UserManager {
+    //관찰자(옵저버) Property Observer
+    var currentUserName: String = "Emmmanuel Goldstein" {
+        willSet(newUserName) {
+            print("Goodbye to \(currentUserName)")
+            print("I hear \(newUserName) is on their way!")
+        }
+        didSet(oldUserName){
+            print("Welcome to \(currentUserName)")
+            print("I miss \(oldUserName) already!")
+        }
+    }
+}
+
+let manager = UserManager()
+
+print(manager.currentUserName)
+
+manager.currentUserName = "Dade Murphy"
+
