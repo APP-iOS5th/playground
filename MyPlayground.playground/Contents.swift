60922394f256d869e9561b68a17a773159981de6
shin-jae-uk <shinju1412@gmail.com>
2024-04-02 11:44:47 +0900

    TicTacToe_subscript

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,72 +1,57 @@
-// Dictionary
+// subscript
 
-struct PersonName {
-    let givenName: String
-    let familyName: String
+enum GridPosotion: String {
+    case player1 = "o"
+    case player2 = "x"
+    case empty = " "
 }
 
-enum CommunicationMethod {
-    case phone
-    case email
-    case textMessage
-    case fax
-    case telepathy
-    case subSpaceRelay
-    case tachyons
+struct TicTacToe {
+    var gridStorage: [[GridPosotion]] = []      // 배열 안에 배열
+    
+    subscript(row: Int, column: Int) -> GridPosotion {
+        get {                                   // 접근자
+            return gridStorage[row][column]
+        }
+        set(newValue) {                         // 설정자
+            gridStorage[row][column] = newValue
+        }
+    }
+    
+    init() {
+        gridStorage.append(Array(repeating: .empty, count: 3))
+        gridStorage.append(Array(repeating: .empty, count: 3))
+        gridStorage.append(Array(repeating: .empty, count: 3))
+    }
+    
+    func gameStateString() -> String {
+        var stateString = "------------\n"
+        stateString += printableString(forRow: gridStorage[0])
+        stateString += "------------\n"
+        stateString += printableString(forRow: gridStorage[1])
+        stateString += "------------\n"
+        stateString += printableString(forRow: gridStorage[2])
+        stateString += "------------\n"
+        
+        return stateString
+    }
+    
+    func printableString(forRow row: [GridPosotion]) -> String {
+        var rowString = "| \(row[0].rawValue) "
+        rowString += "| \(row[1].rawValue) "
+        rowString += "| \(row[2].rawValue) |\n"
+        
+        return rowString
+    }
 }
 
-class Person {
-    let name: PersonName
-    let preferredCommunicationMethod: CommunicationMethod
-    
-    convenience init(givenName: String, familyName: String, commsMethod: CommunicationMethod) {
-        let name = PersonName(givenName: givenName, familyName: familyName)
-        self.init(name: name, commsMethod: commsMethod)
-    }
-    
-    init(name: PersonName, commsMethod: CommunicationMethod) {
-        self.name = name
-        preferredCommunicationMethod = commsMethod
-    }
-    
-    var displayName: String {
-        return "\(name.givenName) \(name.familyName)"
-    }
-}
+var game = TicTacToe()
+print(game.gameStateString())
 
-var crew = Dictionary<String, Person>()
+print(game[0, 0])
 
-crew["Captain"] = Person(givenName: "Jean-Luc",
-                         familyName: "Picard",
-                         commsMethod: .phone)
+game[1, 1] = .player1
+print(game.gameStateString())
 
-crew["First Officer"] = Person(givenName: "William",
-                               familyName: "Riker",
-                               commsMethod: .email)
-
-crew["Chief Engineer"] = Person(givenName: "Geordi",
-                                familyName: "LaForge",
-                                commsMethod: .textMessage)
-
-crew["Second Officer"] = Person(givenName: "Data",
-                                familyName: "Soong",
-                                commsMethod: .fax)
-
-crew["Councillor"] = Person(givenName: "Deanna",
-                            familyName: "Troi",
-                            commsMethod: .telepathy)
-
-crew["Security Officer"] = Person(givenName: "Tasha",
-                                  familyName: "Yar",
-                                  commsMethod: .subSpaceRelay)
-
-crew["Chief Medical Officer"] = Person(givenName: "Beverly",
-                                       familyName: "Crusher",
-                                       commsMethod: .tachyons)
-
-let roles = Array(crew.keys)
-print(roles)
-
-let firstRole = roles.first!
-let cmo = crew[firstRole]!
-print("\(firstRole): \(cmo.displayName)")
+game[0, 2] = .player2
+print(game.gameStateString())
