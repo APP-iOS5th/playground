c5af7a8350f867add604f6e7d947a1a497fdc739
MadCow <lobasketve@naver.com>
2024-04-02 15:39:29 +0900

    protocol 추가 예제

--- Representable.playground/Contents.swift
+++ Representable.playground/Contents.swift
@@ -16,5 +16,28 @@ extension String: IntRepresentable {
     }
 }
 
-var ten: String = "10"
-print(ten.intValue)
+enum CrewComplement: Int {
+    case enterpriseD = 1014
+    case voyager = 150
+    case deepSpaceNine = 2000
+}
+
+extension CrewComplement: IntRepresentable {
+    var intValue: Int {
+        return rawValue
+    }
+}
+
+var intableThings: [IntRepresentable] = []
+intableThings.append(550)
+intableThings.append(1200)
+intableThings.append("5")
+intableThings.append("1009")
+intableThings.append(CrewComplement.enterpriseD)
+intableThings.append(CrewComplement.voyager)
+intableThings.append(CrewComplement.deepSpaceNine)
+
+let over1000 = intableThings.compactMap({ $0.intValue > 1000 ? $0.intValue: nil })
+let over500 = intableThings.filter{ $0.intValue > 500 }.map{ $0.intValue }
+print(over1000)
+print(over500)
