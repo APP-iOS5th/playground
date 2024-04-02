e2361fd8c2901dbe3f7de16b7f80d9dc80731bb2
avan31017 <avan31017@gmail.com>
2024-04-02 13:19:15 +0900

    Subscript

--- TicTacToe.playground/Contents.swift
+++ TicTacToe.playground/Contents.swift
@@ -6,34 +6,52 @@ enum GridPosition: String {
     case empty = " "
 }
 
+
+
 struct TicTacToe {
     var gridStorage: [[GridPosition]] = []
     
     init() {
-        for _ in 0..<3 {
+        for i in 0..<3 {
             gridStorage.append(Array(repeating: .empty, count: 3))
         }
     }
     
+    subscript(row: Int, column: Int) -> GridPosition {
+            get {
+                return gridStorage[row][column]
+            }
+            set(newValue) {
+                gridStorage[row][column] = newValue
+            }
+        }
+    
     func gameStateString() -> String {
-        var stateString = ""
-        
+        var stateString = "-------------\n"
         for i in 0..<3 {
             stateString += printableString(forRow: gridStorage[i])
-            stateString = "-------------\n"
+            stateString += "-------------\n"
         }
         
         return stateString
     }
     
     func printableString(forRow row: [GridPosition]) -> String {
-        var rowString = ""
-        
-        for i in 0..<3 {
-            rowString += "| \(row[i].rawValue)"
-        }
+        var rowString = "| \(row[0].rawValue) "
+        rowString += "| \(row[1].rawValue) "
+        rowString += "| \(row[2].rawValue) |\n"
         
         return rowString
     }
     
 }
+
+var game = TicTacToe()
+print(game.gameStateString())
+
+game.gridStorage[1][1] = .player1
+print(game.gameStateString())
+print(game[1,1])
+
+game.gridStorage[0][2] = .player2
+print(game.gameStateString())
