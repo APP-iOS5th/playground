3c444e83470299821123d7336cb37c28625bebe6
uunwon <chea2089@naver.com>
2024-04-02 14:41:32 +0900

    default new&old Value

--- UserManager.playground/Contents.swift
+++ UserManager.playground/Contents.swift
@@ -1,14 +1,18 @@
 
+// ♥️ property observe ✨
+
 class UserManager {
     var currentUserName: String = "Emmanuel Goldstein" {
         // set 은 값을 쓸 때
         willSet (newUserName) { // 값을 쓰기 전, 클로저임, 값 바뀌기 직전에 호출 ✨
             print("Goodbye to \(currentUserName)")
             print("I hear \(newUserName) is on their way!")
+            // == print("I hear \(newValue) is on their way!")
         }
         didSet (oldUserName) { // 값을 쓴 후, 클로저임, 값 바뀐 후에 호출 ✨
             print("Welcome to \(currentUserName)")
             print("I miss \(oldUserName)")
+            // == print("I miss \(oldValue)")
         }
     }
 }
