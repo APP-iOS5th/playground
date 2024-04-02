b90206a94d5434a445bcb5cb54b1ff4f017ac2d0
jeongwookim <112370814+JeongWo@users.noreply.github.com>
2024-04-02 09:23:54 +0900

    Set

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -293,3 +293,35 @@ print(moviesToWatch.count)
 
 moviesToWatch.remove(at: 6)
 print(moviesToWatch.count)
+
+
+let fibonacciArray: Array<Int> = [1,1,2,3,4,8,13,21,34]
+let fibonaccisSet: Set<Int> = [1,1,2,3,4,8,13,21,34]
+
+print(fibonacciArray.count)
+print(fibonaccisSet.count)
+
+var animals : Set<String> = ["cat","dog","mouse","elephant"]
+
+animals.insert("rabbit")
+print(animals.contains("dog"))
+animals.remove("dog")
+print(animals.contains("dog"))
+
+
+let evenNumbers = Set<Int>(arrayLiteral: 2, 4, 6, 8, 10)
+let oddNumbers: Set<Int> = [1, 3, 5, 7, 9]
+let squareNumbers: Set<Int> = [1, 4, 9]
+let triangularNumbers: Set<Int> = [1, 3, 6, 10]
+
+let evenOrTriangularNumbers = evenNumbers.union(triangularNumbers) // 2, 4, 6, 8, 10, 1, 3, unordered
+print(evenOrTriangularNumbers.count) // 7
+
+let oddAndSquareNumbers = oddNumbers.intersection(squareNumbers) // 1, 9, unordered
+print(oddAndSquareNumbers.count) // 2
+
+let squareOrTriangularNotBoth = squareNumbers.symmetricDifference(triangularNumbers) // 4, 9, 3, 6, 10, unordered
+print(squareOrTriangularNotBoth.count) // 5
+
+let squareNotOdd = squareNumbers.subtracting(oddNumbers) // 4
+print(squareNotOdd.count) // 1
