91945c3d98cfd2ac9677bfa1901ecbd4338ea268
uunwon <chea2089@naver.com>
2024-04-02 09:23:46 +0900

    Set

---
+++ SetBasics.playground/Contents.swift
@@ -0,0 +1,27 @@
+import UIKit
+
+let fibonacciArray: Array<Int> = [1, 1, 2, 3, 5, 8, 13, 21, 34]
+let fibonacciSet: Set<Int> = [1, 1, 2, 3, 5, 8, 13, 21, 34]
+
+let evenNumbers = Set<Int>(arrayLiteral: 2, 4, 6, 8, 10)
+let oddNumbers: Set<Int> = [1, 3, 5, 7, 9]
+let squareNumbers: Set<Int> = [1, 4, 9]
+let triangularNumbers: Set<Int> = [1, 3, 6, 10]
+
+let evenOrTriangularNumbers = evenNumbers.union(triangularNumbers)
+    // 해당 요소들이 중복되지 않고 하나씩 동작한다
+    // 2, 4, 6, 8, 10, 1, 3, unordered
+print(evenOrTriangularNumbers.count) // 7
+
+let oddAndSquareNumbers = oddNumbers.intersection(squareNumbers)
+    // 교집합 만들기
+    // 1, 9, unordered
+print(oddAndSquareNumbers.count) // 2
+
+let squareOrTriangularNotBoth = squareNumbers.symmetricDifference(triangularNumbers)
+    // 여집합 만들기
+    // 4, 9, 3, 6, 10, unordered
+print(squareOrTriangularNotBoth.count) // 5
+
+let squareNotOdd = squareNumbers.subtracting(oddNumbers) // 4
+print(squareNotOdd.count) // 1
