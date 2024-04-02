ced513f297ddb5330205b811576d35f83dc0b72e
Jongchan Yun <jcyun1996@gmail.com>
2024-04-02 17:14:44 +0900

    Access fileprivate

--- AppleInc/AppleStore.playground/Contents.swift
+++ AppleInc/AppleStore.playground/Contents.swift
@@ -13,3 +13,17 @@ public class AppleiPhone {
         self.model = model
     }
 }
+
+fileprivate class Factory {
+    func makeiPhone(ofModel model: DeviceModel) -> AppleiPhone {
+        return AppleiPhone(model: model)
+    }
+}
+
+public class AppleStore {
+    private var factory = Factory()
+    
+    public func selliPhone(ofModel model: DeviceModel) -> AppleiPhone {
+        return factory.makeiPhone(ofModel: model)
+    }
+}
