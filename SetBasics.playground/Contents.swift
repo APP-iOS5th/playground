5ad195a1dd5e95bf8fa22c6191d3901bfd9ec3cc
sohee <danwater86@gmail.com>
2024-04-02 09:26:19 +0900

    Set2

--- SetBasics.playground/Contents.swift
+++ SetBasics.playground/Contents.swift
@@ -10,3 +10,22 @@ animals.insert("rabbit") // rabbit 추가
 print(animals.contains("duck")) // contains는 비교해서 알려줌
 animals.remove("dog")
 print(animals.contains("rabbit"))
+
+
+// Union, Intersection, Symmetric Difference and Subtracting
+let evenNumbers = Set<Int>(arrayLiteral: 2, 4, 6, 8, 10)
+let oddNumbers: Set<Int> = [1, 3, 5, 7, 9]
+let squareNumbers: Set<Int> = [1, 4, 9]
+let triangularNumbers: Set<Int> = [1, 3, 6, 10]
+
+let evenOrTriangularNumbers = evenNumbers.union(triangularNumbers) // 2, 4, 6, 8, 10, 1, 3, unordered
+print(evenOrTriangularNumbers.count) // 7
+
+let oddAndSquareNumbers = oddNumbers.intersection(squareNumbers) // 1, 9, unordered
+print(oddAndSquareNumbers.count) // 2
+
+let squareOrTriangularNotBoth = squareNumbers.symmetricDifference(triangularNumbers) // 4, 9, 3, 6, 10, unordered
+print(squareOrTriangularNotBoth.count) // 5
+
+let squareNotOdd = squareNumbers.subtracting(oddNumbers) // 4
+print(squareNotOdd.count) // 1
