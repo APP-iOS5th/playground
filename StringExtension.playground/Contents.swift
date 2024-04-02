ba480cfd0fa4cecc5dec2317dfe02e5d567fdd7d
MadCow <lobasketve@naver.com>
2024-04-02 15:16:39 +0900

    computed property

--- StringExtension.playground/Contents.swift
+++ StringExtension.playground/Contents.swift
@@ -7,7 +7,15 @@ extension String {
         
         return String(word)
     }
+    
+    var firstWordVar: String {
+        let spaceIndex = firstIndex(of: " ") ?? endIndex
+        let word = prefix(upTo: spaceIndex)
+        
+        return String(word)
+    }
 }
 
 let llap = "Live long, and prosper"
 print(llap.firstWord())
+print(llap.firstWordVar)
