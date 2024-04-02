c55eb6f7165bc6c480eacd4ded36fc0513a551d3
SeongKookKIM <ksk8646@naver.com>
2024-04-02 15:30:19 +0900

    Extention Protocol

--- StringExtention.playground/Contents.swift
+++ StringExtention.playground/Contents.swift
@@ -10,3 +10,23 @@ extension String {
 let llap = "Live long, and prosper"
 let firstWord = llap.firstWord()
 print(firstWord)
+
+// 기존 타입에 프로토콜 준수 추가
+protocol IntRepresentable {
+    var intValue : Int {get}
+}
+
+extension Int : IntRepresentable {
+    var intValue: Int {
+        return self
+    }
+}
+
+extension String: IntRepresentable {
+    var intValue: Int {
+        return Int(self) ?? 0
+    }
+}
+var ten = "10"
+var tenInt: Int = ten.intValue
+print(tenInt)
