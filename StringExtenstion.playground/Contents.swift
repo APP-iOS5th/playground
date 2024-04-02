a4f234df62cea198a0f223c1814c462ade9aae0e
Ruel <c0407h@gmail.com>
2024-04-02 15:16:31 +0900

    computed property

--- StringExtenstion.playground/Contents.swift
+++ StringExtenstion.playground/Contents.swift
@@ -2,7 +2,7 @@
 
 
 extension String {
-    func firstWord() -> String {
+    var firstWord: String {
         let spaceIndex = firstIndex(of: " ") ?? endIndex
         let word = prefix(upTo: spaceIndex)
         return String(word)
@@ -10,5 +10,5 @@ extension String {
 }
 
 let llap = "Live long, and prosper"
-let firstWord = llap.firstWord()
+let firstWord = llap.firstWord
 print(firstWord) 
