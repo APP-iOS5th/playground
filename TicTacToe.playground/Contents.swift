7830a2a6ff3229cbd6041cbdcda1139d894d81e1
hwang <jangku88@naver.com>
2024-04-02 13:21:29 +0900

    parameter label for subscript

--- TicTacToe.playground/Contents.swift
+++ TicTacToe.playground/Contents.swift
@@ -7,7 +7,7 @@ enum GridPosition: String {
 struct TicTacToe {
     var gridStorage: [[GridPosition]] = [] // 이중 배열
     
-    subscript(row: Int, column: Int) -> GridPosition {
+    subscript(atRow row: Int, atColumn column: Int) -> GridPosition {
         get {
             return gridStorage[row][column]
         }
@@ -47,12 +47,11 @@ struct TicTacToe {
 var game = TicTacToe()
 print(game.gameStateString())
 
-print(game[0,0])
+print(game.gridStorage[0][0])
+print(game[atRow: 0,atColumn:0])
 
-//game.gridStorage[1][1] = .player1
-game[1,1] = .player1
+game[atRow:1, atColumn: 1] = .player1
 print(game.gameStateString())
 
-//game.gridStorage[0][2] = .player2
-game[0,2] = .player2
+game[atRow:0, atColumn: 2] = .player2
 print(game.gameStateString())
