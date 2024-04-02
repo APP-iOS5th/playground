ba3876ea493058b7ff87505453618b6c3db548de
Ruel <c0407h@gmail.com>
2024-04-02 14:16:32 +0900

    typealias closure

---
+++ ProgrammeFetcher.playground/Contents.swift
@@ -0,0 +1,29 @@
+
+
+enum Channel {
+    case BBC1
+    case BBC2
+    case BBCNews
+}
+
+class ProgrammeFetcher {
+    typealias FetchResultHandler = (String?, Error?) -> Void
+    
+    func fetchCurrentProgrammeName(forChannel channel: Channel, resultHandler: FetchResultHandler) {
+        
+        
+        let exampleProgramName = "Sherlock"
+        resultHandler(exampleProgramName, nil)
+    }
+    
+    func fetchNextProgrammeName(forChannel channel: Channel, resultHandler: FetchResultHandler) {
+        let exampleProgramName = "Luther"
+        resultHandler(exampleProgramName, nil)
+    }
+}
+
+let fetcher = ProgrammeFetcher()
+fetcher.fetchCurrentProgrammeName(forChannel: .BBC1) { programmeName, error in
+    print(programmeName ?? "N/A")
+}
+
