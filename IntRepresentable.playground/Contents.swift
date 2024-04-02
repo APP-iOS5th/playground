e8555ea25436b768465c423698942f8639890e64
rainbow_an <eventscripts@nate.com>
2024-04-02 15:41:41 +0900

    Protocol Extensions

--- IntRepresentable.playground/Contents.swift
+++ IntRepresentable.playground/Contents.swift
@@ -16,6 +16,26 @@ extension String: IntRepresentable {
     }
 }
 
-var ten = "10"
-var tenInt: Int = ten.intValue
-print(tenInt)
+enum CrewComplement: Int {
+    case enterpriseD = 1014
+    case voyager = 150
+    case deepSpaceNine = 2000
+}
+
+extension CrewComplement: IntRepresentable {
+    var intValue: Int {
+        return rawValue
+    }
+}
+
+var intableThings = [IntRepresentable]()
+intableThings.append(55)
+intableThings.append(1200)
+intableThings.append("5")
+intableThings.append("1009")
+intableThings.append(CrewComplement.enterpriseD)
+intableThings.append(CrewComplement.voyager)
+intableThings.append(CrewComplement.deepSpaceNine)
+
+let over1000 = intableThings.compactMap {$0.intValue > 1000 ? $0.intValue : nil}
+print(over1000)
