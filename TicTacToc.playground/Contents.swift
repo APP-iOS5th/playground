1f34c5e4112b2e260d79d011a3fec61e25e66871
uunwon <chea2089@naver.com>
2024-04-02 13:21:29 +0900

    parameter label for subscript

--- TicTacToc.playground/Contents.swift
+++ TicTacToc.playground/Contents.swift
@@ -9,7 +9,7 @@ struct TicTacToc {
     var gridStorage: [[GridPosition]] = []
     
     // subscript 는 대괄호를 이용해 현미경 같은 거임 (인터페이스.. 느낌)
-    subscript(row: Int, column: Int) -> GridPosition {
+    subscript(atRow row: Int, atColumn column: Int) -> GridPosition {
         // getter: tictactoc 을 가져오거나
         get {
             return gridStorage[row][column] // 위치값 출력
@@ -49,11 +49,11 @@ struct TicTacToc {
 
 var game = TicTacToc() // 초기화를 통해 게임 스타트
 print(game.gameStateString())
-print(game[0, 0])
+print(game[atRow: 0, atColumn: 0])
 
 // game.gridStorage[1][1] = .player1
 // 위 한줄을 아래 한줄로 간편하게 만들 때 subscript 사용
-game[1, 1] = .player1
-game[0, 2] = .player2
+game[atRow: 1, atColumn: 1] = .player1
+game[atRow: 0, atColumn: 2] = .player2
 
 print(game.gameStateString())
