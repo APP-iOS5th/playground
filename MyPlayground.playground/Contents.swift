589b477c46ae1a90ae2d145e656ce025d8c79f9c
jeongwookim <112370814+JeongWo@users.noreply.github.com>
2024-04-02 13:41:16 +0900

    typealias

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -497,3 +497,21 @@ print(game.gameStateString())
 
 game[atRow:0, atColumn: 2] = .player2
 print(game.gameStateString())
+
+
+struct Pug {
+    let name: String
+}
+let pugs = [Pug]()
+
+typealias Grumble = [Pug]
+
+var grumble = Grumble()
+
+let marty = Pug(name: "Marty McPug")
+let wolfie = Pug(name: "Wolfgang Pug")
+let buddy = Pug(name: "Buddy")
+
+grumble.append(marty)
+grumble.append(wolfie)
+grumble.append(buddy)
