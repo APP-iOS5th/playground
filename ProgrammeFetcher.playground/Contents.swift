6e538b1767ffc5d4697c86c7d6e644b19f4854be
iyungui <yungui632@gmail.com>
2024-04-02 14:16:35 +0900

    typealias clousers

---
+++ ProgrammeFetcher.playground/Contents.swift
@@ -0,0 +1,29 @@
+import UIKit
+
+enum Channel {
+    case BBC1
+    case BBC2
+    case BBCNews
+}
+
+class ProgrammeFetcher {
+    
+    typealias FetchResultHandler = (String?, Error?) -> Void
+    
+    func fetchCurrentProgrammeName(forChannel channel: Channel,
+                                   resultHandler: FetchResultHandler) {
+        let exampleProgramName = "Sherlock"
+        resultHandler(exampleProgramName, nil)
+    }
+    
+    func fetchNextProgrammeName(forChannel channel: Channel,
+                                resultHandler: FetchResultHandler) {
+        let exampleProgramName = "Luther"
+        resultHandler(exampleProgramName, nil)
+    }
+}
+
+let fetcher = ProgrammeFetcher()
+fetcher.fetchCurrentProgrammeName(forChannel: .BBC1, resultHandler: { programmeName, error in
+    print(programmeName ?? "N/A")
+})
