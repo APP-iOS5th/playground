49c686d7ea44ad572dcd81ec59e4b71f34462119
jmbae <jmbae@codegrove.co.kr>
2024-04-02 15:30:02 +0900

    Extension Protocol

---
+++ IntRepresentable.playground/Contents.swift
@@ -0,0 +1,19 @@
+
+protocol IntRepresentable {
+    var intValue: Int { get }
+}
+
+extension Int: IntRepresentable {
+    var intValue: Int {
+        return self
+    }
+}
+
+extension String: IntRepresentable {
+    var intValue: Int {
+        return Int(self) ?? 0
+    }
+}
+
+var ten = "10"
+print(ten.intValue)
