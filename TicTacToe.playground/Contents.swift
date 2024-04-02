bd7e44069d30875337515239678f5629b4bfccc0
Ruel <c0407h@gmail.com>
2024-04-02 13:18:51 +0900

    Subscript

--- TicTacToe.playground/Contents.swift
+++ TicTacToe.playground/Contents.swift
@@ -7,34 +7,44 @@ enum GridPosition: String {
 
 struct TicTacToe {
     var gridStorage: [[GridPosition]] = []
-        
-        init() {
-            gridStorage.append(Array(repeating: .empty, count: 3))
-            gridStorage.append(Array(repeating: .empty, count: 3))
-            gridStorage.append(Array(repeating: .empty, count: 3))
+    
+    subscript(atRow row:Int, atColumn column: Int) -> GridPosition {
+        get {
+            return gridStorage[row][column]
         }
-        
-        func gameStateString() -> String {
-            var stateString = "-------------\n"
-            stateString += printableString(forRow: gridStorage[0])
-            stateString += "-------------\n"
-            stateString += printableString(forRow: gridStorage[1])
-            stateString += "-------------\n"
-            stateString += printableString(forRow: gridStorage[2])
-            stateString += "-------------\n"
-            
-            return stateString
+        set(newValue) {
+            gridStorage[row][column]
         }
+    }
+    
+    init() {
+        gridStorage.append(Array(repeating: .empty, count: 3))
+        gridStorage.append(Array(repeating: .empty, count: 3))
+        gridStorage.append(Array(repeating: .empty, count: 3))
+    }
+    
+    func gameStateString() -> String {
+        var stateString = "-------------\n"
+        stateString += printableString(forRow: gridStorage[0])
+        stateString += "-------------\n"
+        stateString += printableString(forRow: gridStorage[1])
+        stateString += "-------------\n"
+        stateString += printableString(forRow: gridStorage[2])
+        stateString += "-------------\n"
         
-        func printableString(forRow row: [GridPosition]) -> String {
-            var rowString = "| \(row[0].rawValue) "
-            rowString += "| \(row[1].rawValue) "
-            rowString += "| \(row[2].rawValue) |\n"
-            return rowString
-        }
-
-
+        return stateString
+    }
+    
+    func printableString(forRow row: [GridPosition]) -> String {
+        var rowString = "| \(row[0].rawValue) "
+        rowString += "| \(row[1].rawValue) "
+        rowString += "| \(row[2].rawValue) |\n"
+        return rowString
+    }
+    
+    
 }
 
 var game = TicTacToe()
+game[atRow: 0, atColumn: 1] = .player1
 print(game.gameStateString())
