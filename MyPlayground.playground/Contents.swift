6f90d33571910cdbba14e4bf37af583e6437b4db
jinwoong <baseball0020@gmail.com>
2024-04-02 09:24:49 +0900

    set

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,12 +1,18 @@
 import UIKit
 
-func normalizedStarRating(forRating rating: Float, ofPossibleTotal total: Float) -> (Int, String) {
-    let fraction = rating / total
-    let ratingOutOf5 = fraction * 5
-    let roundedRating = round(ratingOutOf5)
-    let numberOfStars = Int(roundedRating)
-    let ratingString = "\(numberOfStars) Star Movie"
-    return (numberOfStars,ratingString)
-}
+let evenNumbers = Set<Int>(arrayLiteral: 2, 4, 6, 8, 10)
+let oddNumbers: Set<Int> = [1, 3, 5, 7, 9]
+let squareNumbers: Set<Int> = [1, 4, 9]
+let triangularNumbers: Set<Int> = [1, 3, 6, 10]
 
-normalizedStarRating(forRating: 10.0, ofPossibleTotal: 20.0)
+let evenOrTriangularNumbers = evenNumbers.union(triangularNumbers) // 2, 4, 6, 8, 10, 1, 3, unordered
+print(evenOrTriangularNumbers.count) // 7
+
+let oddAndSquareNumbers = oddNumbers.intersection(squareNumbers) // 1, 9, unordered
+print(oddAndSquareNumbers.count) // 2
+
+let squareOrTriangularNotBoth = squareNumbers.symmetricDifference(triangularNumbers) // 4, 9, 3, 6, 10, unordered
+print(squareOrTriangularNotBoth.count) // 5
+
+let squareNotOdd = squareNumbers.subtracting(oddNumbers) // 4
+print(squareNotOdd.count) // 1
