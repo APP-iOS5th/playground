84e28e011924f401e710bb23d66fff4fcb05f07f
shin-jae-uk <shinju1412@gmail.com>
2024-04-02 10:07:36 +0900

    Hashable

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,31 +1,25 @@
-// Set
+// Hashable
 
-let fibonacciArray: Array<Int> = [1, 1, 2, 3, 5, 8, 13, 21, 34]
-let fibonacciSet: Set<Int> = [1, 1, 2, 3, 5, 8, 13, 21, 34]
+struct Person: Hashable {
+    var name: String
+    var age: Int
+    
+    func hash(into hasher: inout Hasher) {                  // 지역 함수
+        hasher.combine(name)
+        hasher.combine(age)
+    }   // 이름 또는 나이를 통해 해시값으로 구분
+    
+    static func == (lhs: Person, rhs: Person) -> Bool {     // 전역 함수
+        return lhs.name == rhs.name && lhs.age == rhs.age
+    }
+}
 
-print(fibonacciArray)   // 배열은 넣은 순서를 보장
-print(fibonacciSet)     // 넣은 순서 따르지 않고, 항목이 중복되지 않음(유일한 항목 보장)
+let person1 = Person(name: "Alice", age: 30)
+let person2 = Person(name: "Bob", age: 25)
 
-var animals: Set<String> = ["cat", "dog", "mouse", "elephant"]
-animals.insert("rabbit")
-print(animals.contains("dog"))  // set에 "dog"가 포함되어 있는가
-animals.remove("dog")
-print(animals.contains("dog"))
+var peopleSet: Set<Person> = [person1, person2]
 
-// Union, Intersection, Symmetric Difference and Subtracting
-let evenNumbers = Set<Int>(arrayLiteral: 2, 4, 6, 8, 10)
-let oddNumbers: Set<Int> = [1, 3, 5, 7, 9]
-let squareNumbers: Set<Int> = [1, 4, 9]
-let triangularNumbers: Set<Int> = [1, 3, 6, 10]
+let person3 = Person(name: "Alice", age: 30)
 
-let evenOrTriangularNumbers = evenNumbers.union(triangularNumbers) // 2, 4, 6, 8, 10, 1, 3, unordered
-print(evenOrTriangularNumbers.count) // 7
-
-let oddAndSquareNumbers = oddNumbers.intersection(squareNumbers) // 1, 9, unordered
-print(oddAndSquareNumbers.count) // 2
-
-let squareOrTriangularNotBoth = squareNumbers.symmetricDifference(triangularNumbers) // 4, 9, 3, 6, 10, unordered
-print(squareOrTriangularNotBoth.count) // 5
-
-let squareNotOdd = squareNumbers.subtracting(oddNumbers) // 4
-print(squareNotOdd.count) // 1
+peopleSet.insert(person3)
+print(peopleSet.count)      // person1과 person3은 같은 해시값
