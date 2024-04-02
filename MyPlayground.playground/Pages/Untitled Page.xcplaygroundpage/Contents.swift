34049cdec34832484f964e87043e88c1dd437b48
ksiomng0929 <nadana0929@gmail.com>
2024-04-02 09:38:34 +0900

    Hash

--- MyPlayground.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
@@ -267,7 +267,7 @@ func normalizedStarRating(forRating rating: Float, ofPossibleTotal total: Float)
     let ratingString = "\(numberOfStars) Star Movie"
     return (numberOfStars, ratingString)
 }
-*/
+
 
 /// 배열
 var moviesToWatch: Array<String> = Array()
@@ -305,3 +305,46 @@ print(moviesToWatch)
 
 moviesToWatch.remove(at: 6) //삭제
 print(moviesToWatch.count)
+
+/// 데이터를 집합 Set에 포함하기
+import Foundation
+
+let fibonacciArray: Array<Int> = [1, 1, 2, 3, 5, 8, 13, 21, 34]
+let fibonacciSet: Set<Int> = [1, 1, 2, 3, 5, 8, 13, 21, 34]
+print(fibonacciArray.count) // 9
+print(fibonacciSet.count) // 8
+
+var animals: Set<String> = ["cat", "dog", "mouse", "elephant"]
+
+animals.insert("rabbit")
+print(animals.contains("dog")) // true
+animals.remove("dog")
+print(animals.contains("dog")) // false
+
+let evenNumbers = Set<Int>(arrayLiteral: 2, 4, 6, 8, 10)
+let oddNumbers: Set<Int> = [1, 3, 5, 7, 9]
+let squareNumbers: Set<Int> = [1, 4, 9]
+let triangularNumbers: Set<Int> = [1, 3, 6, 10]
+
+let evenOrTriangularNumbers = evenNumbers.union(triangularNumbers) // 2, 4, 6, 8, 10, 1, 3, unordered
+print(evenOrTriangularNumbers.count) // 7
+
+let oddAndSquareNumbers = oddNumbers.intersection(squareNumbers) // 1, 9, unordered
+print(oddAndSquareNumbers.count) // 2
+
+let squareOrTriangularNotBoth = squareNumbers.symmetricDifference(triangularNumbers) // 4, 9, 3, 6, 10, unordered
+print(squareOrTriangularNotBoth.count) // 5
+
+let squareNotOdd = squareNumbers.subtracting(oddNumbers) // 4
+print(squareNotOdd.count) // 1
+*/
+/// Hashable
+struct Person: Hashable {
+    var name: String
+    var age: Int
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(name)
+        hasher.combine(age)
+    }
+}
