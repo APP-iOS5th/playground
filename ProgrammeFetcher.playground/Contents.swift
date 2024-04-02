8e286801acd0d001b531f3d10166962a6a146f3a
JongWon Jeong <jjwon214@gmail.com>
2024-04-02 14:17:08 +0900

    ProgramFetcher_Typealias

--- ProgrammeFetcher.playground/Contents.swift
+++ ProgrammeFetcher.playground/Contents.swift
@@ -1,3 +1,31 @@
-import UIKit
+import Foundation
 
-var greeting = "Hello, playground"
+enum Channel {
+    case BBC1
+    case BBC2
+    case BBCNews
+}
+
+class ProgrammeFetcher {
+    typealias FetchResultHandler = (String?, Error?) -> Void
+    
+    func fetchCurrentProgrammeName(forChannel channel: Channel,
+                                   resultHandler: FetchResultHandler) {
+        // 현재 프로그램 가져오기
+        let programmeName = "Sherlock"
+        resultHandler(programmeName, nil)
+    }
+    func fetchNextProgrammeName(forChannel channel: Channel,
+                                resultHandler: FetchResultHandler) {
+        // 다음 프로그램 가져오기
+        let programmeName = "Luther"
+        resultHandler(programmeName, nil)
+    }
+}
+
+let fetcher = ProgrammeFetcher()
+fetcher.fetchCurrentProgrammeName(forChannel: .BBC1, resultHandler: { program, error in
+    
+    print(program ?? "N?A")
+    
+})
