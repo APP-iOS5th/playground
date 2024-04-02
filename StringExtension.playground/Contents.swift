5544bd563fbbb34817bd547a53dc0062d0280695
MadCow <lobasketve@naver.com>
2024-04-02 14:49:34 +0900

    Extension

---
+++ StringExtension.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+extension String {
+    func firstWord() -> String {
+        let spaceIndex = firstIndex(of: " ") ?? endIndex
+        let word = prefix(upTo: spaceIndex)
+        
+        return String(word)
+    }
+}
+
+let llap = "Live long, and prosper"
+print(llap.firstWord())
