1989985a77101df7cec4c56f1351efd155edd466
Jinx <jinxwift@gmail.com>
2024-04-02 14:28:41 +0900

    typealias closure

---
+++ UserManager.playground/Contents.swift
@@ -0,0 +1,20 @@
+
+class UserManager {
+    var currentUserName: String = "Emmanuel Goldstein" {
+        willSet (newUserName) {
+            print("Goodbye to \(currentUserName)")
+            print("I hear \(newUserName) is on their way!")
+        }
+        didSet (oldUserName) {
+            print("Welcome to \(currentUserName)")
+            print("I miss \(oldUserName) already!")
+        }
+    }
+}
+
+let manager = UserManager()
+
+print(manager.currentUserName)
+
+manager.currentUserName = "Dade Murphy"
+
