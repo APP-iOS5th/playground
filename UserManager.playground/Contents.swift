9e7f706f535023f2b88cf45c595ea1fef0df982c
hwang <jangku88@naver.com>
2024-04-02 14:32:08 +0900

    implicit name

--- UserManager.playground/Contents.swift
+++ UserManager.playground/Contents.swift
@@ -1,13 +1,13 @@
 
 class UserManager {
     var currentUserName: String = "Emmanuel Goldstein" { // 변수인데, willSet,didSet이라는 클로저를 붙여주고 지켜본다. (변화가 있을 때 까지?)
-        willSet (newUserName) { // 프로퍼티 옵져버
+        willSet { // 프로퍼티 옵져버
             print("Goodbye to \(currentUserName)")
-            print("I hear \(newUserName) is on their way!")
+            print("I hear \(newValue) is on their way!")
         }
-        didSet (oldUserName) { // 프로퍼티 옵져버
+        didSet { // 프로퍼티 옵져버
             print("Welcome to \(currentUserName)")
-            print("I miss \(oldUserName) already!")
+            print("I miss \(oldValue) already!")
         }
     }
 }
