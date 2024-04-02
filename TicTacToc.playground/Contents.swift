8f4dbaf71ffe57dc833bb5b144bee34e330ecf96
uunwon <chea2089@naver.com>
2024-04-02 13:19:00 +0900

    Subscript

--- TicTacToc.playground/Contents.swift
+++ TicTacToc.playground/Contents.swift
@@ -8,6 +8,18 @@ enum GridPosition: String {
 struct TicTacToc {
     var gridStorage: [[GridPosition]] = []
     
+    // subscript 는 대괄호를 이용해 현미경 같은 거임 (인터페이스.. 느낌)
+    subscript(row: Int, column: Int) -> GridPosition {
+        // getter: tictactoc 을 가져오거나
+        get {
+            return gridStorage[row][column] // 위치값 출력
+        }
+        // setter: tictactoc 을 쓰거나
+        set(newValue) {
+            gridStorage[row][column] = newValue // 객체에 값 넣음
+        }
+    }
+    
     init() {
         gridStorage.append(Array(repeating: .empty, count: 3))
         gridStorage.append(Array(repeating: .empty, count: 3))
@@ -37,9 +49,11 @@ struct TicTacToc {
 
 var game = TicTacToc() // 초기화를 통해 게임 스타트
 print(game.gameStateString())
+print(game[0, 0])
 
-game.gridStorage[1][1] = .player1
-print(game.gameStateString())
+// game.gridStorage[1][1] = .player1
+// 위 한줄을 아래 한줄로 간편하게 만들 때 subscript 사용
+game[1, 1] = .player1
+game[0, 2] = .player2
 
-game.gridStorage[0][2] = .player2
 print(game.gameStateString())
