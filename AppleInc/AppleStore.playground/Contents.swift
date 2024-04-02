20887da3ac84e9ca19bc3915fc7afc73ff98c517
jeongwookim <112370814+JeongWo@users.noreply.github.com>
2024-04-02 17:09:25 +0900

    AppleInc

---
+++ AppleInc/AppleStore.playground/Contents.swift
@@ -0,0 +1,16 @@
+public enum DeviceModel {
+    
+    case iPhone12
+    case iPhone12Mini
+    case iPhone12Pro
+    case iPhone12ProMax
+}
+
+public class AppleiPhone {
+    public let model: DeviceModel
+    
+    fileprivate init(model: DeviceModel) {
+        self.model = model
+    }
+}
+
