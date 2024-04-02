5670c3b73688e6047431f34ad1a448de8353d1c9
MadCow <lobasketve@naver.com>
2024-04-02 15:30:03 +0900

    protocol

---
+++ Representable.playground/Contents.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+protocol IntRepresentable {
+    var intValue: Int { get }
+}
+
+extension Int: IntRepresentable {
+    var intValue: Int {
+        return self
+    }
+}
+
+extension String: IntRepresentable {
+    var intValue: Int {
+        return Int(self) ?? 0
+    }
+}
+
+var ten: String = "10"
+print(ten.intValue)
