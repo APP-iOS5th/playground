4064bacc25165881ebc62c901d92caf17791ac83
jmbae <jmbae@codegrove.co.kr>
2024-04-02 09:53:13 +0900

    Set with Hashable

---
+++ Hashable.playground/Contents.swift
@@ -0,0 +1,26 @@
+
+struct Person: Hashable {
+    var name: String
+    var age: Int
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(name)
+        hasher.combine(age)
+    }
+    
+    static func == (lhs: Person, rhs: Person) -> Bool {
+        return lhs.name == rhs.name && lhs.age == rhs.age
+    }
+}
+
+let person1 = Person(name: "Alice", age: 30)
+let person2 = Person(name: "Bob", age: 25)
+
+var peopleSet: Set<Person> = [person1, person2]
+
+let person3 = Person(name: "Alice", age: 20)
+
+peopleSet.insert(person3)
+
+print(peopleSet.count)
+print(peopleSet)
