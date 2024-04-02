5b968b2054065535d60c0175d91327a3bbc564fa
Jongchan Yun <jcyun1996@gmail.com>
2024-04-02 11:34:43 +0900

    TicTacToe

---
+++ TicTacToe.playground/Contents.swift
@@ -0,0 +1,39 @@
+
+enum GridPosition: String {
+    case player1 = "o"
+    case player2 = "x"
+    case empty = " "
+}
+
+struct TicTacToe {
+    var gridStorage: [[GridPosition]] = []
+    
+    init() {
+        gridStorage.append(Array(repeating: .empty, count: 3))
+        gridStorage.append(Array(repeating: .empty, count: 3))
+        gridStorage.append(Array(repeating: .empty, count: 3))
+    }
+    
+    func gameStateString() -> String {
+        var stateString = "-------------\n"
+        stateString += printableString(forRow: gridStorage[0])
+        stateString += "-------------\n"
+        stateString += printableString(forRow: gridStorage[1])
+        stateString += "-------------\n"
+        stateString += printableString(forRow: gridStorage[2])
+        stateString += "-------------\n"
+        
+        return stateString
+    }
+    
+    func printableString(forRow row: [GridPosition]) ->  String {
+        var rowString =  "| \(row[0].rawValue) "
+        rowString +=  "| \(row[0].rawValue) "
+        rowString +=  "| \(row[0].rawValue) |\n"
+        
+        return rowString
+    }
+}
+
+var game = TicTacToe()
+print(game.gameStateString())
