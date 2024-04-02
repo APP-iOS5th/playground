746553b7b94814e8b63b4320f38938369ff014fe
wotjs020708 <wotjs020708@gmail.com>
2024-04-02 09:11:29 +0900

    Array

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,38 +1,41 @@
 
-class Person {
-    let givenName: String
-    let middleName: String
-    let familyName: String
-    var countryOfResidence: String = "KR"
-        
-    
-    init(givenName: String, middleName: String, familyName: String) {
-        self.givenName = givenName
-        self.middleName = middleName
-        self.familyName = familyName
-    }
-    
-    func fullName() -> String{
-        return "\(givenName) \(middleName) \(familyName)"
-    }
-    
-    var displayString: String {
-        return  "\(fullName()) - Location: \(countryOfResidence)"
-    }
+var moviestoWatch: Array<String> = Array()
 
-}
+moviestoWatch.append("The Shawshank Redemption")  // 0
+moviestoWatch.append("Ghostbusters") // 1
+moviestoWatch.append("Terminator 2") // 2
 
-var person = Person(givenName: "Jaesun", middleName: " ", familyName: "Eo")
+print(moviestoWatch[1])
+print(moviestoWatch.count)
 
-final class Family: Person {
-    let relationship: String
-    init(givneName: String,
-         middleName: String,
-         familyName: String,
-         relationship: String) {
-        self.relationship = relationship
-        super.init(givenName: givneName, middleName: middleName, familyName: familyName)
-    }
-}
+moviestoWatch.insert("The Matrix", at: 2)
 
-var fmaily = Family(givneName: "Jaesun", middleName: "js", familyName: "Eo", relationship: "me")
+print(moviestoWatch.first ?? "Empty")
+print((moviestoWatch.last ?? "Empty"))
+
+
+let secondMovietoWatch = moviestoWatch[1]
+moviestoWatch[1] = "Ghostbusters (1984)"
+print(moviestoWatch.count)
+print(moviestoWatch)
+
+
+let spyMovieSuggestions: [String] = ["The bourne Identity", "Casino Royale", "Mission Impossible"]
+
+moviestoWatch = moviestoWatch + spyMovieSuggestions
+print(moviestoWatch.count)
+print(moviestoWatch)
+
+var starWarsTrilogy = Array<String>(repeating: "Star Wars: ", count: 3)
+
+starWarsTrilogy[0] += "A New Hope"
+starWarsTrilogy[1] += "Empire Strikes Back"
+starWarsTrilogy[2] += "Return of the Jedi"
+print(starWarsTrilogy)
+
+
+moviestoWatch.replaceSubrange(2...4, with: starWarsTrilogy)
+print(moviestoWatch.count)
+
+moviestoWatch.remove(at: 6)
+print(moviestoWatch.count)
