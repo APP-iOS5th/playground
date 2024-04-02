ad6d2d38c0da09793c02e3b2365d75cc32012b14
shin-jae-uk <shinju1412@gmail.com>
2024-04-02 09:24:26 +0900

    Set

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,40 +1,31 @@
-// Array
+// Set
 
-var moviesToWatch: Array<String> = Array()
+let fibonacciArray: Array<Int> = [1, 1, 2, 3, 5, 8, 13, 21, 34]
+let fibonacciSet: Set<Int> = [1, 1, 2, 3, 5, 8, 13, 21, 34]
 
-moviesToWatch.append("The Shawshank Redemption")
-moviesToWatch.append("Ghostbusters")
-moviesToWatch.append("Terminator 2")
+print(fibonacciArray)   // 배열은 넣은 순서를 보장
+print(fibonacciSet)     // 넣은 순서 따르지 않고, 항목이 중복되지 않음(유일한 항목 보장)
 
-moviesToWatch[0]
-moviesToWatch[1]
-moviesToWatch[2]
-print(moviesToWatch.count)
+var animals: Set<String> = ["cat", "dog", "mouse", "elephant"]
+animals.insert("rabbit")
+print(animals.contains("dog"))  // set에 "dog"가 포함되어 있는가
+animals.remove("dog")
+print(animals.contains("dog"))
 
-moviesToWatch.insert("The Matrix", at: 2) // 기존 2번 내용을 뒤로 밀어내고 새롭게 추가됨
+// Union, Intersection, Symmetric Difference and Subtracting
+let evenNumbers = Set<Int>(arrayLiteral: 2, 4, 6, 8, 10)
+let oddNumbers: Set<Int> = [1, 3, 5, 7, 9]
+let squareNumbers: Set<Int> = [1, 4, 9]
+let triangularNumbers: Set<Int> = [1, 3, 6, 10]
 
-print(moviesToWatch.first ?? "Empty")
-print(moviesToWatch.last ?? "Empty")
+let evenOrTriangularNumbers = evenNumbers.union(triangularNumbers) // 2, 4, 6, 8, 10, 1, 3, unordered
+print(evenOrTriangularNumbers.count) // 7
 
-let secondMovieToWatch = moviesToWatch[1]
-moviesToWatch[1] = "Ghostbusters (1984)"
-print(moviesToWatch.count)
-print(moviesToWatch)
+let oddAndSquareNumbers = oddNumbers.intersection(squareNumbers) // 1, 9, unordered
+print(oddAndSquareNumbers.count) // 2
 
-let spyMovieSuggestion: [String] = ["The Bourne Indenty", "Casino Royale", "Mission Impossible"]
-moviesToWatch = moviesToWatch + spyMovieSuggestion
-print(moviesToWatch.count)
-print(moviesToWatch)
+let squareOrTriangularNotBoth = squareNumbers.symmetricDifference(triangularNumbers) // 4, 9, 3, 6, 10, unordered
+print(squareOrTriangularNotBoth.count) // 5
 
-var starWarsTrilogy = Array<String>(repeating: "Star Wars: ", count: 3)
-starWarsTrilogy[0] += "A New Hope"
-starWarsTrilogy[1] += "Empire Strikes Back"
-starWarsTrilogy[2] += "Return of the Jedi"
-print(starWarsTrilogy)
-
-moviesToWatch.replaceSubrange(2...4, with: starWarsTrilogy) // [2]~[4] 범위에서 배열 교체
-print(moviesToWatch.count)
-
-moviesToWatch.remove(at: 6) // [6] 배열 값 삭제
-print(moviesToWatch.count)
-print(moviesToWatch)
+let squareNotOdd = squareNumbers.subtracting(oddNumbers) // 4
+print(squareNotOdd.count) // 1
