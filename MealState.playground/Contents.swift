7f66560f2f2fa6ede619c14ffe4edb4eb6907817
Cha, Jiyong <wldyd2113@gmail.com>
2024-04-03 13:33:20 +0900

    Mutiple error

--- MealState.playground/Contents.swift
+++ MealState.playground/Contents.swift
@@ -9,10 +9,26 @@ enum MealState {
 }
 enum MealError: Error{
     case canOnlyMoveToAppropriateState
+    case tooMuchSalt
+    case wrongStateToAddSalt
 }
 
 class Meal {
     private(set) var state: MealState = .initial //상태변경을 외부에 공개하고 싶지 않으면 private(set) 클래스 내부에서만 사용
+    private(set) var saltAdded = 0
+    
+    func addSalt() throws { //Void 생략
+        if saltAdded >= 5 {
+            throw MealError.tooMuchSalt
+        }
+        else if case .initial = state, case .buyIngregients = state { //if case는 if let 생각하면됨
+            throw MealError.wrongStateToAddSalt
+        }
+        else {
+            saltAdded = saltAdded + 1
+        }
+    }
+    
     func change(to newState: MealState) throws {
         switch (state, newState){ //state:원래 상태, newState: 다음 상태를 나타냄 여기 구문에서
         case (.initial, .buyIngregients),(.buyIngregients, .prepareIngredients),(.prepareIngredients, .cook),
@@ -59,6 +75,12 @@ do {
     print("Dinner is served!")
     
     
-} catch let error {
-    print(error)
+} catch MealError.canOnlyMoveToAppropriateState {
+    print("It's not possible to move to this state")
+} catch MealError.tooMuchSalt {
+    print("Too much salt!")
+} catch MealError.wrongStateToAddSalt {
+    print("Can't add salt at this stage")
+} catch { //let error 생략함
+    print("Some other error: \(error)")
 }
