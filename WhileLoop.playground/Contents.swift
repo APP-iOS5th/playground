6743018ba47f1286984287aa58c464fd63822cfc
SeongKookKIM <ksk8646@naver.com>
2024-04-03 11:36:38 +0900

    While Loop

--- WhileLoop.playground/Contents.swift
+++ WhileLoop.playground/Contents.swift
@@ -1,9 +1,11 @@
 import Foundation
 
 enum CoinFlip: Int {
-    case heads
-    case tails
+    // 정수형 객체는 degault 0부터 시작
+    case heads //0
+    case tails //1
     
+    // static -> 전역함수
     static func flipCoin() -> CoinFlip {
         // 특정한 랜덤 값
         return CoinFlip(rawValue: Int(arc4random_uniform(2)))!
