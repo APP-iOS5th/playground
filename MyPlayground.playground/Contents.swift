d66df221416ec71b0541ae561d7596a98cd49346
shin-jae-uk <shinju1412@gmail.com>
2024-04-03 11:50:18 +0900

    try, throw, do, catch

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,25 +1,44 @@
-// while 루프로 반복하기 (조건이 참일때까지)
+// try, throw, do, catch로 오류 처리하기
 
-import Foundation
+enum MealState {
+    case initial
+    case buyIngredients
+    case prepareIngredients
+    case cook
+    case plateUp
+    case serve
+}
 
-enum CoinFlip: Int {
-    case heads
-    case tails
+enum MealError: Error {
+    case canOnlyMoveToAppropriateState
+}
+
+class Meal {
+    private(set) var state: MealState = .initial
     
-    static func flipCoin() -> CoinFlip {
-        return CoinFlip(rawValue: Int(arc4random_uniform(2)))!
+    func change(to newState: MealState) throws {
+        switch (state, newState) {
+        case (.initial, .buyIngredients), 
+            (.buyIngredients, .prepareIngredients),
+            (.prepareIngredients, .cook),
+            (.cook, .plateUp),
+            (.plateUp, .serve):
+            state = newState
+        default:
+            throw MealError.canOnlyMoveToAppropriateState
+        }
     }
 }
 
-func howManyHeadsInARow() -> Int {
-    var numberOfHeadsInRow = 0
-    var currentCoinFlip = CoinFlip.flipCoin()
-    
-    while currentCoinFlip == .heads {       // 현재 동전이 앞면이면
-        numberOfHeadsInRow += 1
-        currentCoinFlip = CoinFlip.flipCoin()
-    }
-    return numberOfHeadsInRow
-}
+let dinner = Meal()
 
-let noOfHeads = howManyHeadsInARow()
+do {
+    try dinner.change(to: .buyIngredients)
+    try dinner.change(to: .prepareIngredients)
+    try dinner.change(to: .cook)
+    try dinner.change(to: .plateUp)
+    try dinner.change(to: .serve)
+    print("Dinner is served!")
+} catch let error {
+    print(error)
+}
