8dee9656c2678ca103127f92c9cb05ab535b67f6
MadCow <lobasketve@naver.com>
2024-04-03 14:18:55 +0900

    guard optional unwrap

--- MealState.playground/Contents.swift
+++ MealState.playground/Contents.swift
@@ -11,10 +11,23 @@ enum MealState {
 
 enum MealError: Error {
     case canOnlyMoveToAppropriateState
+    case tooMuchSalt
+    case wrongStateToAddSalt
 }
 
 class Meal {
     private(set) var state: MealState = .initial
+    private(set) var saltAdd: Int = 0
+    
+    func addSalt() throws {
+        if saltAdd >= 5 {
+            throw MealError.tooMuchSalt
+        } else if case self.state = .initial, case self.state = .buyIngredients {
+            throw MealError.wrongStateToAddSalt
+        } else {
+            saltAdd += 1
+        }
+    }
     
     func change(to newState: MealState) throws {
         switch (state, newState) {
@@ -29,17 +42,43 @@ class Meal {
             throw MealError.canOnlyMoveToAppropriateState
         }
     }
+    
+    func buyIngredients() throws {
+        try change(to: .buyIngredients)
+    }
+    
+    func prepareIngredients() throws {
+        try change(to: .prepareIngredients)
+    }
+     
+    func cook() throws {
+        try change(to: .cook)
+    }
+     
+    func plateUp() throws {
+        try change(to: .plateUp)
+    }
+     
+    func serve() throws {
+        try change(to: .serve)
+    }
 }
 
 let dinner = Meal()
 
 do {
-    try dinner.change(to: .buyIngredients)
-    try dinner.change(to: .prepareIngredients)
-    try dinner.change(to: .cook)
-    try dinner.change(to: .plateUp)
-    try dinner.change(to: .serve)
+    try dinner.buyIngredients()
+    try dinner.prepareIngredients()
+    try dinner.cook()
+    try dinner.plateUp()
+    try dinner.serve()
     print("Dinner is served!")
-} catch let error {
-    print(error)
+} catch MealError.canOnlyMoveToAppropriateState {
+    print("It's not possible to move to this state")
+} catch MealError.tooMuchSalt {
+    print("Too much salt!")
+} catch MealError.wrongStateToAddSalt {
+    print("Can't add salt at this stage")
+} catch {
+    print("Some other error: \(error)")
 }
