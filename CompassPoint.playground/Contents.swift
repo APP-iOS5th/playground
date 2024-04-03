5140e3f5590544d0e9bf881e9628be6e3e371c75
jmbae <jmbae@codegrove.co.kr>
2024-04-03 10:30:52 +0900

    combine switch

--- CompassPoint.playground/Contents.swift
+++ CompassPoint.playground/Contents.swift
@@ -19,3 +19,30 @@ func lookTowards(_ direction: CompassPoint) {
 }
 
 lookTowards(.south)
+
+enum Item {
+    case key
+    case lockedDoor
+    case openDoor
+    case bluntKnife
+    case sharpeningStone
+    case sharpKnife
+}
+
+func combine(_ firstItem: Item, with secondItem: Item) -> Item? {
+    switch(firstItem, secondItem) {
+    case (.key, .lockedDoor):
+        print("You have unlocked the door!")
+        return .openDoor
+    case (.bluntKnife, .sharpeningStone):
+        print("Your knife is now sharp")
+        return .sharpKnife
+    default:
+        print("\(firstItem) and \(secondItem) cannot be combined")
+        return nil
+    }
+}
+
+let door = combine(.key, with: .lockedDoor)
+let oilAndWather = combine(.bluntKnife, with: .lockedDoor)
+
