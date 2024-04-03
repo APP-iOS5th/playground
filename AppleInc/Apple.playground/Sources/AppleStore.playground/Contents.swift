01a5c0ac71f0771167be632eed98d404792d9571
shin-jae-uk <shinju1412@gmail.com>
2024-04-03 11:21:17 +0900

    While loop

---
+++ AppleInc/Apple.playground/Sources/AppleStore.playground/Contents.swift
@@ -0,0 +1,29 @@
+
+public enum DeviceModel {   // 외부에서 참조되지만 프리징된다?
+    case iPhone12
+    case iPhone12Mini
+    case iPhone12Pro
+    case iPhone12ProMax
+}
+
+public class AppleiPhone {
+    public let model: DeviceModel
+    
+    fileprivate init(model: DeviceModel) {
+        self.model = model
+    }
+}
+
+fileprivate class Factory {
+    func makeiPhone(ofModel model: DeviceModel) -> AppleiPhone {
+        return AppleiPhone(model: model)
+    }
+}
+
+public class AppleStore {
+    private var factory = Factory()
+    
+    public func selliPhone(ofModel model: DeviceModel) -> AppleiPhone {
+        return factory.makeiPhone(ofModel: model)
+    }
+}
--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,13 +1,25 @@
-// String Extension(확장)
+// while 루프로 반복하기 (조건이 참일때까지)
 
-extension String {
-    func firstWord() -> String {
-        let spaceIndex = firstIndex(of: " ") ?? endIndex
-        let word = prefix(upTo: spaceIndex)
-        return String(word + "(It's me!)")
+import Foundation
+
+enum CoinFlip: Int {
+    case heads
+    case tails
+    
+    static func flipCoin() -> CoinFlip {
+        return CoinFlip(rawValue: Int(arc4random_uniform(2)))!
     }
 }
 
-let llap = "Live long, and prosper"
-let firstWord = llap.firstWord()
-print(firstWord)
+func howManyHeadsInARow() -> Int {
+    var numberOfHeadsInRow = 0
+    var currentCoinFlip = CoinFlip.flipCoin()
+    
+    while currentCoinFlip == .heads {       // 현재 동전이 앞면이면
+        numberOfHeadsInRow += 1
+        currentCoinFlip = CoinFlip.flipCoin()
+    }
+    return numberOfHeadsInRow
+}
+
+let noOfHeads = howManyHeadsInARow()
