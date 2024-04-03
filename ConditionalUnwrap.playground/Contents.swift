61372318f88950510e6a8a79115a29ab49b0313b
yehjinjang <j95033176@gmail.com>
2024-04-03 09:46:37 +0900

    Optional Unwrap

--- ConditionalUnwrap.playground/Contents.swift
+++ ConditionalUnwrap.playground/Contents.swift
@@ -29,3 +29,32 @@ func printBallDetails(ofNumber number: Int){
 printBallDetails(ofNumber: 2)
 printBallDetails(ofNumber: 8)
 printBallDetails(ofNumber: 12)
+
+class PoolFrame {
+    var player1BallType: PoolBallType?
+    var player2BallType: PoolBallType?
+    
+}
+class PoolTable{
+    var currentFrame: PoolFrame?
+    
+}
+func printBallTypeOfPlayer1(forTable table: PoolTable){
+
+// 이런 중첩 조건문이 밑에 처럼 간단하게 될수있음 그것이 수위푸튜의 장점 !ㅍ 할수있긴함 
+//    let frame = table.currentFrame
+//    if(frame != nil){
+//        let ballType = frame?.player1BallType
+//        if(ballType != nil){
+//            print(ballType?.rawValue)
+//        }else {
+//            print("Player1has no ball type or there is no current ")
+//        }
+//    }
+    if let frame = table.currentFrame, let ballType = frame.player1BallType{
+        print(ballType.rawValue)
+    }else {
+        print("Player 1 has no ball type or there is no current frame")
+        
+    }
+}
