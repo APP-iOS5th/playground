4aa46da3a563f8bce637b9e69efc0bab3c50353e
jeongwookim <112370814+JeongWo@users.noreply.github.com>
2024-04-03 10:31:08 +0900

    Compasspoint

---
+++ CompasPoint.playground/Contents.swift
@@ -0,0 +1,49 @@
+
+enum CompasPoint {
+    case north
+    case south
+    case east
+    case west
+}
+
+func lookTowards(_ direction: CompasPoint) {
+    switch direction {
+    case .north:
+        print("To the north lies a winding road")
+    case .south:
+        print("To the south is the Prancing Pony tavern")
+    case .east:
+        print("To the east is a blacksmith")
+    case .west:
+        print("To the west is the town square")
+    }
+}
+
+lookTowards(.south)
+
+enum Item {
+    case key
+    case lockedDoor
+    case openDoor
+    case bluntKnife
+    case sharpeningStone
+    case sharpKnife
+}
+
+
+func combine(_ firstItem: Item, with secondItem: Item) -> Item? {
+    switch(firstItem, secondItem) {
+    case (.key, .lockedDoor):
+        print("You have unlocked the door!")
+        return .openDoor
+    case (.bluntKnife, .sharpeningStone):
+        print("Your knife is now sharp")
+        return .sharpKnife
+    default:
+        print("\(firstItem) and \(secondItem) cannot be combined")
+        return nil
+    }
+}
+
+let door = combine(.key, with: .lockedDoor)
+let oilAndWather = combine(.bluntKnife, with: .lockedDoor)
