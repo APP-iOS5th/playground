518a1af5da8df038379a20d9e8c9fa2a5d2d8aa5
yehjinjang <j95033176@gmail.com>
2024-04-03 11:26:25 +0900

    CoinFlip

--- Loop.playground/Contents.swift
+++ Loop.playground/Contents.swift
@@ -1,3 +1,32 @@
-import UIKit
 
-var greeting = "Hello, playground"
+let theBeatles = ["John","Paul","George","Ringo"]
+
+for musician in theBeatles{
+    print(musician)
+}
+
+for value in 1...12{
+    print("5 * \(value) = \(value*5)")
+}
+
+//구간표기 (...뒤에 설정안해주면 무한히 반복됨(밑에 조건문을 써서 멈추게할수있음)
+for value in 1..<12{
+    print("5 * \(value) = \(value*5)")
+}
+
+for value in 1... {
+    if value > 12{
+        break
+    }
+    print("5 * \(value) = \(value*5)")
+}
+
+let beatlesByInstrument = ["rhythm guitar": "John",
+                           "bass guitar": "Paul",
+                           "lead guitar": "George",
+                           "drums": "Ringo"]
+
+for (key, value) in beatlesByInstrument {
+    print("\(value) plays \(key)")
+}
+
