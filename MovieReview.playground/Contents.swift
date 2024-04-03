6482cee93e63003a4127f6bf071a863e9626f508
Jin Park <jinpark1504@gmail.com>
2024-04-03 14:53:17 +0900

    fatalError and precondition

--- MovieReview.playground/Contents.swift
+++ MovieReview.playground/Contents.swift
@@ -12,9 +12,12 @@ enum ClassificationState {
 }
 class MovieReviewClassifier {
     var state: ClassificationState = .initial
+    // for error from precondition
+//    var state: ClassificationState = .classifying
     
     func classify(forStarsOutOf10 stars: Int) -> MovieReviewClass {
-        
+        precondition(state == .initial, "Classifier state must be initial")
+        assert(state == .initial)
         state = .classifying
         
         defer {
@@ -26,8 +29,10 @@ class MovieReviewClassifier {
             return .good
         } else if stars > 3 {
             return .average
-        } else {
+        } else if stars > 0{
             return .bad
+        } else {
+            fatalError("Star rating must be between 1 and 10")
         }
     }
 }
@@ -35,5 +40,7 @@ class MovieReviewClassifier {
 let classifier = MovieReviewClassifier()
 
 let review1 = classifier.classify(forStarsOutOf10: 6)
+// for error from fatalError
+//let review1 = classifier.classify(forStarsOutOf10: -1)
 print(review1)
 
