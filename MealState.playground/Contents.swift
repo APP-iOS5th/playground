952d604ed758c237c01868df96fcd9bfe9b066e1
Cha, Jiyong <wldyd2113@gmail.com>
2024-04-03 13:11:25 +0900

    throw with function call

--- MealState.playground/Contents.swift
+++ MealState.playground/Contents.swift
@@ -14,7 +14,7 @@ enum MealError: Error{
 class Meal {
     private(set) var state: MealState = .initial //상태변경을 외부에 공개하고 싶지 않으면 private(set) 클래스 내부에서만 사용
     func change(to newState: MealState) throws {
-        switch (state, newState){
+        switch (state, newState){ //state:원래 상태, newState: 다음 상태를 나타냄 여기 구문에서
         case (.initial, .buyIngregients),(.buyIngregients, .prepareIngredients),(.prepareIngredients, .cook),
             (.cook, .plateUp),
             (.plateUp, .serve):
@@ -24,6 +24,25 @@ class Meal {
             
         }
     }
+    func buyIngredients() throws {
+        try change(to: .buyIngregients)
+    }
+    
+    func prepareIngredients() throws {
+        try change(to: .prepareIngredients)
+    }
+     
+    func cook() throws {
+        try change(to: .cook)
+    }
+     
+    func plateUp() throws {
+        try change(to: .plateUp)
+    }
+     
+    func serve() throws {
+        try change(to: .serve)
+    }
 }
 
 let dinner = Meal()
@@ -32,14 +51,14 @@ let dinner = Meal()
  do 구문안에 있는거 안전하게 지켜줌
  */
 do {
-    try dinner.change(to: .buyIngregients)
-    try dinner.change(to: .prepareIngredients)
-    try dinner.change(to: .cook)
-    try dinner.change(to: .plateUp)
-    try dinner.change(to: .serve)
+    try dinner.buyIngredients()
+    try dinner.prepareIngredients()
+    try dinner.cook()
+    try dinner.plateUp()
+    try dinner.serve()
     print("Dinner is served!")
     
     
-}catch let error{
+} catch let error {
     print(error)
 }
