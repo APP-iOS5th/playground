9344bd5b60955585358ec1bd308a8863dfe84377
hwang <jangku88@naver.com>
2024-04-03 10:31:43 +0900

    Combine Switch

--- CompassPoint.playground/Contents.swift
+++ CompassPoint.playground/Contents.swift
@@ -25,3 +25,37 @@ func lookTowrds(_ direction: CompassPoint) {
 }
 
 lookTowrds(.south)
+
+
+
+/// 조합할 수 있는 아이템의 열거형을 정의
+enum Item {
+    case key
+    case lockedDoor
+    case openDoor
+    case bluntKnife
+    case sharpeningStone
+    case sharpKnife
+}
+
+/// 두 아이템을 결합하여 새 아이템을 만드는 함수
+/// - Parameters:
+///   - firstItem: 첫 번째 아이템
+///   - secondItem: 두 번째 아이템
+/// - Returns: 두 아이템의 결합 조건에 맞는 새로운 아이템을 반환하고 결합할 수 없는 경우 nil을 반환.
+func combine(_ firstItem: Item, with secondItem: Item) -> Item? { // Switch 문의 특징 : 복합으로 가능
+    switch(firstItem, secondItem) {
+    case (.key, .lockedDoor):
+        print("You have unlocked the door!")
+        return .openDoor
+    case (.bluntKnife, .sharpeningStone):
+        print("Your knife is now sharp")
+        return .sharpKnife
+    default:
+        print("\(firstItem) and \(secondItem) cannot be combined")
+        return nil
+    }
+}
+
+let door = combine(.key, with: .lockedDoor)
+let oilAndWather = combine(.bluntKnife, with: .lockedDoor)
