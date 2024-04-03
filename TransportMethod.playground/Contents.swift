adb6b470c8aec52b2a22d672624b1490163e2ada
jihye <ganzi410@gmail.com>
2024-04-03 17:22:59 +0900

    extension with protocol generic

--- TransportMethod.playground/Contents.swift
+++ TransportMethod.playground/Contents.swift
@@ -12,8 +12,14 @@ protocol TransportMethod {
 
 struct Train: TransportMethod {
     typealias CollectionPoint = TrainStation
-    var defaultCollectionPoint: TrainStation { return TrainStation.BMS }
-    var averageSpeedInKPH: Double { return 100 }
+    
+    var defaultCollectionPoint: TrainStation {
+        return TrainStation.BMS
+    }
+    
+    var averageSpeedInKPH: Double {
+        return 100
+    }
 }
 
 enum TrainStation: String, TransportLocation {
@@ -39,16 +45,59 @@ enum TrainStation: String, TransportLocation {
         }
     }
 }
-}
 
 class Journey<TransportType: TransportMethod> {
     let start: TransportType.CollectionPoint
     let end: TransportType.CollectionPoint
     let method: TransportType
+    var distanceInKMs: Double
+    var durationInHours: Double
     
-    init(start: TransportType.CollectionPoint, end: TransportType.CollectionPoint, method: TransportType) {
+    init(start: TransportType.CollectionPoint,
+         end: TransportType.CollectionPoint,
+         method: TransportType) {
         self.start = start
         self.end = end
         self.method = method
+        distanceInKMs = end.location.distance(from: start.location) / 1000
+        durationInHours = distanceInKMs / method.averageSpeedInKPH
     }
 }
+
+extension CLLocation: TransportLocation {
+    var location: CLLocation {
+        return self
+    }
+}
+
+enum Road: TransportMethod {
+    typealias CollectionPoint = CLLocation
+    
+    case car
+    case motobike
+    case van
+    case hgv
+    
+    var defaultCollectionPoint: CLLocation {
+        return CLLocation(latitude: 51.1, longitude: 0.1)
+    }
+    
+    var averageSpeedInKPH: Double {
+        switch self {
+        case .car: return 60
+        case .motobike: return 70
+        case .van: return 55
+        case .hgv: return 50
+        }
+    }
+}
+
+let start = CLLocation(latitude: 51.3994669, longitude: 0.0116888)
+let end = CLLocation(latitude: 51.2968654, longitude: 0.5053609)
+let roadJourney = Journey(start: start, end: end, method: Road.car)
+
+let distanceByRoad = roadJourney.distanceInKMs
+let durationByRoad = roadJourney.durationInHours
+
+print("Journey distance: \(distanceByRoad) km")
+print("Journey duration: \(durationByRoad) hours")
