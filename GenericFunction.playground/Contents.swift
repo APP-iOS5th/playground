db56bf0dbaea2b4b2aae8284a6767b789a32f91b
hwang <jangku88@naver.com>
2024-04-03 16:08:23 +0900

    Generic Function

--- GenericFunction.playground/Contents.swift
+++ GenericFunction.playground/Contents.swift
@@ -1,9 +1,11 @@
 
 /**  제네릭을 사용한 함수 사용  **/
+/// 1. 제네릭 함수 생성
+///     • 여러 키에 대해 동일한 값을 딕셔너리에 삽입하는 제네릭 함수 생성.
 
 // 내가 나중에 결절할 타입의 이름이 아이템이야
-func makeDuplicates<ItemType>(of item: ItemType, withKeys keys: Set<String>) -> [String: ItemType] {
-    var duplicates = [String: ItemType]()
+func makeDuplicates<ItemType, KeyType: Hashable>(of item: ItemType, withKeys keys: Set<KeyType>) -> [KeyType: ItemType] {
+    var duplicates = [KeyType: ItemType]()
     for key in keys {
         duplicates[key] = item
     }
