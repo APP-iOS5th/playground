34a2f4d68977786fbf19c66d2ba138aa4ae377d9
hwang <jangku88@naver.com>
2024-04-03 15:30:59 +0900

    Generic, update RencentList used generic

--- Generic.playground/Contents.swift
+++ Generic.playground/Contents.swift
@@ -2,17 +2,20 @@
 /**  Generic   **/
 /// 1. RecentList 생성
 ///     • 사용자가 복사한 마지막 다섯 개의 문자열을 저장하는 커스텀 컬렉션 객체를 만듬.
+/// 2. 제네릭을 사용하여 RecentList 업데이트
 
-class RecentList {
-    var slot1: String?
-    var slot2: String?
-    var slot3: String?
-    var slot4: String?
-    var slot5: String?
+
+// ListItemType 이라는 대명사, 나중에 들어올 타입이라고 정의. (호출할 때 지정한 타입으로 변경됨.)
+class RecentList<ListItemType> {
+    var slot1: ListItemType?
+    var slot2: ListItemType?
+    var slot3: ListItemType?
+    var slot4: ListItemType?
+    var slot5: ListItemType?
     
     /// 각 slot을 하나씩 아래로 이동
     /// - Parameter recent: recent slot
-    func add(recent: String) {
+    func add(recent: ListItemType) {
         slot5 = slot4
         slot4 = slot3
         slot5 = slot2
@@ -22,8 +25,8 @@ class RecentList {
     
     /// slot에 있는 문자열 반환해주는 함수
     /// - Returns: slot에 있는 문자열 반환
-    func getAll() -> [String] {
-        var recent = [String]()
+    func getAll() -> [ListItemType] {
+        var recent = [ListItemType]()
         
         if let slot1 = slot1 {
             recent.append(slot1)
@@ -44,7 +47,7 @@ class RecentList {
     }
 }
 
-let recentlyCopiedList = RecentList()
+let recentlyCopiedList = RecentList<String>()
 recentlyCopiedList.add(recent: "First")
 recentlyCopiedList.add(recent: "Next")
 recentlyCopiedList.add(recent: "Last")
