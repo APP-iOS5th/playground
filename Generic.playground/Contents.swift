181280d7baa9968c20bce004768380ff29827f85
wotjs020708 <wotjs020708@gmail.com>
2024-04-03 15:42:34 +0900

    Generic person

--- Generic.playground/Contents.swift
+++ Generic.playground/Contents.swift
@@ -40,3 +40,29 @@ recentlyCopiedList.add(recent: "Last")
 
 var recentlyCopied = recentlyCopiedList.getAll()
 print(recentlyCopied)
+
+
+class Person{
+    let name: String
+    init(name: String) {
+        self.name = name
+    }
+}
+
+let rod = Person(name: "Rod")
+let jane = Person(name: "Jane")
+let freddy = Person(name: "Freddy")
+
+let lastCalledList = RecentList<Person>()
+
+
+lastCalledList.add(recent: rod)
+lastCalledList.add(recent: jane)
+lastCalledList.add(recent: freddy)
+
+let lastCalled = lastCalledList.getAll()
+for person in lastCalled {
+    print(person.name)
+}
+
+
