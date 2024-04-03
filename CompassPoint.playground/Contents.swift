7dc2c9abc1c478353141599336d995844dd793f2
MadCow <lobasketve@naver.com>
2024-04-03 10:08:38 +0900

    switch

---
+++ CompassPoint.playground/Contents.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+enum CompassPoint {
+    case north
+    case south
+    case east
+    case west
+}
+
+func lookTowards(direction: CompassPoint) {
+    switch direction {
+        case .north: print("To the north lies a winding road")
+        case .south: print("To the south is the Prancing Pony tavern")
+        case .east: print("To the east is a blacksmith")
+        case .west: print("To the west is the town square")
+    }
+}
+
+lookTowards(direction: .west)
