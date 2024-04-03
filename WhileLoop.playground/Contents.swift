ab684b17c11c9544c92568c542b04169e452bb8a
jmbae <jmbae@codegrove.co.kr>
2024-04-03 11:22:51 +0900

    While Loop

---
+++ WhileLoop.playground/Contents.swift
@@ -0,0 +1,20 @@
+import Foundation
+
+enum CoinFlip: Int {
+    case heads
+    case tails
+    
+    static func flipCoin() -> CoinFlip {
+        return CoinFlip(rawValue: Int(arc4random_uniform(2)))!
+    }
+}
+
+func howManyHeadsInARow() -> Int {
+    var numberOfHeadsInRow = 0    
+    while CoinFlip.flipCoin() == .heads {
+        numberOfHeadsInRow = numberOfHeadsInRow + 1
+    }
+    return numberOfHeadsInRow
+}
+
+let noOfHeads = howManyHeadsInARow()
