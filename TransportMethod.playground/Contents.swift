9c34b47696a74bbd1c97a7c1c490692c14ea9988
jihye <ganzi410@gmail.com>
2024-04-03 16:39:16 +0900

    Protocol Generic

---
+++ TransportMethod.playground/Contents.swift
@@ -0,0 +1,31 @@
+
+protocol TransportMethod {
+    associatedtype CollectionPoint
+    var defaultCollectionPoint: CollectionPoint { get }
+    var averageSpeedInKPH: Double { get }
+}
+
+struct Train: TransportMethod {
+    typealias CollectionPoint = TrainStation
+    var defaultCollectionPoint: TrainStation { return TrainStation.BMS }
+    var averageSpeedInKPH: Double { return 100 }
+}
+
+enum TrainStation: String {
+    case BMS = "Bromley South"
+    case VIC = "London Victoria"
+    case RAI = "Rainham (Kent)"
+    case BTN = "Brighton (East Sussex)"
+}
+
+class Journey<TransportType: TransportMethod> {
+    let start: TransportType.CollectionPoint
+    let end: TransportType.CollectionPoint
+    let method: TransportType
+    
+    init(start: TransportType.CollectionPoint, end: TransportType.CollectionPoint, method: TransportType) {
+        self.start = start
+        self.end = end
+        self.method = method
+    }
+}
