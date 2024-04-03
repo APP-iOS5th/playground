67eba33ec6b2751c9f34862e79b59b03a1aaf4fa
Cha, Jiyong <wldyd2113@gmail.com>
2024-04-03 17:25:49 +0900

    extension with protocol generic

--- TransprortMethod.playground/Contents.swift
+++ TransprortMethod.playground/Contents.swift
@@ -46,10 +46,12 @@ enum TrainStation: String, TransportLocation {
     }
 }
 
-class Journey<TransportType: TransportMethod> {
+class Journey<TransportType: TransportMethod> { //제네릭이랑 프로토콜 같이 사용
     let start: TransportType.CollectionPoint
     let end: TransportType.CollectionPoint
     let method: TransportType
+    var distanceInKMs: Double
+    var durationInHours:Double
     
     init(start: TransportType.CollectionPoint,
          end: TransportType.CollectionPoint,
@@ -57,6 +59,46 @@ class Journey<TransportType: TransportMethod> {
         self.start = start
         self.end = end
         self.method = method
+        distanceInKMs = end.location.distance(from: start.location) / 1000
+        durationInHours = distanceInKMs/method.averageSpeedInKPH
     }
     
 }
+
+extension CLLocation: TransportLocation {
+    var location: CLLocation{
+        return self
+    }
+}
+
+enum Road: TransportMethod {
+    typealias CollectionPoint = CLLocation
+    
+    case car
+    case motobike
+    case van
+    case hgv
+    
+    var defaultCollectionPoint: CLLocation {
+        return CLLocation(latitude: 51.1, longitude: 0.1)
+    }
+    
+    var averageSpeedInKPH: Double {
+        switch self {
+        case .car: return 60
+        case .motobike: return 70
+        case .van: return 50
+        case .hgv: return 50
+            
+        }
+    }
+}
+let start = CLLocation(latitude: 51.3994669, longitude: 0.0116888)
+let end = CLLocation(latitude: 51.2968654, longitude: 0.5053609)
+let roadJourney = Journey(start: start, end: end, method: Road.car)
+
+let distanceByRoad = roadJourney.distanceInKMs
+let durationByRoad = roadJourney.durationInHours
+
+print("Journey distance: \(distanceByRoad) km")
+print("Journey duration: \(durationByRoad) hours")
