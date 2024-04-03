1c1fb4ba12a51bce52a551f90189d81f055e3c5d
Cha, Jiyong <wldyd2113@gmail.com>
2024-04-03 09:05:21 +0900

    AppleInc 에서 Playground 파일 삭제

--- AppleInc/AppleStore.swift
+++ AppleInc/AppleStore.swift
@@ -1,29 +1,30 @@
-
 public enum DeviceModel {
-    case iPhone12
-    case iPhone12Mini
-    case iPhone12Pro
-    case iPhone12ProMax
+    case iPhone13
+    case iPhone13Mini
+    case iPhone13Pro
+    case iPhone13ProMax
 }
 
-public class AppleiPhone { //외부에서 수정불가
+public class AppleiPhone {
+    
     public let model: DeviceModel
     
-    fileprivate init(model: DeviceModel){
+    fileprivate init(model: DeviceModel) {
         self.model = model
     }
 }
 
 fileprivate class Factory {
-    func makeiPhone(ofModel model: DeviceModel) -> AppleiPhone{
+    func makeiPhone(ofModel model: DeviceModel) -> AppleiPhone {
         return AppleiPhone(model: model)
     }
 }
 
 public class AppleStore {
+    
     private var factory = Factory()
+    
     public func selliPhone(ofModel model: DeviceModel) -> AppleiPhone {
         return factory.makeiPhone(ofModel: model)
     }
-    
 }
--- AppleInc/SecretProductDepartment.swift
+++ AppleInc/SecretProductDepartment.swift
@@ -1,10 +1,12 @@
+
 class SecretProductDepartment {
-    private var secretCodeWord =  "Titan"
+    
+    private var secretCodeWord = "Titan"
     private var secretProducts = ["Apple Glasses",
                                   "Apple Car",
                                   "Apple Brain Implant"]
     
-    func nextProduct(codeWord: String) -> String? {
+    func nextProduct(givenCodeWord codeWord: String) -> String? {
         let codeCorrect = codeWord == secretCodeWord
         return codeCorrect ? secretProducts.first : nil
     }
