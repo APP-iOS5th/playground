bd4707c15b9256e9f7c08ff6f0ba1125b09ee249
Ruel <c0407h@gmail.com>
2024-04-03 11:38:45 +0900

    While Loop

--- WhileLoop.playground/Contents.swift
+++ WhileLoop.playground/Contents.swift
@@ -1,3 +1,24 @@
-import UIKit
+import Foundation
 
-var greeting = "Hello, playground"
+enum CoinFlip: Int {
+    case heads
+    case tails
+    
+    static func flipCoin() -> CoinFlip {
+        return CoinFlip(rawValue: Int(arc4random_uniform(2)))!
+    }
+}
+
+func howManyHeadsInARow() -> Int {
+    var numberOfHeadsInRow = 0
+    var currentCoinFlip = CoinFlip.flipCoin()
+    
+    while currentCoinFlip == .heads {
+        numberOfHeadsInRow += 1
+        currentCoinFlip = CoinFlip.flipCoin()
+    }
+    
+    return numberOfHeadsInRow
+}
+
+let noOfHeads = howManyHeadsInARow()
