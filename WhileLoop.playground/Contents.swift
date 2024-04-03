ab9184e14556b168caefd42bff11caddc0330061
Jinx <jinxwift@gmail.com>
2024-04-03 11:20:38 +0900

    while loop

---
+++ WhileLoop.playground/Contents.swift
@@ -0,0 +1,23 @@
+import Foundation
+
+enum CoinFlip: Int {
+    case heads
+    case tails
+    
+    static func flipCoin() -> CoinFlip {
+        return CoinFlip(rawValue: Int(arc4random_uniform(2)))!
+    }
+}
+
+func howManyHeadsInARow() -> Int {
+    var numberOfHeadsInRow = 0
+    var currentCoinFlip = CoinFlip.flipCoin()
+
+    while currentCoinFlip == .heads {
+    numberOfHeadsInRow = numberOfHeadsInRow + 1
+    currentCoinFlip = CoinFlip.flipCoin()
+    }
+    return numberOfHeadsInRow
+}
+
+let noOfHeads = howManyHeadsInARow()
