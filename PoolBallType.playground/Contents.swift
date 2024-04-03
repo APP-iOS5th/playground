d3290d558a92682e0f71137d613eb8a2bca34815
Jongchan Yun <jcyun1996@gmail.com>
2024-04-03 09:25:29 +0900

    If

--- PoolBallType.playground/Contents.swift
+++ PoolBallType.playground/Contents.swift
@@ -4,3 +4,21 @@ enum PoolBallType {
     case stripe
     case black
 }
+
+func poolBallType(forNumber number: Int) -> PoolBallType? {
+    if number > 0 && number < 8 {
+        return .solid
+    } else if number > 8 {
+        return .stripe
+    } else if number == 8 {
+        return .black
+    } else {
+        return nil
+    }
+}
+
+let two = poolBallType(forNumber: 2)
+let eight = poolBallType(forNumber: 8)
+let twelve = poolBallType(forNumber: 12)
+let zero = poolBallType(forNumber: 0)
+let sixteen = poolBallType(forNumber: 16)
