dd991c6a0df72a797e7eebee47bdc486ee25c452
Jinx <jinxwift@gmail.com>
2024-04-03 16:48:17 +0900

    assoicated type with generic and protocol

--- TransportMethod.playground/Contents.swift
+++ TransportMethod.playground/Contents.swift
@@ -1,7 +1,7 @@
 import CoreLocation
 
 protocol TransportMethod {
-    associatedtype CollectionPoint
+    associatedtype CollectionPoint: TransportLocation
     var defaultCollectionPoint: CollectionPoint { get }
     var averageSpeedInKPH: Double { get }
 }
@@ -15,14 +15,31 @@ struct Train: TransportMethod {
     var averageSpeedInKPH: Double {
         return 100
     }
-        
+    
 }
 
-enum TrainStation: String {
+enum TrainStation: String, TransportLocation {
     case BMS = "Bromley South"
     case VIC = "London Victoria"
     case RAI = "Rainham (Kent)"
     case BTN = "Brighton (East Sussex)"
+    
+    var location: CLLocation {
+        switch self {
+        case .BMS:
+            return CLLocation(latitude: 51.4000504,
+                              longitude: 0.0174237)
+        case .VIC:
+            return CLLocation(latitude: 51.4952103,
+                              longitude: -0.1438979)
+        case .RAI:
+            return CLLocation(latitude: 51.3663,
+                              longitude: 0.61137)
+        case .BTN:
+            return CLLocation(latitude: 50.829,
+                              longitude: -0.14125)
+        }
+    }
 }
 
 protocol TransportLocation {
