d1c28cbf07df296cec7916f2cd13fbcba1e079c1
Hyunseo Yook <hhh3332@naver.com>
2024-04-03 11:50:13 +0900

    try, do, catch

---
+++ MealState.playground/Contents.swift
@@ -0,0 +1,42 @@
+
+enum MealState {
+    case initial
+    case buyIngredients
+    case prepareIngrdients
+    case cook
+    case plateUp
+    case serve
+}
+enum MealError: Error {
+    case canOnlyMoveToAppropriateState
+}
+
+class Meal {
+    private(set) var state: MealState = .initial
+    
+    func change(to newState: MealState) throws {
+        switch (state, newState) {
+        case (.initial, .buyIngredients),
+            (.buyIngredients, .prepareIngrdients),
+            (.prepareIngrdients, .cook),
+            (.cook, .plateUp),
+            (.plateUp, .serve):
+            state = newState
+        default:
+            throw MealError.canOnlyMoveToAppropriateState
+        }
+    }
+}
+
+let dinner = Meal()
+
+do {
+    try dinner.change(to: .buyIngredients)
+    try dinner.change(to: .prepareIngrdients)
+    try dinner.change(to: .cook)
+    try dinner.change(to: .plateUp)
+    try dinner.change(to: .serve)
+    print("Dinner is served")
+} catch let error {
+    print(error)
+}
