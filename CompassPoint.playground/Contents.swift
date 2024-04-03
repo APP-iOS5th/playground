6d259e2e6e05fddb95da33608ce49adad51ecb6f
Cha, Jiyong <wldyd2113@gmail.com>
2024-04-03 10:49:19 +0900

    ubderscore wildcard on switch statement

--- CompassPoint.playground/Contents.swift
+++ CompassPoint.playground/Contents.swift
@@ -47,3 +47,32 @@ func combine(_ firstItem: Item, with secondItem: Item) -> Item? {
 
 let door = combine(.key, with: .lockedDoor)
 let oilAndWather = combine(.bluntKnife, with: .lockedDoor)
+
+enum Character: String {
+    case wizard
+    case bartender
+    case dragon
+}
+
+func say(_ textToSay: String, to character: Character? = nil) {
+    switch(textToSay, character) {
+    case ("abracadabra", .wizard?):
+        print("The wizard says, \"Hey, that's my line!\"")
+        
+    case ("Pour me a drink", .bartender?):
+        print("The bartender pours you a drink")
+        
+    case ("Can I have some of your gold?", .dragon?):
+        print("The dragon burns you to death with his fiery breath")
+        
+    case (let textSaid, nil): //textToSay를 textSaid로 바꿈
+        print("You say \"\(textSaid)\", to no-one.")
+        
+    case (_, let anyCharacter?): // _(와일드카드 식별자): any 무엇이든 될수 있음
+        print("The \(anyCharacter) looks at you, blankly")
+    }
+}
+say("Is anybody there?")
+say("Pour me a drink", to: .bartender)
+say("Can I open a tab?", to: .bartender)
+say("Hello", to: .dragon)
