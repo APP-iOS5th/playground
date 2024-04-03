2fda03dc82cd933f5f0a45660ad072ca46a573bb
Cha, Jiyong <wldyd2113@gmail.com>
2024-04-03 11:23:25 +0900

    While Loop

--- WhileLoop.playground/Contents.swift
+++ WhileLoop.playground/Contents.swift
@@ -1,3 +1,21 @@
-import UIKit
+import Foundation
 
-var greeting = "Hello, playground"
+enum Coinflip: Int {
+    case heads
+    case tails
+    
+    static func flipCoin() -> Coinflip {
+        return Coinflip(rawValue: Int(arc4random_uniform(2)))!
+    }
+}
+
+func howManyHeadInARow() -> Int {
+    var numberOfHeadsInRow = 0
+    
+    while Coinflip.flipCoin() == .heads{
+        numberOfHeadsInRow = numberOfHeadsInRow + 1
+    }
+    return numberOfHeadsInRow
+}
+
+let noOfHeads = howManyHeadInARow()
