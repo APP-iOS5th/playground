e2b00dffc75dd217d07c66fc9cf25adfd6f081b2
Jin Park <jinpark1504@gmail.com>
2024-04-03 16:09:20 +0900

    generic with protocol

--- GenericFunction.playground/Contents.swift
+++ GenericFunction.playground/Contents.swift
@@ -1,3 +1,14 @@
-import UIKit
 
-var greeting = "Hello, playground"
+func makeDuplicates<ItemType, KeyType: Hashable>(of item: ItemType, withKeys keys: Set<KeyType>) -> [KeyType: ItemType] {
+    var duplicates = [KeyType: ItemType]()
+    for key in keys {
+        duplicates[key] = item
+    }
+    return duplicates
+}
+
+let awards: Set<String> = ["Best Director", "Best Picture", "Best Original Screenplay", "Best International Feature"]
+
+let oscars2020 = makeDuplicates(of: "Parasite", withKeys: awards)
+print(oscars2020["Best Picture"] ?? "")
+print(oscars2020["Best International Feature"] ?? "")
