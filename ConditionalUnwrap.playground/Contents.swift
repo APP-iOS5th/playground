5c2030400f6f982ea362385be57b4ab883e27f38
jmbae <jmbae@codegrove.co.kr>
2024-04-03 09:50:50 +0900

    PoolFrame multiple conditional unwraping

--- ConditionalUnwrap.playground/Contents.swift
+++ ConditionalUnwrap.playground/Contents.swift
@@ -31,3 +31,49 @@ printBallDetails(ofNumber: 8)
 printBallDetails(ofNumber: 12)
 printBallDetails(ofNumber: 0)
 printBallDetails(ofNumber: 16)
+
+class PoolFrame {
+    var player1BallType: PoolBallType?
+    var player2BallType: PoolBallType?
+}
+
+class PoolTable {
+    var currentFrame: PoolFrame?
+}
+
+func printBallTypeOfPlayer1(forTable table: PoolTable) {
+//    전통적인 방법
+//    let frame = table.currentFrame
+//    if(frame != nil) {
+//        let ballType = frame?.player1BallType
+//        if(ballType != nil) {
+//            print(ballType.rawValue)
+//        } else {
+//            print("Player 1 has no ball type or there is no current frame")
+//        }
+//    } else {
+//        print("Player 1 has no ball type or there is no current frame")
+//
+//    }
+    
+    if let frame = table.currentFrame, let ballType = frame.player1BallType {
+        print(ballType.rawValue)
+    } else {
+        print("Player 1 has no ball type or there is no current frame")
+    }
+}
+
+let table = PoolTable()
+table.currentFrame = nil
+printBallTypeOfPlayer1(forTable: table)
+
+let frame = PoolFrame()
+frame.player1BallType = nil
+frame.player2BallType = nil
+table.currentFrame = frame
+printBallTypeOfPlayer1(forTable: table)
+
+frame.player1BallType = .solid
+frame.player2BallType = .stripe
+
+printBallTypeOfPlayer1(forTable: table)
