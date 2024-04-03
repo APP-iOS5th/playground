bb5a3c06e3e4dde7d24e51b6f9145fe2e75dcccc
SeongKookKIM <ksk8646@naver.com>
2024-04-03 16:08:10 +0900

    Generic Function

---
+++ GenericFunction.playground/Contents.swift
@@ -0,0 +1,15 @@
+
+func makeDuplicates<ItemType, KeyType: Hashable>(of item: ItemType, withKey keys: Set<KeyType>) -> [KeyType:ItemType]{
+    var duplicates = [KeyType: ItemType]()
+    for key in keys {
+        duplicates[key] = item
+    }
+    return duplicates
+}
+
+let awards: Set<String> = ["Best Director", "Best Picture", "Best Original Screenplay", "Best International Feature"]
+let oscars2020 = makeDuplicates(of: "parasite", withKey: awards)
+
+print(oscars2020["Best Picture"] ?? "")
+print(oscars2020["Best International Feature"] ?? "")
+print(oscars2020["Best"] ?? "")
