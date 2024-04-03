ccf99d20572817c3f27bed5a8f94e6805228e70b
jihye <ganzi410@gmail.com>
2024-04-03 10:49:04 +0900

    underscore wildcard on switch statement

--- CompassPoint.playground/Contents.swift
+++ CompassPoint.playground/Contents.swift
@@ -20,3 +20,54 @@ func lookTowards(_ direction: CompassPoint) {
 }
 
 lookTowards(.south)
+
+enum Item {
+    case key
+    case lockedDoor
+    case openDoor
+    case bluntKnife
+    case sharpningStone
+    case sharpKnife
+}
+
+func combine(_ firstItem: Item, with secondItem: Item) -> Item? {
+    switch (firstItem, secondItem) {
+    case (.key, .lockedDoor):
+        print("You have unlocked the door!")
+        return .openDoor
+    case (.bluntKnife, .sharpningStone):
+        print("Your knife is now sharp")
+        return .sharpKnife
+    default:
+        print("\(firstItem) and \(secondItem) cannot be combined")
+        return nil
+    }
+}
+
+let door = combine(.key, with: .lockedDoor)
+let oilAndWather = combine(.bluntKnife, with: .lockedDoor)
+
+enum Character: String {
+    case wizard
+    case bartender
+    case dragon
+}
+
+func say(_ textToSay: String, to character: Character? = nil) {
+    switch (textToSay, character) {
+    case("abracadabra", .wizard?):
+        print("The wizard says, \"Hey, that's my line!\"")
+    case ("Pour me a drink", .bartender?):
+        print("The bartender pours you a drink")
+    case ("Can I have some of your gold?", .dragon?):
+        print("The dragon burns you to death with his fiery breath")
+    case (let textSaid, nil):
+        print("You say \"\(textSaid)\", to no-one.")
+    case (_, let anyCharacter?):
+        print("The \(anyCharacter) looks at you, blankly")
+    }
+}
+
+say("Is anybody there?")
+say("Pour me a drink", to: .bartender)
+say("Can I open a tab", to: .bartender)
