65c899f9b74d2637ca7f5100f590a092e6a5d2fa
uunwon <chea2089@naver.com>
2024-04-03 09:25:01 +0900

    If Else description

--- Apple.playground/Sources/AppleStore.swift
+++ Apple.playground/Sources/AppleStore.swift
@@ -1,8 +1,34 @@
-//
-//  AppleStore.swift
-//  AppleInc
-//
-//  Created by uunwon on 4/3/24.
-//
+// 🍎 애플 공장이 돌아간다는 걸 본다고 생각하자 ✨
+// 우리는 아이폰 모델명은 볼 수 있다
+public enum DeviceModel {
+    case iPhone12
+    case iPhone12Mini
+    case iPhone12Pro
+    case iPhone12ProMax
+}
 
-import Foundation
+public class AppleiPhone {
+    public let model: DeviceModel
+    
+    fileprivate init(model: DeviceModel) {
+        self.model = model
+    }
+}
+
+// 공장은 외부에 공개되어있지 않다 🏭
+fileprivate class Factory {
+    // 아이폰 생성 메소드
+    func makeiPhone(ofModel model: DeviceModel) -> AppleiPhone {
+        return AppleiPhone(model: model)
+    }
+}
+
+// 스토어는 외부에 공개되어있다 🏬
+public class AppleStore {
+    private var factory = Factory()
+    
+    // 우리는 공장은 못보지만 살 수만 있다
+    public func selliPhone(ofModel model: DeviceModel) -> AppleiPhone {
+        return factory.makeiPhone(ofModel: model)
+    }
+}
--- Apple.playground/Sources/SwiftLanguage.swift
+++ Apple.playground/Sources/SwiftLanguage.swift
@@ -1,8 +1,11 @@
-//
-//  SwiftLanguage.swift
-//  AppleInc
-//
-//  Created by uunwon on 4/3/24.
-//
-
-import Foundation
+// ♥️ open ✨은 수정 삭제 전부 다 공개되는 거임
+// swift 언어는 open 을 강조함
+open class SwiftLanguage {
+    open func versionNumber() -> Float {
+        return 5.0
+    }
+    
+    open func supportedPlatforms() -> [String] {
+        return ["iOS", "macOSX", "tvOS", "watchOS", "Linux"]
+    }
+}
