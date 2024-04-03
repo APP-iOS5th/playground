0a2ec9341d12ce9643c09d0714d9fe03e04359f6
Jihye Park <deviloper.park@gmail.com>
2024-04-03 13:33:21 +0900

    Multiple errors

--- MealState.playground/Contents.swift
+++ MealState.playground/Contents.swift
@@ -9,11 +9,24 @@ enum MealState {
 
 enum MealError: Error {
     case canOnlyMoveToAppropriateState
+    case tooMuchSalt
+    case wrongStateToAddSalt
 }
 
 class Meal {
     private(set) var state: MealState = .initial
     
+    private(set) var saltAdded = 0
+    
+    func addSalt() throws -> Void{
+        if saltAdded >= 5 {
+            throw MealError.tooMuchSalt
+        } else if case .initial = state, case.buyIngredients = state {
+            throw MealError.wrongStateToAddSalt // 소금이 준비되기 전에도 소금을 넣으면 에러 발생
+        } else {
+            saltAdded = saltAdded + 1
+        }
+    }
     func change(to newState: MealState) throws {
         switch (state, newState) {
         // 순서 정의
@@ -27,6 +40,27 @@ class Meal {
             throw MealError.canOnlyMoveToAppropriateState
         }
     }
+
+// change function과 동일
+//    func buyIngredients() throws {
+//        try change(to: .buyIngredients)
+//    }
+//    
+//    func prepareIngredients() throws {
+//        try change(to: .prepareIngredients)
+//    }
+//     
+//    func cook() throws {
+//        try change(to: .cook)
+//    }
+//     
+//    func plateUp() throws {
+//        try change(to: .plateUp)
+//    }
+//     
+//    func serve() throws {
+//        try change(to: .serve)
+//    }
 }
 
 let dinner = Meal()
@@ -38,6 +72,26 @@ do {
     try dinner.change(to: .plateUp)
     try dinner.change(to: .serve)
     print("Dinner is served!")
+} catch MealError.canOnlyMoveToAppropriateState  {
+    print("It's not possible to move to this state")
+} catch MealError.tooMuchSalt {
+    print("Too much salt!")
+} catch MealError.wrongStateToAddSalt {
+    print("Can't add salt at this stage")
 } catch let error {
-    (error)
+    print("Some other error: \(error)")
 }
+
+// 주석 처리한 function 사용 시 코드
+//let dinner = Meal()
+//
+//do {
+//    try dinner.buyIngredients()
+//    try dinner.prepareIngredients()
+//    try dinner.cook()
+//    try dinner.plateUp()
+//    try dinner.serve()
+//    print("Dinner is served!")
+//} catch let error {
+//    (error)
+//}
