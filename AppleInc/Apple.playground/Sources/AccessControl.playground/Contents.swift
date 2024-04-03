e1fcf441159b52f507af324ac0a1ec5b48f50f0c
Jinx <jinxwift@gmail.com>
2024-04-03 09:24:46 +0900

    poolballtype

---
+++ AppleInc/Apple.playground/Sources/AccessControl.playground/Contents.swift
@@ -0,0 +1,11 @@
+import AppleInc
+
+class WinSwift: SwiftLanguage {
+    override func versionNumber() -> Float {
+        return 5.3
+    }
+    
+    override func supportedPlatforms() -> [String] {
+        var supported = super.supportedPlatforms()
+    }
+}
