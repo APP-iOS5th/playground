f691761236d678776fb37500f32ac55935e66d18
Ruel <c0407h@gmail.com>
2024-04-03 16:38:49 +0900

    Protocol Generic

---
+++ TransportMethod.playground/Contents.swift
@@ -0,0 +1,44 @@
+import CoreLocation
+
+protocol TransportMethod {
+    associatedtype CollectionPoint
+    var defaultCollectionPoint: CollectionPoint { get }
+    var averageSpeedInKPH: Double { get }
+}
+
+struct Train: TransportMethod {
+    typealias CollectionPoint = TrainStation
+    
+    var defaultCollectionPoint: TrainStation {
+        return TrainStation.BMS
+    }
+    
+    var averageSpeedInKPH: Double {
+        return 100
+    }
+}
+
+enum TrainStation: String {
+    case BMS = "Bromley South"
+    case VIC = "London Victoria"
+    case RAI = "Rainham (Kent)"
+    case BTN = "Brighton (Ease Sussex)"
+}
+
+protocol TransportLocation {
+    var location: CLLocation { get }
+}
+
+class Journey<TransportType: TransportMethod> {
+    let start: TransportType.CollectionPoint
+    let end: TransportType.CollectionPoint
+    let method: TransportType
+    
+    init(start: TransportType.CollectionPoint, end: TransportType.CollectionPoint, method: TransportType) {
+        self.start = start
+        self.end = end
+        self.method = method
+    }
+    
+    
+}
