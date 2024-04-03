73153097a011ff4a4b4abc3a227b9dff64b07760
Jongchan Yun <jcyun1996@gmail.com>
2024-04-03 15:07:29 +0900

    FatalError

---
+++ MovieReviewClass.playground/Contents.swift
@@ -0,0 +1,50 @@
+
+enum MovieReviewClass {
+    case bad
+    case average
+    case good
+    case brilliant
+}
+
+enum ClassificationState {
+    case initial
+    case classifying
+    case complete
+}
+
+
+class MovieReviewClassifier {
+    var state: ClassificationState = .initial
+    
+    func classify(forStarsOutOf10 stars: Int) -> MovieReviewClass {
+        precondition(state == .initial, "Classifier state must be inital")
+
+        state = .classifying
+        
+        defer {
+            state = .complete
+        }
+        defer {
+            state = .complete
+        }
+
+        if stars > 8 {
+            return .brilliant
+        } else if stars > 6 {
+            return .good
+        } else if stars > 3 {
+            return .average
+        } else if stars > 0 {
+            return .bad
+        } else {
+            fatalError("Start rating must be between 1 and 10")
+        }
+
+    }
+}
+
+let classifier = MovieReviewClassifier()
+
+let review1 = classifier.classify(forStarsOutOf10: -1)
+print(review1)
+print(classifier.state)
