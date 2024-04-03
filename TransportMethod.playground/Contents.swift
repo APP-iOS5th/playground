d4a9a57cf8cd5c89c8d881a916896e457951a46a
JongWon Jeong <jjwon214@gmail.com>
2024-04-03 17:25:09 +0900

    extension with protocol generic

--- TransportMethod.playground/Contents.swift
+++ TransportMethod.playground/Contents.swift
@@ -1,15 +1,14 @@
 import CoreLocation
 
-//MARK: - protocol with generic
-
-protocol TransportMethod {
-    associatedtype CollectionPoint
-    var defaultCollectionPoint: CollectionPoint { get }
-    var averageSpeedInKPH: Double { get }
-}
-
+//MARK: - ptotocol with generic
 protocol TransportLocation {
     var location: CLLocation { get }
+}
+
+protocol TransportMethod {
+    associatedtype CollectionPoint: TransportLocation
+    var defaultCollectionPoint: CollectionPoint { get }
+    var averageSpeedInKPH: Double { get }
 }
 
 struct Train: TransportMethod {
@@ -33,17 +32,17 @@ enum TrainStation: String, TransportLocation {
     var location: CLLocation {
         switch self {
         case .BMS:
-            return CLLocation(latitude: 51.4000504, longitude: 0.0174237)
-            
+            return CLLocation(latitude: 51.4000504,
+                              longitude: 0.0174237)
         case .VIC:
-            return CLLocation(latitude: 51.4952103, longitude: 0.0174237)
-
+            return CLLocation(latitude: 51.4952103,
+                              longitude: -0.1438979)
         case .RAI:
-            return CLLocation(latitude: 51.3663, longitude: 0.0174237)
-
+            return CLLocation(latitude: 51.3663,
+                              longitude: 0.61137)
         case .BTN:
-            return CLLocation(latitude: 51.892, longitude: 0.0174237)
-
+            return CLLocation(latitude: 50.829,
+                              longitude: -0.14125)
         }
     }
 }
@@ -52,14 +51,54 @@ class Journey<TransportType: TransportMethod> {
     let start: TransportType.CollectionPoint
     let end: TransportType.CollectionPoint
     let method: TransportType
-    
+    var distanceInKMs: Double
+    var durationInHours: Double
+
     init(start: TransportType.CollectionPoint,
          end: TransportType.CollectionPoint,
          method: TransportType) {
         self.start = start
         self.end = end
         self.method = method
+        distanceInKMs = end.location.distance(from: start.location) / 1000
+        durationInHours = distanceInKMs / method.averageSpeedInKPH
     }
-    
 }
 
+extension CLLocation: TransportLocation {
+    var location: CLLocation {
+        return self
+    }
+}
+
+enum Road: TransportMethod {
+    typealias CollectionPoint = CLLocation
+
+    case car
+    case motobike
+    case van
+    case hgv
+
+    var defaultCollectionPoint: CLLocation {
+        return CLLocation(latitude: 51.1, longitude: 0.1)
+    }
+
+    var averageSpeedInKPH: Double {
+        switch self {
+        case .car: return 60
+        case .motobike: return 70
+        case .van: return 55
+        case .hgv: return 50
+        }
+    }
+}
+
+let start = CLLocation(latitude: 51.3994669, longitude: 0.0116888)
+let end = CLLocation(latitude: 51.2968654, longitude: 0.5053609)
+let roadJourney = Journey(start: start, end: end, method: Road.car)
+
+let distanceByRoad = roadJourney.distanceInKMs
+let durationByRoad = roadJourney.durationInHours
+
+print("Journey distance: \(distanceByRoad) km")
+print("Journey duration: \(durationByRoad) hours")
