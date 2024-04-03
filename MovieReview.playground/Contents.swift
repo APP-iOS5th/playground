2861d69aa19bc0f3e9d4fb190106b6ea5847a506
Jinx <jinxwift@gmail.com>
2024-04-03 14:43:58 +0900

    defer 끝내기

--- MovieReview.playground/Contents.swift
+++ MovieReview.playground/Contents.swift
@@ -6,10 +6,23 @@ enum MovieReviewClass {
     case brilliant
 }
 
+enum ClassificationState {
+    case initial
+    case classifying
+    case complete
+}
 
 class MovieReviewClassifier {
+    var state: ClassificationState = .initial
     
     func classify(forStarsOutOf10 stars: Int) -> MovieReviewClass {
+        
+        state = .classifying
+        
+        defer {
+            state = .complete
+        }
+        
         if stars > 8 {
             return .brilliant
         } else if stars > 6 {
@@ -21,3 +34,9 @@ class MovieReviewClassifier {
         }
     }
 }
+
+let classifier = MovieReviewClassifier()
+
+let review1 = classifier.classify(forStarsOutOf10: 9)
+print(review1)
+print(classifier.state)
