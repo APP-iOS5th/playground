ce2aa90d8838412d7f3ff6684b9a93ff2452962b
ksiomng0929 <nadana0929@gmail.com>
2024-04-03 16:08:09 +0900

    jeneric with protocal

--- Generic.playground/Contents.swift
+++ Generic.playground/Contents.swift
@@ -1,66 +1,82 @@
-//제내릭 나중에 들어올 타입 -> 대명살 ㅗ대충 통일시켜놓고..나중에 정의해주는건가?
+////제내릭 나중에 들어올 타입 -> 대명살 ㅗ대충 통일시켜놓고..나중에 정의해주는건가?
+//
+//class RecentList<ListiTemType> {
+//    var slot1: ListiTemType?
+//    var slot2: ListiTemType?
+//    var slot3: ListiTemType?
+//    var slot4: ListiTemType?
+//    var slot5: ListiTemType?
+//    
+//    func add(recent: ListiTemType) {
+//        slot5 = slot4
+//        slot4 = slot3
+//        slot3 = slot2
+//        slot2 = slot1
+//        slot1 = recent
+//    }
+//    
+//    func getAll() -> [ListiTemType] {
+//        var recent = [ListiTemType]()
+//        if let slot1 = slot1 {
+//            recent.append(slot1)
+//        }
+//        if let slot2 = slot2 {
+//            recent.append(slot2)
+//        }
+//        if let slot3 = slot3 {
+//            recent.append(slot3)
+//        }
+//        if let slot4 = slot4 {
+//            recent.append(slot4)
+//        }
+//        if let slot5 = slot5 {
+//            recent.append(slot5)
+//        }
+//        return recent
+//    }
+//}
+//
+//let recentlyCopiedList = RecentList<String>()
+//recentlyCopiedList.add(recent: "First")
+//recentlyCopiedList.add(recent: "Next")
+//recentlyCopiedList.add(recent: "Last")
+//var recentlyCopied = recentlyCopiedList.getAll()
+//print(recentlyCopied)
+//
+//class Person {
+//    let name: String
+//    init(name: String) {
+//        self.name = name
+//    }
+//}
+//
+//let rod = Person(name: "Rod")
+//let jane = Person(name: "Jane")
+//let freddy = Person(name: "Freddy")
+//
+//let lastCalledList = RecentList<Person>()
+//
+//lastCalledList.add(recent: freddy)
+//lastCalledList.add(recent: jane)
+//lastCalledList.add(recent: rod)
+//
+//let lastCalled = lastCalledList.getAll()
+//for person in lastCalled {
+//    print(person.name)
+//}
 
-class RecentList<ListiTemType> {
-    var slot1: ListiTemType?
-    var slot2: ListiTemType?
-    var slot3: ListiTemType?
-    var slot4: ListiTemType?
-    var slot5: ListiTemType?
-    
-    func add(recent: ListiTemType) {
-        slot5 = slot4
-        slot4 = slot3
-        slot3 = slot2
-        slot2 = slot1
-        slot1 = recent
+func makeDuplicates<ItemType, KeyType: Hashable>(of item: ItemType, withKeys keys: Set<KeyType>) -> [KeyType: ItemType] {
+    var duplicates = [KeyType: ItemType]()
+    for key in keys {
+        duplicates[key] = item
     }
-    
-    func getAll() -> [ListiTemType] {
-        var recent = [ListiTemType]()
-        if let slot1 = slot1 {
-            recent.append(slot1)
-        }
-        if let slot2 = slot2 {
-            recent.append(slot2)
-        }
-        if let slot3 = slot3 {
-            recent.append(slot3)
-        }
-        if let slot4 = slot4 {
-            recent.append(slot4)
-        }
-        if let slot5 = slot5 {
-            recent.append(slot5)
-        }
-        return recent
-    }
+    return duplicates
 }
 
-let recentlyCopiedList = RecentList<String>()
-recentlyCopiedList.add(recent: "First")
-recentlyCopiedList.add(recent: "Next")
-recentlyCopiedList.add(recent: "Last")
-var recentlyCopied = recentlyCopiedList.getAll()
-print(recentlyCopied)
-
-class Person {
-    let name: String
-    init(name: String) {
-        self.name = name
-    }
-}
-
-let rod = Person(name: "Rod")
-let jane = Person(name: "Jane")
-let freddy = Person(name: "Freddy")
-
-let lastCalledList = RecentList<Person>()
-
-lastCalledList.add(recent: freddy)
-lastCalledList.add(recent: jane)
-lastCalledList.add(recent: rod)
-
-let lastCalled = lastCalledList.getAll()
-for person in lastCalled {
-    print(person.name)
-}
+let awards: Set<String> = ["Best Director",
+                           "Best Picture",
+                           "Best Original Screenplay",
+                           "Best International Feature"]
+let oscars2020 = makeDuplicates(of: "Parasite", withKeys: awards)
+print(oscars2020["Best Picture"] ?? "")
+print(oscars2020["Best International Feature"] ?? "")
