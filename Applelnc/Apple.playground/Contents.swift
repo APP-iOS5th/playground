fc3c42c44345fe4cee683c6619d39d4fb8323493
wotjs020708 <wotjs020708@gmail.com>
2024-04-03 09:08:50 +0900

    -

--- Applelnc/Apple.playground/Contents.swift
+++ Applelnc/Apple.playground/Contents.swift
@@ -1,3 +1,34 @@
-import UIKit
 
-var greeting = "Hello, playground"
+public class Person {
+    public let name: String
+    
+    public init(name: String) {
+        self.name = name
+    }
+    
+}
+
+public class Apple {
+    public private(set) var ceo: Person
+    private var employee = [Person]()
+    public let store = AppleStore()
+    private let secretDepartment = SecretProductDepartment()
+    
+    public init() {
+        ceo = Person(name: "Tim Cook")
+        employee.append(ceo)
+    }
+    
+    public func newEmployee(person: Person) {
+        employee.append(person)
+    }
+    
+    func weeklyProductMeeting() {
+        var superSecretProduct = secretDepartment.nextProduct(codeWord: "Not sure!")
+        
+        superSecretProduct = secretDepartment.nextProduct(codeWord: "Titen")
+        
+        print(superSecretProduct as Any)
+    }
+    
+}
---
+++ SwiftLanguage.playground/Contents.swift
@@ -0,0 +1,9 @@
+open class SwiftLanguage {
+    open func versionNumber() -> Float {
+        return 5.0
+    }
+    open func supportedPlatforms() -> [String] {
+        return ["iOS", "macOS", "tvOS", "watchOS", "Linux"]
+    }
+}
+
