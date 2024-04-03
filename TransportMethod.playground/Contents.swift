808041877efcb26d3078326cb7222d03c64d8a6c
Ruel <c0407h@gmail.com>
2024-04-03 17:22:32 +0900

    extension with protocol generic

--- TransportMethod.playground/Contents.swift
+++ TransportMethod.playground/Contents.swift
@@ -46,12 +46,56 @@ class Journey<TransportType: TransportMethod> {
     let start: TransportType.CollectionPoint
     let end: TransportType.CollectionPoint
     let method: TransportType
+    let distanceInKMs: Double
+    let durationInHours: Double
     
     init(start: TransportType.CollectionPoint, end: TransportType.CollectionPoint, method: TransportType) {
         self.start = start
         self.end = end
         self.method = method
+        distanceInKMs = end.location.distance(from: start.location) / 1000
+        durationInHours = distanceInKMs / method.averageSpeedInKPH
     }
     
     
 }
+
+extension CLLocation: TransportLocation {
+    var location: CLLocation {
+        return self
+    }
+}
+
+enum Road: TransportMethod {
+    typealias CollectionPoint = CLLocation
+    
+    case car
+    case motobike
+    case van
+    case hgv
+    
+    var defaultCollectionPoint: CLLocation {
+        return CLLocation(latitude: 51.1, longitude: 0.1)
+    }
+    
+    var averageSpeedInKPH: Double {
+        switch self {
+        case .car: return 60
+        case .motobike: return 70
+        case .van: return 55
+        case .hgv: return 50
+        }
+    }
+}
+
+
+let start = CLLocation(latitude: 51.3994669, longitude: 0.0116888)
+let end = CLLocation(latitude: 51.2968654, longitude: 0.5053609)
+
+let roadJourney = Journey(start: start, end: end, method: Road.car)
+
+let distanceByRoad = roadJourney.distanceInKMs
+let distanceByInHours = roadJourney.durationInHours
+
+print("Journey distance: \(distanceByRoad) km")
+print("Journey duration: \(distanceByInHours) hours")
