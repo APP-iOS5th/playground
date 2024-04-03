fcec218d23d04f54da1396d50afab79e28ea32da
Jihye Park <deviloper.park@gmail.com>
2024-04-03 11:36:27 +0900

    코드 축약

--- WhileLoop.playground/Contents.swift
+++ WhileLoop.playground/Contents.swift
@@ -5,20 +5,24 @@ enum CoinFlip: Int {
     case tails
     
     static func flipCoin() -> CoinFlip {
-        return CoinFlip(rawValue: Int(arc4random_uniform(2)))! // 랜덤값 2개
+        return CoinFlip(rawValue: Int(arc4random_uniform(2)))! // 0 or 1을 주는 함수
     }
 }
 
 func howManyHeadsInARow() -> Int {
     var numberOfHeadsInRow = 0
-    var currentCoinFlip = CoinFlip.flipCoin()
-    
-    while currentCoinFlip == .heads {
+//    var currentCoinFlip = CoinFlip.flipCoin()
+//    
+//    while currentCoinFlip == .heads {
+//        numberOfHeadsInRow = numberOfHeadsInRow + 1
+//        currentCoinFlip = CoinFlip.flipCoin()
+//
+//    }
+    while CoinFlip.flipCoin() == .heads {
         numberOfHeadsInRow = numberOfHeadsInRow + 1
-        currentCoinFlip = CoinFlip.flipCoin()
-        print(currentCoinFlip)
+//        currentCoinFlip = CoinFlip.flipCoin()
+
     }
-    print(currentCoinFlip)
     return numberOfHeadsInRow
 }
 
