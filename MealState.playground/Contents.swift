571033af5a18f9e20773c2c9a6511083bdba1002
jmbae <jmbae@codegrove.co.kr>
2024-04-03 13:11:03 +0900

    throw with function call

--- MealState.playground/Contents.swift
+++ MealState.playground/Contents.swift
@@ -27,16 +27,37 @@ class Meal {
             throw MealError.canOnlyMoveToAppropriateSate
         }
     }
+    
+    func buyIngredients() throws {
+        try change(to: .buyIngregients)
+    }
+    
+    func prepareIngredients() throws {
+        try change(to: .prepareIngredients)
+    }
+     
+    func cook() throws {
+        try change(to: .cook)
+    }
+     
+    func plateUp() throws {
+        try change(to: .plateUp)
+    }
+     
+    func serve() throws {
+        try change(to: .serve)
+    }
+
 }
 
 let dinner = Meal()
 
 do {
-    try dinner.change(to: .buyIngregients)
-    try dinner.change(to: .prepareIngredients)
-    try dinner.change(to: .cook)
-    try dinner.change(to: .plateUp)
-    try dinner.change(to: .serve)
+    try dinner.buyIngredients()
+    try dinner.prepareIngredients()
+    try dinner.cook()
+    try dinner.plateUp()
+    try dinner.serve()
     print("Dinner is served!")
 } catch let error {
     print(error)
