97fbf7e86f989366f5a761c6d31453c9d6437349
Jihye Park <deviloper.park@gmail.com>
2024-04-03 15:05:01 +0900

    Precondition, FatalError

--- MovieReview.playground/Contents.swift
+++ MovieReview.playground/Contents.swift
@@ -17,20 +17,26 @@ class MovieReviewClassifier {
     
     func classify(forStarsOutOf10 stars: Int) -> MovieReviewClass {
         
+        // 함수 실행 전제조건
+        precondition(state == .initial, "Classifier state must be inital")
+
         state = .classifying
         
         // 함수가 언제 끝나는지 모르므로 defer를 통해 나중에 실행
         defer {
             state = .complete
         }
+        
         if stars > 8 {
             return .brilliant
         } else if stars > 6 {
             return .good
         } else if stars > 3 {
             return .average
-        } else {
+        } else if stars > 0{
             return .bad
+        } else {
+            fatalError("Start rating must be between 1 and 10")
         }
     }
 }
