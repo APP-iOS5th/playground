3bcf94ab6083b5f8cd2163b822b2ec1b3732232c
Ruel <c0407h@gmail.com>
2024-04-03 11:13:51 +0900

    for loop

--- Loop.playground/Contents.swift
+++ Loop.playground/Contents.swift
@@ -1,3 +1,33 @@
-import UIKit
 
-var greeting = "Hello, playground"
+
+let theBeatles = ["John", "Paul", "George", "Ringo"]
+
+for musician in theBeatles {
+    print(musician)
+}
+
+for value in 1...12 {
+    print("5 X \(value) = \(5 * value)")
+}
+
+
+for value in 1..<12 {
+    print("5 X \(value) = \(5 * value)")
+}
+
+
+for value in 1... {
+    if value > 12 {
+        break
+    }
+    print("5 X \(value) = \(5 * value)")
+}
+
+let beatlesByInstrument = ["rhythm guitar": "John",
+                            "bass guitar": "Paul",
+                            "lead guitar": "George",
+                            "drums": "Ringo"]
+
+for (key, value) in beatlesByInstrument {
+    print("\(value) plays \(key)")
+}
