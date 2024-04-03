ee5fcb8f405c14b099cfc62e2479767772495369
JongWon Jeong <jjwon214@gmail.com>
2024-04-03 14:45:15 +0900

    defer

---
+++ MovieReview.playground/Contents.swift
@@ -0,0 +1,44 @@
+import UIKit
+
+enum MovieReviewClass {
+    case bad
+    case average
+    case good
+    case brilliant
+}
+
+enum ClassificationState {
+    case initial
+    case classifying
+    case complete
+}
+
+class MovieReviewClassifier {
+    
+    var state: ClassificationState = .initial
+    
+    func classfy(forStarsOutOf10 stars: Int) -> MovieReviewClass {
+        
+        state = .classifying
+        
+        defer {
+            state = .complete
+        }
+        if stars > 8 {
+            return .brilliant
+        } else if stars > 6 {
+            return .good
+        } else if stars > 3 {
+            return . average
+        } else {
+            return .bad
+        }
+    }
+    
+}
+
+let classifier = MovieReviewClassifier()
+
+let review1 = classifier.classfy(forStarsOutOf10: 9)
+print(review1)
+print(classifier.state)
