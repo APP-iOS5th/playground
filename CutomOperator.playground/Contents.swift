d4331b8d1f27682c03d0682766dd4dcb95066e7f
SeongKookKIM <ksk8646@naver.com>
2024-04-04 11:16:39 +0900

    General Operator

--- CutomOperator.playground/Contents.swift
+++ CutomOperator.playground/Contents.swift
@@ -1,27 +1,22 @@
 
 infix operator >>>
 
-func >>> (lhs: String, rhs:String) -> String {
+
+
+func >>> <Element> (lhs:Element, rhs: Array<Element>) -> Array<Element> {
     var combined = rhs
     combined.append(lhs)
     return combined
 }
 
-func >>> (lhs:String, rhs: [String]) -> [String] {
-    var combined = rhs
-    combined.append(lhs)
-    return combined
-}
-
-func >>> (lhs:[String], rhs: [String]) -> [String] {
+func >>> <Element> (lhs: Array<Element>, rhs: Array<Element>) -> Array<Element> {
     var combined = rhs
     // 두개 다 배열일경우 contentsOf 배열을 풀어서 넣어주는 역할
     combined.append(contentsOf: lhs)
     return combined
 }
 
-let appendedString = "Two" >>> "One"
-print(appendedString)
+
 
 let appendedString2 = "Two" >>> ["One"]
 print(appendedString2)
