a532dd9a9661a89197ac196b5e4f323083e8d459
Jihye Park <deviloper.park@gmail.com>
2024-04-04 15:29:54 +0900

    JSON

---
+++ JSONPlayground.playground/Contents.swift
@@ -0,0 +1,51 @@
+import Foundation
+import PlaygroundSupport
+
+PlaygroundPage.current.needsIndefiniteExecution = true
+
+let config = URLSessionConfiguration.default
+let session = URLSession(configuration: config)
+
+
+func fetchRepo(forUsername username:String) {
+    let urlString = "https://api.github.com/users/\(username)/repos"
+    let url = URL(string: urlString)!
+    var request = URLRequest(url: url)
+    
+    // header에 적용
+    request.setValue("application/vnd.github.v3+json", forHTTPHeaderField: "Accept")
+    
+    let task = session.dataTask(with: request) { (data, response, error) in
+        
+        defer {
+            PlaygroundPage.current.finishExecution()
+        }
+        
+        guard let jsonData = data else {
+            print(error ?? "Network Error")
+            return
+        }
+        
+        do {
+            // 가져온 데이터를 json 형태로
+            let deserialized = try JSONSerialization.jsonObject(with: jsonData, options: [])
+            print(deserialized)
+            
+            // as?(다운캐스팅): deserialized 타입을 [[String: Any]](딕셔너리 배열의 배열)로 다운캐스팅
+            guard let repos = deserialized as? [[String: Any]] else { // 실행 구문이기 때문에 generic이 아닌 any 사용
+                print("Unexpected Response")
+                return
+            }
+            print(repos)
+            
+        } catch {
+            print(error)
+            
+        }
+    }
+    
+    task.resume()
+}
+
+//fetchRepo(forUsername: "jihyeep")
+fetchRepo(forUsername: "APP-iOS5th")
