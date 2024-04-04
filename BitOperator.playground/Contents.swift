2c76eee7e9b6db5d9441adf72c6dccb60271b931
jmbae <jmbae@codegrove.co.kr>
2024-04-04 10:09:53 +0900

    Shift Operation

--- BitOperator.playground/Contents.swift
+++ BitOperator.playground/Contents.swift
@@ -8,18 +8,23 @@ let six: Int   = 0b110
 let seven: Int = 0b111
 
 
-let phone: Int        = 0b0000001
-let tablet: Int       = 0b0000010
-let watch: Int        = 0b0000100
-let laptop: Int       = 0b0001000
-let desktop: Int      = 0b0010000
-let tv: Int           = 0b0100000
-let brainImplant: Int = 0b1000000
+let phone: Int        = 1 << 0
+let tablet: Int       = 1 << 1
+let watch: Int        = 1 << 2
+let laptop: Int       = 1 << 3
+let desktop: Int      = 1 << 4
+let tv: Int           = 1 << 5
+let brainImplant: Int = 1 << 6
 
 print(String(five, radix: 2))
 print(String(phone + tablet + watch, radix: 2))
 print(String(desktop, radix: 2))
 print(desktop)
+
+let testBit = 0b101010
+print(String(testBit, radix: 2))
+let resultBit = testBit << 2
+print(String(resultBit, radix: 2))
 
 let supportedDevices = phone + tablet + tv
 
