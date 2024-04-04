12e251e86476382e679adc328c5654f5262fd8d0
wotjs020708 <wotjs020708@gmail.com>
2024-04-04 13:41:09 +0900

    Foundation

--- NastedType.playground/Contents.swift
+++ NastedType.playground/Contents.swift
@@ -1,3 +1,25 @@
-import UIKit
 
-var greeting = "Hello, playground"
+class Device {
+    
+    enum Category {
+        case watch
+        case phone
+        case tablet
+    }
+    
+    enum Orientation {
+        case portrait
+        case portraitUpsideDown
+        case landscapeLeft
+        case landscapeRight
+    }
+    
+    let category: Category
+    var currentOrientation: Orientation = .portrait
+    
+    init(category: Category) {
+        self.category = category
+    }
+}
+
+Device(category: Device.Category.phone)
