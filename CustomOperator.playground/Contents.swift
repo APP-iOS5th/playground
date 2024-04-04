6026c6f9c04388efb12cb86bce4b28ad62f7a135
Cha, Jiyong <wldyd2113@gmail.com>
2024-04-04 11:16:18 +0900

    Generic Operator

--- CustomOperator.playground/Contents.swift
+++ CustomOperator.playground/Contents.swift
@@ -1,26 +1,17 @@
 
 infix operator >>> //>>>연산자를 새로 추가
 
-func >>> (lhs: String, rhs: String) -> String {
+func >>> <Element>(lhs: Element, rhs: Array<Element>) -> Array<Element> {
     var combined = rhs
     combined.append(lhs)
     return combined
 }
 
-func >>>(lhs: String, rhs: [String]) -> [String] {
-    var combined = rhs
-    combined.append(lhs)
-    return combined
-}
-
-func >>>(lhs: [String], rhs: [String]) -> [String] {
+func >>> <Element>(lhs: Array<Element>, rhs: Array<Element>) -> Array<Element> {
     var combined = rhs
     combined.append(contentsOf: lhs) //배열인건 풀어서 추가해주는 append
     return combined
 }
-
-let appendString = "Two" >>> "One"
-print(appendString)
 
 let appendString2 = "Two" >>> ["One"]
 print(appendString2)
