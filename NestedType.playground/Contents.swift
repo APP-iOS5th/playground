6f0e3c1182f302c9c21e2e1950a7d8ec173baa4c
kyhlsd <kmyghn@gmail.com>
2024-04-04 13:03:31 +0900

    NestedType

---
+++ NestedType.playground/Contents.swift
@@ -0,0 +1,24 @@
+
+class Device {
+    enum Category {
+        case watch
+        case phone
+        case tablet
+    }
+    
+    enum Orientation {
+        case portrait
+        case portraitUpsideDown
+        case landscapeLeft
+        case landspaceRight
+    }
+    
+    let category: Category
+    var currentOrientation: Orientation = .portait
+    
+    init(category: Category){
+        self.category = category
+    }
+}
+
+Device(category: Device.Category.phone)
