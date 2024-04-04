5e0032e48175146c3b334490fdbf1bcc11487951
iyungui <yungui632@gmail.com>
2024-04-04 09:38:48 +0900

    Bit Operation

---
+++ BitOperator.playground/Contents.swift
@@ -0,0 +1,37 @@
+
+let zero: Int  = 0b000
+let one: Int   = 0b001
+let two: Int   = 0b010
+let three: Int = 0b011
+let four: Int  = 0b100
+let five: Int  = 0b101
+let six: Int   = 0b110
+let seven: Int = 0b111
+
+
+let phone: Int        = 0b0000001
+let tablet: Int       = 0b0000010
+let watch: Int        = 0b0000100
+let laptop: Int       = 0b0001000
+let desktop: Int      = 0b0010000
+let tv: Int           = 0b0100000
+let brainImplant: Int = 0b1000000
+
+print(String(phone + tablet + watch, radix: 2))
+print(String(phone, radix: 2))
+
+let supportedDevices = phone + tablet + tv
+
+func isSupported(device: Int) -> Bool {
+    let bitWiseANDResult = supportedDevices & device
+    let containsDevice = bitWiseANDResult == device
+    return containsDevice
+}
+
+let phoneSupported = isSupported(device: phone)
+print(phoneSupported)
+
+let brainImplantSupported = isSupported(device: brainImplant)
+print(brainImplantSupported)
+
+
