adcc3d64d17a7a48a4244b0c738fdd44f2fe0ff3
Jin Park <jinpark1504@gmail.com>
2024-04-04 11:19:17 +0900

    custom operator with generic

--- CustomOperator.playground/Contents.swift
+++ CustomOperator.playground/Contents.swift
@@ -1,29 +1,37 @@
 
 infix operator >>>
 
-func >>> (lhs: String, rhs: String) -> String {
+//func >>> (lhs: String, rhs: String) -> String {
+//    var combined = rhs
+//    combined.append(lhs)
+//    return combined
+//}
+
+func >>> <Element>(lhs: Element, rhs: [Element]) -> [Element] {
     var combined = rhs
     combined.append(lhs)
     return combined
 }
 
-func >>> (lhs: String, rhs: [String]) -> [String] {
-    var combined = rhs
-    combined.append(lhs)
-    return combined
-}
-
-func >>> (lhs: [String], rhs: [String]) -> [String] {
+//func >>> (lhs: [String], rhs: [String]) -> [String] {
+//    var combined = rhs
+//    combined.append(contentsOf: lhs)
+//    return combined
+//}
+func >>> <Element>(lhs: Array<Element>, rhs: Array<Element>) -> Array<Element> {
     var combined = rhs
     combined.append(contentsOf: lhs)
     return combined
 }
 
-let appendedString = "Two" >>> "One"
-print(appendedString)
+//let appendedString = "Two" >>> "One"
+//print(appendedString)
 
 let appendedString2 = "Two" >>> ["One"]
 print(appendedString2)
 
 let appendedString3 = ["Two", "Three"] >>> ["One", "Zero"]
 print(appendedString3)
+
+let appendedString4 = [2, "Three"] >>> ["One", 0]
+print(appendedString4)
