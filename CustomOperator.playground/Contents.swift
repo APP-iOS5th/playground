b4109d557c9d68b6dc3f083343e510bf9d81f838
jmbae <jmbae@codegrove.co.kr>
2024-04-04 11:16:10 +0900

    Generic Operator

--- CustomOperator.playground/Contents.swift
+++ CustomOperator.playground/Contents.swift
@@ -1,3 +1,21 @@
-import Cocoa
 
-var greeting = "Hello, playground"
+infix operator >>>
+
+func >>> <Element>(lhs: Element, rhs: Array<Element>) -> Array<Element> {
+    var combined = rhs
+    combined.append(lhs)
+    return combined
+}
+
+func >>> <Element>(lhs: Array<Element>, rhs: Array<Element>) -> Array<Element> {
+    var combined = rhs
+    combined.append(contentsOf: lhs)
+    return combined
+}
+
+
+let appendedString2 = "Two" >>> ["One"]
+print(appendedString2)
+
+let appendedString3 = ["Three", "Four"] >>> ["One", "Two"]
+print(appendedString3)
