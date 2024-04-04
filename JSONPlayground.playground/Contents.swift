b1a2efb581221bf972b3f6e5b0de578fbbbac8e4
jeongwookim <112370814+JeongWo@users.noreply.github.com>
2024-04-04 16:44:22 +0900

    XMLParser

--- JSONPlayground.playground/Contents.swift
+++ JSONPlayground.playground/Contents.swift
@@ -4,10 +4,15 @@ import PlaygroundSupport
 
 PlaygroundPage.current.needsIndefiniteExecution = true
 
+enum ResponseError: Error {
+    case requestUnSuccessful
+    case unexpectedResponseStructure
+}
+
 let config = URLSessionConfiguration.default
 let session = URLSession(configuration: config)
 
-func fetchRepo(forUsername username: String) {
+func fetchRepo(forUsername username: String, completionHandler: @escaping ([[String: Any]]?, Error?) -> Void) {
     let urlString = "https://api.github.com/users/\(username)/repos"
     let url = URL(string: urlString)!
     var request = URLRequest(url: url)
@@ -21,7 +26,7 @@ func fetchRepo(forUsername username: String) {
         }
         
         guard let jsonData = data else {
-            print(error ?? "Network Error")
+            completionHandler(nil, ResponseError.requestUnSuccessful)
             return
         }
         
@@ -30,16 +35,23 @@ func fetchRepo(forUsername username: String) {
             print(deserialized)
             
             guard let repos = deserialized as? [[String: Any]] else {
-                print("Unexpected Response")
+                completionHandler(nil, ResponseError.unexpectedResponseStructure)
                 return
             }
-            print(repos)
+            completionHandler(repos, nil)
         } catch {
-            print(error)
+            completionHandler(nil, error)
         }
         
     }
     task.resume()
     
 }
-fetchRepo(forUsername: "JeongWo")
+
+fetchRepo(forUsername: "JeongWo") { (repos, error) in
+    if let repos = repos {
+        print(repos)
+    } else {
+        print(error as Any)
+    }
+}
