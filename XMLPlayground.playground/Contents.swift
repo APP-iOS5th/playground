9ff7517a116b65be84e7a086353fd89c5c5b20a1
uunwon <chea2089@naver.com>
2024-04-04 17:03:34 +0900

    XML parser

---
+++ XMLPlayground.playground/Contents.swift
@@ -0,0 +1,23 @@
+import Foundation
+import PlaygroundSupport
+
+PlaygroundPage.current.needsIndefiniteExecution = true
+
+func fetchBBCNewsRSSFeed() {
+    
+    let session = URLSession.shared
+    let url = URL(string: "http://feeds.bbci.co.uk/news/rss.xml")!
+    let dataTask = session.dataTask(with: url) { (data, response, error) in
+        
+        guard let data = data, error == nil else {
+            print(error ?? "Unexpected response")
+            return
+        }
+        
+        let dataAsString = String(data: data, encoding: .utf8)!
+        print(dataAsString)
+    }
+    dataTask.resume()
+}
+
+fetchBBCNewsRSSFeed()
