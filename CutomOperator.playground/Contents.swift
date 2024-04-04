2e8c913e9f7e4f0650ba6b0d5119f2e27c249883
SeongKookKIM <ksk8646@naver.com>
2024-04-04 11:35:08 +0900

    custom operater overloading

--- CutomOperator.playground/Contents.swift
+++ CutomOperator.playground/Contents.swift
@@ -24,3 +24,29 @@ print(appendedString2)
 let appendedString3 = ["Three"] >>> ["Four"]
 print(appendedString3)
 
+struct Task {
+    let name: String
+}
+
+// CustomStringConvertible 문자열 타입을 이쁘게 알려주는 프로토콜 -> description 필수값으로 들어가야함
+class TaskList: CustomStringConvertible {
+    private var tasks: [Task] = []
+    func append(task: Task) {
+        tasks.append(task)
+    }
+    var description: String {
+        return tasks.map { $0.name }.joined(separator: "\n")
+    }
+}
+
+extension TaskList {
+    static func >>> (lhs: Task, rhs: TaskList) {
+        rhs.append(task: lhs)
+    }
+}
+
+let shoppingList = TaskList()
+let task1 = Task(name: "get milk")
+task1 >>> shoppingList
+let task2 = Task(name: "get teabags")
+shoppingList.append(task: task2)
