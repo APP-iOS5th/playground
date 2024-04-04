85acfb2e71c560eae069689ea716e3f4c17770a8
MadCow <lobasketve@naver.com>
2024-04-04 15:26:31 +0900

    JSON

---
+++ JSONPlayground.playground/Contents.swift
@@ -0,0 +1,45 @@
+import Foundation
+import PlaygroundSupport
+
+PlaygroundPage.current.needsIndefiniteExecution = true
+
+let config = URLSessionConfiguration.default
+let session = URLSession(configuration: config)
+
+func fetchRepo(forUsername username: String) {
+    let urlString = "https://api.github.com/users/\(username)/repos"
+    let url = URL(string: urlString)!
+    var request = URLRequest(url: url)
+    request.setValue("application/vnd.github.v3+json", forHTTPHeaderField: "Accept")
+    
+    let task = session.dataTask(with: request) { (data, response, error) in
+        defer {
+            PlaygroundPage.current.finishExecution()
+        }
+        
+        guard let jsonData = data else {
+            print(error ?? "Network Error!")
+            return
+        }
+        
+        do {
+            let deserialized = try JSONSerialization.jsonObject(with: jsonData, options: [])
+//            print(deserialized)
+            
+            guard let repos = deserialized as? [[String: Any]] else {
+                print("ERROR while deserialized >> Unexpected Response!")
+                return
+            }
+            
+            repos.forEach{ print($0) }
+            print(repos.count) // 27
+            
+        } catch {
+            print("ERROR! > \(error)")
+        }
+    }
+    
+    task.resume()
+}
+
+fetchRepo(forUsername: "madcow95")
