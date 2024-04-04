d71908fb3e70de52e14b8fff6b653b9bc88515b7
Ruel <c0407h@gmail.com>
2024-04-04 11:35:01 +0900

    custom operator overloading

--- CustomOperator.playground/Contents.swift
+++ CustomOperator.playground/Contents.swift
@@ -29,3 +29,38 @@ print(appendedString2)
 
 let appendedString3 = ["Three", "Four"] >>> ["One", "Two"]
 print(appendedString3)
+
+
+struct Task{
+    let name : String
+}
+
+class TaskList: CustomStringConvertible {
+    private var tasks: [Task] = []
+    
+    func append(task: Task) {
+        tasks.append(task)
+    }
+    
+    var description: String  {
+        return tasks.map { $0.name }.joined(separator: "\n")
+    }
+}
+
+extension TaskList {
+    static func >>> (lhs: Task, rhs: TaskList) {
+        rhs.append(task: lhs)
+    }
+}
+
+let shoppingList = TaskList()
+let task1 = Task(name: "상추")
+task1 >>> shoppingList
+
+let task2 = Task(name: "삼겹살")
+shoppingList.append(task: task2)
+
+let task3 = Task(name: "쌈장")
+shoppingList.append(task: task3)
+
+print(shoppingList.description)
