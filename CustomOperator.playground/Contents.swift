767088ac5159d45067a7f66904ce921a55d988d2
Jihye Park <deviloper.park@gmail.com>
2024-04-04 11:35:33 +0900

    Custom operator overloading

--- CustomOperator.playground/Contents.swift
+++ CustomOperator.playground/Contents.swift
@@ -1,5 +1,7 @@
 // 사용자 정의 연산자
+//prefix operator >>>
 infix operator >>>
+//postfix operator >>>
 
 func >>> (lhs: String, rhs: String) -> String {
     var combined = rhs
@@ -20,6 +22,20 @@ func >>> (lhs: [String], rhs: [String]) -> [String] {
     return combined
 }
 
+// generic
+func >>> <Element>(lhs: Element, rhs: Array<Element>) -> Array<Element> {
+    var combined = rhs
+    combined.append(lhs)
+    return combined
+}
+
+func >>> <Element>(lhs: Array<Element>, rhs: Array<Element>) -> Array<Element> {
+    var combined = rhs
+    combined.append(contentsOf: lhs) // 배열에서 하나씩 풀어서 붙임
+    return combined
+}
+
+
 let appendedString = "Two" >>> "One" // 좌항을 우항 다음에 붙임
 print(appendedString)
 
@@ -28,3 +44,36 @@ print(appendedString2)
 
 let appendedString3 = ["Three", "Four"] >>> ["One", "Two"] // ["One", "Two", "Three", "Four"]
 print(appendedString3)
+
+
+struct Task {
+    let name: String
+}
+
+class TaskList: CustomStringConvertible {
+    private var tasks: [Task] = []
+    
+    func append(task: Task) {
+        tasks.append(task)
+    }
+    
+    var description: String {
+        return tasks.map { $0.name }.joined(separator: "\n")
+    }
+}
+
+// 연산자 추가 정의(함수 overloading)
+extension TaskList {
+    
+    static func >>> (lhs: Task, rhs: TaskList) {
+        rhs.append(task: lhs)
+    }
+}
+
+let shoppingList = TaskList()
+
+let task1 = Task(name: "get milk")
+task1 >>> shoppingList
+
+let task2 = Task(name: "get teabags")
+shoppingList.append(task: task2)
