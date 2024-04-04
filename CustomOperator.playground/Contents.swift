90ce80703c465fee4495fb3c3c3b102d77e00397
jihye <ganzi410@gmail.com>
2024-04-04 11:35:04 +0900

    custom operator overloading

--- CustomOperator.playground/Contents.swift
+++ CustomOperator.playground/Contents.swift
@@ -1,26 +1,19 @@
 
 infix operator >>>
 
-func >>> (lhs: String, rhs: String) -> String {
+
+func >>> <Element>(lhs: Element, rhs: Array<Element>) -> Array<Element> {
     var combined = rhs
     combined.append(lhs)
     return combined
 }
 
-func >>> (lhs: String, rhs: [String]) -> [String] {
-    var combined = rhs
-    combined.append(lhs)
-    return combined
-}
-
-func >>> (lhs: [String], rhs: [String]) -> [String] {
+func >>> <Element>(lhs: Array<Element>, rhs: Array<Element>) -> Array<Element> {
     var combined = rhs
     combined.append(contentsOf: lhs)
     return combined
 }
 
-let appendString =  "Two" >>> "One"
-print(appendString)
 
 let appendString2 = "Two" >>> ["One"]
 print(appendString2)
@@ -28,3 +21,29 @@ print(appendString2)
 let appendString3 = ["Three", "Four"] >>> ["One", "Two"]
 print(appendString3)
 
+
+struct Task {
+    let name: String
+}
+
+class TaskList: CustomStringConvertible {
+    private var tasks: [Task] = []
+    func append(task: Task) {
+        tasks.append(task)
+    }
+    var description: String {
+        return tasks.map { $0.name }.joined(separator: "\n")
+    }
+}
+
+extension TaskList {
+    static func >>> (lhs: Task, rhs: TaskList) {
+        rhs.append(task: lhs)
+    }
+}
+
+let shoppingList = TaskList()
+let task1 = Task(name: "get milk")
+task1 >>> shoppingList
+let task2 = Task(name: "get teabags")
+shoppingList.append(task: task2)
