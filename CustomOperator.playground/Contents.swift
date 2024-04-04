1ac1c17139f2ec84f64b4db89df01389c2a3df4f
Cha, Jiyong <wldyd2113@gmail.com>
2024-04-04 11:35:08 +0900

    custom operater overloading

--- CustomOperator.playground/Contents.swift
+++ CustomOperator.playground/Contents.swift
@@ -18,3 +18,33 @@ print(appendString2)
 
 let appendString3 = ["Three","Four"] >>> ["One", "Two"]
 print(appendString3)
+
+struct Task {
+    let name: String
+}
+
+class TaskList: CustomStringConvertible {
+    private var tasks: [Task] = []
+    func append(task: Task){
+        tasks.append(task)
+    }
+    var description: String {
+        return tasks.map { $0.name }.joined(separator: "\n") //배열을 맵핑
+    }
+}
+
+extension TaskList {
+    static func >>> (lhs: Task, rhs: TaskList){
+        rhs.append(task: lhs)
+    }
+}
+
+let shoppingList = TaskList()
+
+let task1 = Task(name: "get milk")
+task1 >>> shoppingList
+
+let task2 = Task(name: "get teabags")
+shoppingList.append(task: task2)
+
+print(shoppingList)
