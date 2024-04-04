346f9663af28ef48bf70dd7614473a56f0316966
jihye <ganzi410@gmail.com>
2024-04-04 14:27:57 +0900

    URLSession

---
+++ URLSession.playground/Contents.swift
@@ -0,0 +1,22 @@
+import PlaygroundSupport
+import Foundation
+import UIKit
+
+PlaygroundPage.current.needsIndefiniteExecution = true
+
+let config = URLSessionConfiguration.default
+let session = URLSession(configuration: config)
+
+let urlString = "https://imgs.xkcd.com/comics/api.png"
+let url = URL(string: urlString)!
+let request = URLRequest(url: url)
+
+let task = session.dataTask(with: request, completionHandler: { (data, response, error ) in
+    guard let imageData = data else {
+        return
+    }
+    
+    _ = UIImage(data: imageData)
+})
+
+task.resume()
