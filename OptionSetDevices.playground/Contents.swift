89a3265a541c192445c0f2e7b0bafad791a642a7
Cha, Jiyong <wldyd2113@gmail.com>
2024-04-04 10:24:57 +0900

    OptionSet

---
+++ OptionSetDevices.playground/Contents.swift
@@ -0,0 +1,26 @@
+
+
+struct Devices: OptionSet { //OptionSet 프로토콜
+    let rawValue: Int
+    
+    static let phone = Devices(rawValue: 1 << 0)    
+    static let tablet = Devices(rawValue: 1 << 1)
+    static let watch = Devices(rawValue: 1 << 2)
+    static let laptop = Devices(rawValue: 1 << 3)
+    static let desktop = Devices(rawValue: 1 << 4)
+    static let tv = Devices(rawValue: 1 << 5)
+    static let brainImplant = Devices(rawValue: 1 << 6)
+
+    static let none: Devices = []
+    
+    static let all: Devices = [.phone, .tablet, .watch, .laptop, .desktop, .tv, .brainImplant]
+    
+    static let stationary: Devices = [.desktop, .tv]
+    
+    static let supportsUIKit: Devices = [.phone, .tablet, .tv]
+    
+}
+
+let supportedDevices: Devices = [.phone, .tablet, .watch, .tv]
+
+let phoneIsSupported = supportedDevices.contains(.phone)
