4540943d884575f906c8fd996ed5528729f40002
jmbae <jmbae@codegrove.co.kr>
2024-04-04 13:36:55 +0900

    Foundation and Calendar Date

---
+++ FoundationPlayground.playground/Contents.swift
@@ -0,0 +1,34 @@
+import Foundation
+
+func howLongUntilChristmas() -> String {
+    let calendar = Calendar.current
+    let timeZone = TimeZone.current
+    
+    let now = Date()
+    let yearOfNextChristmas = calendar.component(.year, from: now)
+    
+    var component = DateComponents(calendar: calendar,
+                                   timeZone: timeZone,
+                                   year: yearOfNextChristmas,
+                                   month: 12,
+                                   day: 25,
+                                   hour: 0,
+                                   minute: 0,
+                                   second: 0)
+    
+    var christmas = component.date!
+    if christmas < now {
+        component.year = yearOfNextChristmas + 1
+        christmas = component.date!
+    }
+    
+    let componentFormatter = DateComponentsFormatter()
+    componentFormatter.unitsStyle = .full
+    componentFormatter.allowedUnits = [.month, .day, .hour, .minute, .second]
+    
+    return componentFormatter.string(from: now, to: christmas)!
+}
+
+let timeUntilChristmas = howLongUntilChristmas()
+print("Time until Christmas: \(timeUntilChristmas)")
+
