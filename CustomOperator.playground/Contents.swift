66eb724050ba9562e3f538fd5ece5af8a99f9db3
Ruel <c0407h@gmail.com>
2024-04-04 11:16:16 +0900

    Generic Operator

--- CustomOperator.playground/Contents.swift
+++ CustomOperator.playground/Contents.swift
@@ -2,26 +2,26 @@
 
 infix operator >>>
 
-func >>> (lhs: String, rhs: String) -> String {
+//func >>> (lhs: String, rhs: String) -> String {
+//    var combined = rhs
+//    combined.append(lhs)
+//    return combined
+//}
+
+func >>> <Element> (lhs: Element, rhs: [Element]) -> [Element] {
     var combined = rhs
     combined.append(lhs)
     return combined
 }
 
-func >>> (lhs: String, rhs: [String]) -> [String] {
-    var combined = rhs
-    combined.append(lhs)
-    return combined
-}
-
-func >>> (lhs: [String], rhs: [String]) -> [String] {
+func >>> <Element> (lhs: [Element], rhs: [Element]) -> [Element] {
     var combined = rhs
     combined.append(contentsOf: lhs)
     return combined
 }
 
-let appendedString = "Two" >>> "One"
-print(appendedString)
+//let appendedString = "Two" >>> "One"
+//print(appendedString)
 
 
 let appendedString2 = "Two" >>> ["One"]
