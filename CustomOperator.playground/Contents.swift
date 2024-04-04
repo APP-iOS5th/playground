6441e1ae35b44df9a27297b4611a675f77f060bf
Jin Park <jinpark1504@gmail.com>
2024-04-04 11:36:15 +0900

    custom operator with extension

--- CustomOperator.playground/Contents.swift
+++ CustomOperator.playground/Contents.swift
@@ -35,3 +35,35 @@ print(appendedString3)
 
 let appendedString4 = [2, "Three"] >>> ["One", 0]
 print(appendedString4)
+
+struct Task {
+    let name: String
+}
+
+// CustomStringConvertible 에 description 이 정의되어 있고 출력시 description 의 내용을 출력
+// CustomStringConvertible 이 없으면 클래스 네임 TaskList를 출력할 것이다
+class TaskList: CustomStringConvertible {
+    private var tasks: [Task] = []
+    func append(task: Task) {
+        tasks.append(task)
+    }
+    var description: String {
+        return tasks.map { $0.name }.joined(separator: "\n")
+    }
+}
+
+// Task 가 배열이 아니므로 custom operator 를 사용하기 위해 extension 사용
+extension TaskList {
+    static func >>> (lhs: Task, rhs:TaskList) {
+        rhs.append(task: lhs)
+    }
+}
+
+let shoppingList = TaskList()
+let task1 = Task(name: "get milk")
+//shoppingList.append(task: task1)
+task1 >>> shoppingList
+let task2 = Task(name: "get teabags")
+//shoppingList.append(task: task2)
+task2 >>> shoppingList
+print(shoppingList)
