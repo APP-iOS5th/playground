c4e8116abebb2db64f346d661f9998b9bd5e1821
JongWon Jeong <jjwon214@gmail.com>
2024-04-05 09:17:03 +0900

    initialize iOS project

---
+++ Cocoatouch/Cocoatouch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cocoatouch
+//
+//  Created by 정종원 on 4/4/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
