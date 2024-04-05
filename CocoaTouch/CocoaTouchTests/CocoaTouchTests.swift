698ced295717aa89a3f2fff1a3d245fa94d0b62e
MadCow <lobasketve@naver.com>
2024-04-05 15:31:27 +0900

    XCTest - Error

--- CocoaTouch/CocoaTouchTests/CocoaTouchTests.swift
+++ CocoaTouch/CocoaTouchTests/CocoaTouchTests.swift
@@ -8,6 +8,31 @@
 import XCTest
 @testable import CocoaTouch
 
+class MockURLSession: URLSession {
+    override func dataTask(with request: URLRequest, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
+        return MockURLSessionDataTask(completionHandler: completionHandler, request: request)
+    }
+}
+
+class MockURLSessionDataTask: URLSessionDataTask {
+    
+    var completionHandler: (Data?, URLResponse?, Error?) -> Void
+    var request: URLRequest
+    
+    init(completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void, request: URLRequest) {
+        self.completionHandler = completionHandler
+        self.request = request
+        super.init()
+    }
+    
+    var calledResume = false
+    
+    override func resume() {
+        calledResume = true
+    }
+    
+}
+
 final class CocoaTouchTests: XCTestCase {
     
     var viewControllerUnderTest: ReposTableViewController?
@@ -16,8 +41,52 @@ final class CocoaTouchTests: XCTestCase {
         viewControllerUnderTest = ReposTableViewController()
     }
     
+    override func tearDown() {
+        super.tearDown()
+        viewControllerUnderTest = nil
+    }
+    
     func testThatRepoIsNotNil() {
         XCTAssertNotNil(viewControllerUnderTest?.repos)
+    }
+    
+    var mockData: Data {
+        guard let path = Bundle.main.path(forResource: "mock_Data", ofType: "json") else {
+            return Data()
+        }
+        print(path)
+        guard let contents = FileManager.default.contents(atPath: path) else {
+            return Data()
+        }
+        print(contents)
+//        if let path = Bundle.main.path(forResource: "mock_Data", ofType: "json"), let contents = FileManager.default.contents(atPath: path) {
+//            print(contents)
+//            return contents
+//        }
+        
+        return contents
+    }
+    
+    func testThatFetchRepoParsesSuccessfulData() {
+        viewControllerUnderTest?.session = MockURLSession()
+        
+        var responseObject: FetchReposResult?
+        
+        let result = viewControllerUnderTest?.fetchRepos(forUsername: "madcow95", completionHandler: { res in
+            print("response > \(res)")
+            responseObject = res
+        }) as? MockURLSessionDataTask
+        
+        
+        result?.completionHandler(mockData, nil, nil)
+        print(mockData)
+        switch responseObject {
+        case .success(let repos):
+            XCTAssertEqual(repos.count, 9)
+            XCTAssertEqual(repos.first?.name, "aerogear-ios-oauth2")
+        default:
+            XCTFail()
+        }
     }
 
     override func setUpWithError() throws {
