c36e094602b1a49459cca284e52d5ca81e2cbbbf
hwang <jangku88@naver.com>
2024-04-05 15:00:21 +0900

    XCTest

--- CocoaTouch/CocoaTouchTests/CocoaTouchTests.swift
+++ CocoaTouch/CocoaTouchTests/CocoaTouchTests.swift
@@ -9,15 +9,26 @@ import XCTest
 @testable import CocoaTouch
 
 final class CocoaTouchTests: XCTestCase {
-
+    
+    var viewControllerUnderTest: ReposTableViewController?
+    
+    override func setUp() {
+        viewControllerUnderTest = ReposTableViewController()
+    }
+    
+    //테스트 코드의 이름을 내가 하려는 테스트의 이름으로 하는게 좋음
+    func testThatRepoIsNotNil() {
+        XCTAssertNotNil(viewControllerUnderTest?.repos)
+    }
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
-
+    
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
+    
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
@@ -25,12 +36,12 @@ final class CocoaTouchTests: XCTestCase {
         // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
         // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
     }
-
+    
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
         self.measure {
             // Put the code you want to measure the time of here.
         }
     }
-
+    
 }
