b78df5c1792cd55cc902d10abe646f608ec5054f
seokyung402 <wni402@naver.com>
2024-04-05 10:42:27 +0900

    Repos TableViewCell reuseIdentifier setting

--- CocoaTouch/CocoaTouch/ReposTableViewController.swift
+++ CocoaTouch/CocoaTouch/ReposTableViewController.swift
@@ -7,11 +7,75 @@
 
 import UIKit
 
+struct Repo: Codable {
+    let name: String?
+    let url: URL?
+    
+    enum CodingKeys: String, CodingKey {
+        case name = "name"
+        case url = "html_url"
+    }
+}
+
+enum FetchReposResult {
+    case success([Repo])
+    case failure(Error)
+}
+
+enum ResponseError: Error {
+    case requestUnsuccessful
+    case enexpectedResponseStructure
+}
+
 class ReposTableViewController: UITableViewController {
+    
+    internal var session = URLSession.shared
+    internal var repos = [Repo]()
+    
+    @discardableResult
+    internal func fetchRepos(forUsername username: String, completionHandler: @escaping (FetchReposResult) -> Void) -> URLSessionDataTask? {
+        
+        let urlString = "https://api.github.com/users/\(username)/repos"
+        guard let url = URL(string: urlString) else {
+            return nil
+        }
+        var request = URLRequest(url: url)
+        request.setValue("application/vnd.github.v3+json",
+          forHTTPHeaderField: "Accept")
+        let task = session.dataTask(with: request) { (data,
+          response, error) in
+            
+            // First unwrap the optional data
+            guard let data = data else {
+                completionHandler(.failure(ResponseError.requestUnsuccessful))
+                return
+            }
+            
+            do {
+                let decoder = JSONDecoder()
+                let responseObject = try decoder.decode([Repo].self, from: data)
+                
+                completionHandler(.success(responseObject))
+            } catch {
+                completionHandler(.failure(error))
+            }
+        }
+        task.resume()
+
+        return task
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.title = "Repos"
+        
+        let repo1 = Repo(name: "Test Repo 1", url: URL(string: "https://example.com/repo1"))
+        let repo2 = Repo(name: "Test Repo 2", url: URL(string: "https://example.com/repo2"))
 
+        repos.append(contentsOf: [repo1, repo2])
+        
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
@@ -23,23 +87,22 @@ class ReposTableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return repos.count
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
+        let cell = tableView.dequeueReusableCell(withIdentifier: "RepoCell", for: indexPath)
+        
+        let repo = repos[indexPath.row]
+        cell.textLabel?.text = repo.name
 
         return cell
     }
-    */
 
     /*
     // Override to support conditional editing of the table view.
