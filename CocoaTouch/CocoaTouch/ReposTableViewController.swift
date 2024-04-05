e6e1d40e83ce9207cb233c84de638fe258629d53
Ruel <c0407h@gmail.com>
2024-04-05 09:15:25 +0900

    initaial iOS Project

---
+++ CocoaTouch/CocoaTouch/ReposTableViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ReposTableViewController.swift
+//  CocoaTouch
+//
+//  Created by Chung Wussup on 4/4/24.
+//
+
+import UIKit
+
+class ReposTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+      
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    
+        return 0
+    }
+
+
+}
