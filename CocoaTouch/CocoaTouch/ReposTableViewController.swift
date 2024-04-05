2d50b5570d4a420cd558a61472ec0a0f41515220
Cha, Jiyong <wldyd2113@gmail.com>
2024-04-05 14:17:53 +0900

    TextField delegation

--- CocoaTouch/CocoaTouch/ReposTableViewController.swift
+++ CocoaTouch/CocoaTouch/ReposTableViewController.swift
@@ -172,3 +172,32 @@ class ReposTableViewController: UITableViewController {
      */
     
 }
+extension ReposTableViewController: UITextFieldDelegate {
+    
+    //이벤트 핸들링
+    public func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        guard let enteredUsername = textField.text else{
+            repos = []
+            tableView.reloadData()
+            return true
+        }
+        fetchRepos(forUsername: enteredUsername) { [weak self] result in
+            switch result {
+            case .success(let repos):
+                self?.repos = repos
+            case .failure(let error):
+                self?.repos = []
+                print("There was an error: \(error)")
+            }
+            DispatchQueue.main.async {
+                self?.tableView.reloadData()
+            }
+            
+        }
+        
+        textField.resignFirstResponder()
+        
+        return true
+    }
+    
+}
