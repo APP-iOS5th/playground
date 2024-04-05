f0e1800589366ab8beb092a5dfc1190447db01de
hwang <jangku88@naver.com>
2024-04-05 15:30:03 +0900

    TearDown

--- CocoaTouch/CocoaTouchTests/CocoaTouchTests.swift
+++ CocoaTouch/CocoaTouchTests/CocoaTouchTests.swift
@@ -8,17 +8,56 @@
 import XCTest
 @testable import CocoaTouch
 
+class MockURLSession: URLSession {
+    override func dataTask(with request: URLRequest, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
+        return MockURLSessionDataTask(completionHandler: completionHandler, request: request)
+    }
+}
+
+class MockURLSessionDataTask: URLSessionDataTask {
+    
+    var completionHandler: (Data?, URLResponse?, Error?) -> Void
+    var request: URLRequest
+    
+    init(completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void, request: URLRequest) {
+        self.completionHandler = completionHandler
+        self.request = request
+        super.init()
+    }
+    
+    var calledResume = false
+    
+    override func resume() {
+        calledResume = true
+    }
+    
+}
+
+
 final class CocoaTouchTests: XCTestCase {
     
     var viewControllerUnderTest: ReposTableViewController?
     
     override func setUp() {
+        super.setUp()
         viewControllerUnderTest = ReposTableViewController()
+    }
+    
+    override func tearDown() {
+         viewControllerUnderTest = nil
+        super.tearDown()
     }
     
     //테스트 코드의 이름을 내가 하려는 테스트의 이름으로 하는게 좋음
     func testThatRepoIsNotNil() {
         XCTAssertNotNil(viewControllerUnderTest?.repos)
+    }
+    
+    func testThatFetchRepoParsesSuccessfulData() {
+        
+        viewControllerUnderTest?.fetchRepos(forUserName: "", completionHandler: { (respone) in
+            print("\(respone)")
+        })
     }
     
     override func setUpWithError() throws {
