11d5dbafe645f7a5a8f59eea68e33f845002e44e
hwang <jangku88@naver.com>
2024-04-05 15:55:15 +0900

    Create BarChart View

--- Simple_iOS.playground/Contents.swift
+++ Simple_iOS.playground/Contents.swift
@@ -32,3 +32,50 @@ class BarView: UIView {
     }
 }
 
+class BarChart: UIView {
+    
+    private var barViews: [BarView] = []
+    private var maxValue: Float = 0.0
+
+    var interBarMargin: CGFloat = 5.0
+
+    var bars: [Bar] = [] {
+        didSet {
+            self.barViews.forEach { $0.removeFromSuperview() }
+            
+            var barViews = [BarView]()
+            
+            let barCount = CGFloat(bars.count)
+            
+            // Calculate the max value before calculating size
+            for bar in bars {
+                maxValue = max(maxValue, bar.value)
+            }
+            
+            var xOrigin: CGFloat = interBarMargin
+
+            let margins = interBarMargin * (barCount+1)
+            let width = (frame.width - margins) / barCount
+            
+            for bar in bars {
+
+                let height = barHeight(forValue: bar.value)
+                let rect = CGRect(x: xOrigin,
+                                  y: bounds.height - height,
+                                  width: width,
+                                  height: height)
+                let view = BarView(frame: rect,
+                                   color: bar.color.displayColor)
+                barViews.append(view)
+                addSubview(view)
+                
+                xOrigin = view.frame.maxX + interBarMargin
+            }
+            self.barViews = barViews
+        }
+    }
+    
+    private func barHeight(forValue value: Float) -> CGFloat {
+        return (frame.size.height / CGFloat(maxValue)) * CGFloat(value)
+    }
+}
