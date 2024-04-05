c2f00b2c9074e29f1566ff8fcd7772bb8108538f
wotjs020708 <wotjs020708@gmail.com>
2024-04-05 15:02:14 +0900

    Test

--- CocoaTouch/CocoaTouchTests/CocoaTouchTests.swift
+++ CocoaTouch/CocoaTouchTests/CocoaTouchTests.swift
@@ -9,7 +9,22 @@ import XCTest
 @testable import CocoaTouch
 
 final class CocoaTouchTests: XCTestCase {
+    
+    var viewControllerUnderTest: ReposTableViewController?
+    
+    override func setUp() {
+        viewControllerUnderTest = ReposTableViewController()
+        
+    }
+    
+    override func tearDown() {
+        viewControllerUnderTest = nil
+        super.tearDown()
+    }
 
+    func testThatRepoIsNotNil() {
+        XCTAssertNotNil(viewControllerUnderTest?.repos)
+    }
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
