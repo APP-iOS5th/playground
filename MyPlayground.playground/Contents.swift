4fff0b2f424544e41c99bcd00be864afff32dbd1
shin-jae-uk <shinju1412@gmail.com>
2024-04-05 10:43:07 +0900

    Repos TableViewCell reuseIdentifier setting

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,41 +1,8 @@
-// Protocol Generic
 
-protocol TransportMethod {
-    associatedtype CollectionPoint
-    var defaultCollectionPoint: CollectionPoint { get }
-    var averageSpeedInKPH: Double { get }
-}
+var x = 10
 
-struct Train: TransportMethod {
-    typealias CollectionPoint = TrainStation
-    
-    var defaultCollectionPoint: TrainStation {
-        return TrainStation.BMS
-    }
-    
-    var averageSpeedInKPH: Double {
-        return 100
-    }
-}
-
-enum TrainStation: String {
-    case BMS = "Bromley South"
-    case VIC = "London Victoria"
-    case RAI = "Rainham (Kent)"
-    case BTN = "Brighton (East Sussex)"
-}
-
-class Journey<TransportType: TransportMethod> {
-    let start: TransportType.CollectionPoint
-    let end: TransportType.CollectionPoint
-    let method: TransportType
-    
-    init(start: TransportType.CollectionPoint,
-         end: TransportType.CollectionPoint,
-         method: TransportType) {
-        self.start = start
-        self.end = end
-        self.method = method
-    }
-    
+for index in 1...20 {
+    let y = index * x
+    x -= 1
+    print(y)
 }
