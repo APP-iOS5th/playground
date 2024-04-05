0ab666f604215e21a18bf2053ff165a96eafa1c5
hwang <jangku88@naver.com>
2024-04-05 15:54:19 +0900

    Create Color struct and Bar,BarView

--- Simple_iOS.playground/Contents.swift
+++ Simple_iOS.playground/Contents.swift
@@ -1,3 +1,34 @@
 import UIKit
 
-var greeting = "Hello, playground"
+struct Color {
+    let red: Float
+    let green: Float
+    let blue: Float
+    let alpha: Float = 1.0
+    
+    var displayColor: UIColor {
+        return UIColor(red: CGFloat(red),
+                       green: CGFloat(green),
+                       blue: CGFloat(blue),
+                       alpha: CGFloat(alpha))
+    }
+}
+
+struct Bar {
+    var value: Float
+    var color: Color
+}
+
+class BarView: UIView {
+    
+    init(frame: CGRect, color: UIColor) {
+        super.init(frame: frame)
+        backgroundColor = color
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        backgroundColor = .red
+    }
+}
+
