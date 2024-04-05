87e0b021a565420f2ce089ad91ac886504bb5829
jihye <ganzi410@gmail.com>
2024-04-05 14:18:00 +0900

    TextField delegate event handling

--- CocoaTouch/CocoaTouch/ReposTableViewController.swift
+++ CocoaTouch/CocoaTouch/ReposTableViewController.swift
@@ -164,3 +164,30 @@ class ReposTableViewController: UITableViewController {
      */
     
 }
+
+extension ReposTableViewController: UITextFieldDelegate {
+    public func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        guard let enteredUserName = textField.text else {
+            repos = []
+            tableView.reloadData()
+            return true
+        }
+        
+        fetchRepos(forUserName: enteredUserName) { [weak self] result in
+            switch result {
+            case .success(let repos):
+                self?.repos = repos
+            case .failure(let error):
+                self?.repos =  []
+                print("There was an error: \(error)")
+            }
+            DispatchQueue.main.async {
+                self?.tableView.reloadData()
+            }
+        }
+        
+        textField.resignFirstResponder()
+        
+        return true
+    }
+}
