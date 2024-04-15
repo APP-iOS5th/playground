57cd50e8c951b7e0b3bfd73ad3ac7698abda86ff
jeongwookim <112370814+JeongWo@users.noreply.github.com>
2024-04-15 11:49:05 +0900

    override

--- MyPlaygroundClass.playground/Contents.swift
+++ MyPlaygroundClass.playground/Contents.swift
@@ -59,3 +59,38 @@ print(bankAccount.accountNumber)
 bankAccount.displayBalance()
 
 print(BankAccount.getMaxBalance())
+
+class SavingAccount: BankAccount {
+    var interestRate: Float = 0.0
+    
+    init(number: Int, balance: Float, rate: Float) {
+        interestRate = rate
+        super.init(number: number, balance: balance)
+    }
+    
+    func calculateInterest() -> Float {
+        return interestRate * accountBlance
+    }
+    override func displayBalance() {
+        super.displayBalance()
+        print("Prevailing interest reate is \(interestRate)")
+    }
+}
+
+var savingAccount = SavingAccount(number: 12311, balance: 600.00, rate: 0.07)
+
+print(savingAccount.calculateInterest())
+savingAccount.displayBalance()
+
+
+extension Double {
+    var squared: Double {
+        return self * self
+    }
+    var cubed: Double {
+        return self * self * self
+    }
+}
+
+let myValue: Double = 3.0
+print(myValue.squared)
