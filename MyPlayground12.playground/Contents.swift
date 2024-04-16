6e413824dc0e416d0fea4dcc462e17f1e8d67649
jeongwookim <112370814+JeongWo@users.noreply.github.com>
2024-04-16 10:04:45 +0900

    Class&Struct

---
+++ MyPlayground12.playground/Contents.swift
@@ -0,0 +1,56 @@
+
+struct SampleStruct {
+    var name: String
+    
+    init(name: String) {
+        self.name = name
+    }
+    
+    func buildHelloMsg() {
+        "Hello" + name
+    }
+}
+
+class SampleClass {
+    var name: String
+    init(name: String) {
+        self.name = name
+    }
+    func buildingHelloMsg() {
+        "Hello" + name
+    }
+}
+
+let myStruct1 = SampleStruct(name: "Mark")
+var myStruct2 = myStruct1
+myStruct2.name = "David"
+
+print(myStruct1.name)
+print(myStruct2.name)
+
+let myClass1 = SampleClass(name: "mark")
+var myClass2 = myClass1
+myClass2.name = "David"
+
+print(myClass1.name)
+print(myClass2.name)
+
+enum Temperature {
+    case hot
+    case warm
+    case cold(cetigrade: Int)
+}
+
+func displayTempInfo(temp: Temperature) {
+    switch temp {
+    case .hot:
+        print("It is hot")
+    case .warm:
+        print("It is warm")
+    case .cold(let centigrade) where centigrade <= 0:
+        print("Ice warning: \(centigrade) degrees.")
+    case .cold:
+        print("It is cold")
+    }
+}
+displayTempInfo(temp: .cold(cetigrade: -10))
