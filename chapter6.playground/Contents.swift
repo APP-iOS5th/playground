0cee4c8572f85237d38bdaea6d6391dc5b4739ea
Jinx <jinxwift@gmail.com>
2024-04-18 09:24:53 +0900

    chapter6 교재 스터디

---
+++ chapter6.playground/Contents.swift
@@ -0,0 +1,77 @@
+import UIKit
+
+//var x: Int?
+//var y = 10
+//
+//x = 10
+//x = x! + y
+//x = y
+//
+//
+//x = y * 10 + z - 5 / 4
+//
+//var result: Bool?
+//var x = 10
+//var y = 20
+//
+//result = x < y
+//print(result)
+
+//var flag = true
+//var secondFlag = !flag
+//print(flag)
+//print(secondFlag)
+//
+//if (10 < 20) || (20 < 10) {
+//    print("Expression is true")
+//}
+//
+//if (10 < 20) && (20 < 10) {
+//    print("Expression is true")
+//} else {
+//    print("Expression is not true")
+//}
+
+//let x = 10
+//let y = 20
+//
+//print("Largest number is \(x > y ? x : y)")
+//print("Largest number is \(x < y ? x : y)")
+//
+//let customerName: String? = nil
+//print("Welcome back, \(customerName ?? "customer")")
+//
+//let customerName2: String? = "Joohyun"
+//print("Welcome back, \(customerName2 ?? "customer")")
+
+//let y = 3
+//let z = ~y
+//print("Result is \(z)")
+
+//let x = 171
+//let y = 3
+//let z = x & y
+//print("Result is \(z)")
+
+//let x = 171
+//let y = 3
+//let z = x | y
+//print("Result is \(z)")
+//
+
+//let x = 171
+//let y = 3
+//let z = x ^ y
+//print("Result is \(z)")
+//
+
+//let x = 171
+//let z = x << 1
+//print("Result is \(z)")
+
+let x = 171
+let z = x >> 1
+print("Result is \(z)")
+
+
+
